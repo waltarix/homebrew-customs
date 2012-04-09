18d5de32ff0551708d692a835cb825b1c6faaa04
waltari <gitting.crew@gmail.com>
2012-04-09 16:53:47 +0900

    canything formula

---
+++ canything.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Canything < Formula
+  homepage 'https://github.com/keiji0/canything'
+  url 'https://github.com/keiji0/canything/tarball/master'
+  md5 'b32f80adf2b7b4a73cee9eda57bfeb2a'
+  version '1.0.0'
+
+  def install
+    inreplace 'Makefile', '-lncursesw', "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib -lncursesw"
+    system 'make'
+    bin.install 'canything'
+  end
+end
