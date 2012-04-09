aa4c4ff9f90f670e52d154cf8e4eab9059a5d8d9
waltari <gitting.crew@gmail.com>
2012-04-09 16:57:42 +0900

    canything: fix version string

--- canything.rb
+++ canything.rb
@@ -4,7 +4,7 @@ class Canything < Formula
   homepage 'https://github.com/keiji0/canything'
   url 'https://github.com/keiji0/canything/tarball/master'
   md5 'b32f80adf2b7b4a73cee9eda57bfeb2a'
-  version '1.0.0'
+  version '20110511'
 
   def install
     inreplace 'Makefile', '-lncursesw', "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib -lncursesw"
