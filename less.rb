5d8184cffeaf8959a2ddb997bdabed109e5ab37b
waltari <gitting.crew@gmail.com>
2012-05-31 16:49:08 +0900

    less: fix dependency

--- less.rb
+++ less.rb
@@ -6,9 +6,10 @@ class Less < Formula
   md5 '56f9f76ffe13f70155f47f6b3c87d421'
 
   depends_on 'pcre'
+  depends_on 'waltarix/customs/ncurses'
 
   def install
-    ENV.append "LIBS", "-lncurses"
+    ENV.append "LIBS", "-L#{HOMEBREW_PREFIX}/lib -lncursesw"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}", "--with-regex=pcre"
     system "make install"
