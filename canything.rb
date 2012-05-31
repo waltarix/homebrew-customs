e1d8ec9cb34571ff06b5bf0543a77313e1b074d1
waltari <gitting.crew@gmail.com>
2012-05-31 16:48:35 +0900

    canything: fix dependency

--- canything.rb
+++ canything.rb
@@ -6,6 +6,8 @@ class Canything < Formula
   md5 'b32f80adf2b7b4a73cee9eda57bfeb2a'
   version '20110511'
 
+  depends_on 'waltarix/customs/ncurses'
+
   def install
     inreplace 'Makefile', '-lncursesw', "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib -lncursesw"
     system 'make'
