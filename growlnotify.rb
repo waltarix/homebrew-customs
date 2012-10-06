1efc669cb8f466bd5c3b9897c4b613a137fefabc
waltari <gitting.crew@gmail.com>
2012-10-06 11:03:40 +0900

    growlnotify 2.0

--- growlnotify.rb
+++ growlnotify.rb
@@ -1,12 +1,33 @@
 require 'formula'
 
+def pkg_name; 'GrowlNotify.pkg'; end
+def bin_payload; 'growlnotify.pkg/Payload'; end
+def man_payload; 'growlnotify-1.pkg/Payload'; end
+
+class GrowlnotifyPkgDownloadStrategy < CurlDownloadStrategy
+  def stage
+    safe_system '/usr/bin/unzip', @tarball_path
+    safe_system '/usr/bin/xar', '-xf', pkg_name, bin_payload
+    safe_system '/usr/bin/xar', '-xf', pkg_name, man_payload
+
+    # extract
+    system "cat #{bin_payload} | gzip -d - | cpio -id" # => growlnotify
+    system "cat #{man_payload} | gzip -d - | cpio -id" # => growlnotify.1
+  end
+end
+
 class Growlnotify < Formula
-  url 'https://github.com/indirect/growlnotify/tarball/v1.3'
-  md5 '44f771b0bafd0fc077ecc546841d9831'
-  homepage 'http://growl.info/extras.php#growlnotify'
+  url 'http://growl.cachefly.net/GrowlNotify-2.0.zip'
+  sha1 'efd54dec2623f57fcbbba54050206d70bc7746dd'
+  version '2.0'
+  homepage 'http://growl.info/extras.php'
+
+  def download_strategy
+    GrowlnotifyPkgDownloadStrategy
+  end
 
   def install
-    bin.install "growlnotify"
-    man1.install gzip("growlnotify.1")
+    bin.install 'growlnotify'
+    man1.install gzip('growlnotify.1')
   end
 end
