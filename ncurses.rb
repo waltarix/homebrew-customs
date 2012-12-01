f9012236e543be54416131271f5c6432d509795f
waltari <gitting.crew@gmail.com>
2012-12-01 10:42:36 +0900

    ncurses: merge upstream

--- ncurses.rb
+++ ncurses.rb
@@ -6,12 +6,10 @@ class Ncurses < Formula
   mirror 'http://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz'
   sha1 '3e042e5f2c7223bffdaac9646a533b8c758b65b5'
 
-  def options
-    [['--universal', 'Build for both 32 & 64 bit Intel.']]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
 
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
