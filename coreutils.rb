cd717853ee91a6f0ef0e1bc8b3a36409a69a1aa7
waltari <gitting.crew@gmail.com>
2014-11-05 13:18:10 +0900

    coreutils 8.23

--- coreutils.rb
+++ coreutils.rb
@@ -1,28 +1,37 @@
-require 'formula'
+require "formula"
 
 class Coreutils < Formula
-  homepage 'http://www.gnu.org/software/coreutils'
-  url 'http://ftpmirror.gnu.org/coreutils/coreutils-8.21.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.21.tar.xz'
-  sha256 'adaa44bdab3fa5eb352e80d8a31fdbf957b78653d0c2cd30d63e161444288e18'
+  homepage "https://www.gnu.org/software/coreutils"
+  url "http://ftpmirror.gnu.org/coreutils/coreutils-8.23.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/coreutils/coreutils-8.23.tar.xz"
+  sha256 "ec43ca5bcfc62242accb46b7f121f6b684ee21ecd7d075059bf650ff9e37b82d"
+  revision 1
 
-  depends_on 'xz' => :build
+  conflicts_with "ganglia", :because => "both install `gstat` binaries"
+  conflicts_with "idutils", :because => "both install `gid` and `gid.1`"
+
+  # Patch adapted from upstream commits:
+  # http://git.savannah.gnu.org/gitweb/?p=coreutils.git;a=commitdiff;h=6f9b018
+  # http://git.savannah.gnu.org/gitweb/?p=coreutils.git;a=commitdiff;h=3cf19b5
+  patch :DATA
 
   def patches
-    'https://gist.github.com/waltarix/1408362/raw/291a2193ae7a6caff41258f63eb1c9cb93e53f88/coreutils-ls-utf8mac.patch'
+    'https://gist.githubusercontent.com/waltarix/1408362/raw'
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--program-prefix=g"
+    system "./configure", "--prefix=#{prefix}",
+                          "--program-prefix=g",
+                          "--without-gmp"
     system "make install"
 
     # Symlink all commands into libexec/gnubin without the 'g' prefix
     coreutils_filenames(bin).each do |cmd|
-      (libexec/'gnubin').install_symlink bin/"g#{cmd}" => cmd
+      (libexec/"gnubin").install_symlink bin/"g#{cmd}" => cmd
     end
     # Symlink all man(1) pages into libexec/gnuman without the 'g' prefix
     coreutils_filenames(man1).each do |cmd|
-      (libexec/'gnuman'/'man1').install_symlink man1/"g#{cmd}" => cmd
+      (libexec/"gnuman"/"man1").install_symlink man1/"g#{cmd}" => cmd
     end
   end
 
@@ -32,12 +41,12 @@ class Coreutils < Formula
     If you really need to use these commands with their normal names, you
     can add a "gnubin" directory to your PATH from your bashrc like:
 
-        PATH="#{opt_prefix}/libexec/gnubin:$PATH"
+        PATH="#{opt_libexec}/gnubin:$PATH"
 
     Additionally, you can access their man pages with normal names if you add
     the "gnuman" directory to your MANPATH from your bashrc as well:
 
-        MANPATH="#{opt_prefix}/libexec/gnuman:$MANPATH"
+        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
 
     EOS
   end
@@ -45,9 +54,24 @@ class Coreutils < Formula
   def coreutils_filenames (dir)
     filenames = []
     dir.find do |path|
-      next if path.directory? or path.basename.to_s == '.DS_Store'
-      filenames << path.basename.to_s.sub(/^g/,'')
+      next if path.directory? or path.basename.to_s == ".DS_Store"
+      filenames << path.basename.to_s.sub(/^g/,"")
     end
     filenames.sort
   end
 end
+
+__END__
+diff --git a/Makefile.in b/Makefile.in
+index 140a428..bae3163 100644
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -2566,7 +2566,7 @@ pkglibexecdir = @pkglibexecdir@
+ # Use 'ginstall' in the definition of PROGRAMS and in dependencies to avoid
+ # confusion with the 'install' target.  The install rule transforms 'ginstall'
+ # to install before applying any user-specified name transformations.
+-transform = s/ginstall/install/; $(program_transform_name)
++transform = s/ginstall/install/;/libstdbuf/!$(program_transform_name)
+ ACLOCAL = @ACLOCAL@
+ ALLOCA = @ALLOCA@
+ ALLOCA_H = @ALLOCA_H@
