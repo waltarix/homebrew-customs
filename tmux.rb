db13c72506a4605b2329697fd573cfed68caa748
waltari <gitting.crew@gmail.com>
2015-05-16 13:30:28 +0900

    tmux 2.0

--- tmux.rb
+++ tmux.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Tmux < Formula
   homepage 'http://tmux.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9a.tar.gz'
-  sha1 '815264268e63c6c85fe8784e06a840883fcfc6a2'
+  url 'https://downloads.sourceforge.net/project/tmux/tmux/tmux-2.0/tmux-2.0.tar.gz'
+  sha1 '977871e7433fe054928d86477382bd5f6794dc3d'
 
   bottle do
     cellar :any
-    revision 1
-    sha1 "5a5e180e33339671bc8c82ed58c26862da037f30" => :yosemite
-    sha1 "6092f92f5cd7eeb6ddf3b555cd4e655c4c85e826" => :mavericks
-    sha1 "981c8c199a2ea3df18b6651205b4616459ae1f8c" => :mountain_lion
+    sha256 "91a14274005416c9a20f64f149f732837b0503c0ddcfdc80f87c0576e99ee3fa" => :yosemite
+    sha256 "d70b62ddf26d2113a108622643550dc50248c98188af27d7e2e76e415f43588d" => :mavericks
+    sha256 "a1468fd6ac69c18c4773a65c11b2811525d542d911f6c6642e87c0e195f6c4c1" => :mountain_lion
   end
 
   def pour_bottle?
@@ -30,18 +29,18 @@ class Tmux < Formula
   depends_on 'homebrew/dupes/ncurses'
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1399751/raw/e60e879335bf3b91fef4592b194cc524bcb95388/tmux-ambiguous-width-cjk.patch"
-    sha256 "b77fa3de2cd43f1fe0c0423576b38e363d387343e8d0ed8a2702e67068d64fdf"
+    url "https://gist.githubusercontent.com/waltarix/1399751/raw/d3c9f57e7c22f3b5303c23520b434dc33313f7a5/tmux-ambiguous-width-cjk.patch"
+    sha256 "f2c47a70734205b3200d47a6dc838717aa19c7d4bcc601aa3c625838e9301ab0"
   end
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1399751/raw/d581fc517db8173581e4518e2025916a5cf5de09/tmux-do-not-combine-utf8.patch"
-    sha256 "97ec3f8375ce9e5e0d823cc79c056064c4da829d791eb8cc485d19bd43ae3eb2"
+    url "https://gist.githubusercontent.com/waltarix/1399751/raw/c53aff0a0c51f0b1be5c9d46b4e3807fe3560dbb/tmux-do-not-combine-utf8.patch"
+    sha256 "d1fbf914f734ea94641efe28476f428f3bc2d6fb04a0eb43114084f0526b229d"
   end
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1399751/raw/5914827c8f7fecfdb73c641e02c471acd55eb2af/tmux-pane-border-ascii.patch"
-    sha256 "d8ccd3696f08eabdf9dd823452fba4acfecda65f4238e8ef3851855c268dcbec"
+    url "https://gist.githubusercontent.com/waltarix/1399751/raw/289974e9b9e538d6afcd641935a14e108eae69a7/tmux-pane-border-ascii.patch"
+    sha256 "f2838066b2765a19c0d4421beb885302b9e4dba94cb8b9419a58a51da5a781cb"
   end
 
   def install
