06023fbe022d7327a999b2593da96b4225a7d031
waltari <gitting.crew@gmail.com>
2015-09-07 09:18:11 +0900

    zsh: 5.1

--- zsh.rb
+++ zsh.rb
@@ -1,31 +1,31 @@
-require 'formula'
-
 class Zsh < Formula
-  desc "A UNIX shell (command interpreter)"
-  homepage 'http://www.zsh.org/'
-  url 'https://downloads.sourceforge.net/project/zsh/zsh/5.0.8/zsh-5.0.8.tar.bz2'
-  mirror 'http://www.zsh.org/pub/zsh-5.0.8.tar.bz2'
-  sha256 '8079cf08cb8beff22f84b56bd72bb6e6962ff4718d816f3d83a633b4c9e17d23'
+  desc "UNIX shell (command interpreter)"
+  homepage "http://www.zsh.org/"
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1/zsh-5.1.tar.gz"
+  mirror "http://www.zsh.org/pub/zsh-5.1.tar.gz"
+  sha256 "e3731381810e690fb955cedfa8be51b0934bfa1ff38c709f54138960e3decd99"
 
   bottle do
-    sha1 "83d646649569ade648db6a44c480709d63268a25" => :yosemite
-    sha1 "935990ced3a6d3a3027bac4b32ac8f031e8fa244" => :mavericks
-    sha1 "c6e8055106d0b939cec5674469099bfd63d53f9e" => :mountain_lion
+    sha256 "ef2e5dd13668edd59725b6a320db7513a6635ec7d3fd30891eb4e87ace6887e8" => :yosemite
+    sha256 "313444fc801db870fce3855e3ecda1f8a63aa24e44aaa448805d2dd61e62d584" => :mavericks
+    sha256 "78563c521de6861a2278f8e9d44aa8eac86e5c699d0d75018f805ec34286c9cd" => :mountain_lion
   end
 
   def pour_bottle?
     false
   end
 
-  depends_on 'gdbm'
-  depends_on 'pcre'
-  depends_on 'homebrew/dupes/ncurses'
+  option "without-etcdir", "Disable the reading of Zsh rc files in /etc"
 
-  option 'disable-etcdir', 'Disable the reading of Zsh rc files in /etc'
+  deprecated_option "disable-etcdir" => "without-etcdir"
+
+  depends_on "gdbm"
+  depends_on "pcre"
+  depends_on "homebrew/dupes/ncurses"
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1407905/raw/c691d18f93269711bee64b985730de58e41bcaa0/zsh-ambiguous-width-cjk.patch"
-    sha256 "c38ecf9efd60873b43f3d5742690aecc982799592db165a6ff7d42fecf4eb0fa"
+    url "https://gist.githubusercontent.com/waltarix/1407905/raw/df46b8c86fb986892e8c434fe2b9b35952e5a615/zsh-ambiguous-width-cjk.patch"
+    sha256 "9cd729d61b09a574b90806967f1feaa31cc19e6fe8a476928cc7818c8cb058de"
   end
 
   def install
@@ -51,10 +51,10 @@ class Zsh < Formula
       zsh_cv_c_broken_wcwidth=yes
     ]
 
-    if build.include? 'disable-etcdir'
-      args << '--disable-etcdir'
+    if build.without? "etcdir"
+      args << "--disable-etcdir"
     else
-      args << '--enable-etcdir=/etc'
+      args << "--enable-etcdir=/etc"
     end
 
     system "./configure", *args
@@ -67,15 +67,16 @@ class Zsh < Formula
     system "make", "install.info"
   end
 
-  test do
-    system "#{bin}/zsh", "--version"
-  end
-
   def caveats; <<-EOS.undent
     Add the following to your zshrc to access the online help:
       unalias run-help
       autoload run-help
       HELPDIR=#{HOMEBREW_PREFIX}/share/zsh/help
     EOS
+  end
+
+  test do
+    assert_equal "homebrew\n",
+      shell_output("#{bin}/zsh -c 'echo homebrew'")
   end
 end
