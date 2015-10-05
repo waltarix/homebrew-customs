dde2afb4c847b73358fa6a5a8a2dd0e22677db55
waltari <gitting.crew@gmail.com>
2015-10-05 09:10:11 +0900

    zsh: 5.1.1

--- zsh.rb
+++ zsh.rb
@@ -1,9 +1,9 @@
 class Zsh < Formula
   desc "UNIX shell (command interpreter)"
   homepage "http://www.zsh.org/"
-  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1/zsh-5.1.tar.gz"
-  mirror "http://www.zsh.org/pub/zsh-5.1.tar.gz"
-  sha256 "e3731381810e690fb955cedfa8be51b0934bfa1ff38c709f54138960e3decd99"
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1.1/zsh-5.1.1.tar.gz"
+  mirror "http://www.zsh.org/pub/zsh-5.1.1.tar.gz"
+  sha256 "94ed5b412023761bc8d2f03c173f13d625e06e5d6f0dff2c7a6e140c3fa55087"
 
   bottle do
     sha256 "ef2e5dd13668edd59725b6a320db7513a6635ec7d3fd30891eb4e87ace6887e8" => :yosemite
@@ -24,8 +24,8 @@ class Zsh < Formula
   depends_on "homebrew/dupes/ncurses"
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1407905/raw/df46b8c86fb986892e8c434fe2b9b35952e5a615/zsh-ambiguous-width-cjk.patch"
-    sha256 "9cd729d61b09a574b90806967f1feaa31cc19e6fe8a476928cc7818c8cb058de"
+    url "https://gist.githubusercontent.com/waltarix/1407905/raw/f7b16c21df602c2b1b866b78939206c00ca8511d/zsh-ambiguous-width-cjk.patch"
+    sha256 "a0a903033a2a9c2f7da8d72713ece9721f187036e35568df824132360b8ca5ca"
   end
 
   def install
