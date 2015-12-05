4d80c43dd3c4d0d798c44bfe5848245fc166b231
waltari <gitting.crew@gmail.com>
2015-12-05 14:33:16 +0900

    zsh: 5.2

--- zsh.rb
+++ zsh.rb
@@ -1,18 +1,23 @@
 class Zsh < Formula
   desc "UNIX shell (command interpreter)"
   homepage "http://www.zsh.org/"
-  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1.1/zsh-5.1.1.tar.gz"
-  mirror "http://www.zsh.org/pub/zsh-5.1.1.tar.gz"
-  sha256 "94ed5b412023761bc8d2f03c173f13d625e06e5d6f0dff2c7a6e140c3fa55087"
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.2/zsh-5.2.tar.gz"
+  mirror "http://www.zsh.org/pub/zsh-5.2.tar.gz"
+  sha256 "fa924c534c6633c219dcffdcd7da9399dabfb63347f88ce6ddcd5bb441215937"
 
   bottle do
-    sha256 "ef2e5dd13668edd59725b6a320db7513a6635ec7d3fd30891eb4e87ace6887e8" => :yosemite
-    sha256 "313444fc801db870fce3855e3ecda1f8a63aa24e44aaa448805d2dd61e62d584" => :mavericks
-    sha256 "78563c521de6861a2278f8e9d44aa8eac86e5c699d0d75018f805ec34286c9cd" => :mountain_lion
+    sha256 "cd259a16f0645a085e01b49b6ff92f5d11605775e6b10ba4759be3487fb29b5d" => :el_capitan
+    sha256 "0d33821053045530db4f7b9588d9f5dced22d02a8b6491260aecb990ad55fb7e" => :yosemite
+    sha256 "47b00e430b5922fdf7260ccdd77e510c50961a55e11cc89aa029a11e8e82d309" => :mavericks
   end
 
   def pour_bottle?
     false
+  end
+
+  head do
+    url "git://git.code.sf.net/p/zsh/code"
+    depends_on "autoconf" => :build
   end
 
   option "without-etcdir", "Disable the reading of Zsh rc files in /etc"
@@ -24,14 +29,15 @@ class Zsh < Formula
   depends_on "homebrew/dupes/ncurses"
 
   patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1407905/raw/f7b16c21df602c2b1b866b78939206c00ca8511d/zsh-ambiguous-width-cjk.patch"
-    sha256 "a0a903033a2a9c2f7da8d72713ece9721f187036e35568df824132360b8ca5ca"
+    url "https://gist.githubusercontent.com/waltarix/1407905/raw/9793af98626677a1760ccd75e7254a9f7f8e1095/zsh-ambiguous-width-cjk.patch"
+    sha256 "30358bfae7c379a8afdbc0c5a31ad28c02f828035863c16803d04f1f204e965d"
   end
 
   def install
     ncurses = Formula["ncurses"]
     ENV.append "LDFLAGS", "-L#{ncurses.lib}"
     ENV.append "CPPFLAGS", "-I#{ncurses.include}"
+    system "Util/preconfig" if build.head?
 
     args = %W[
       --prefix=#{prefix}
@@ -63,11 +69,19 @@ class Zsh < Formula
     inreplace ["Makefile", "Src/Makefile"],
       "$(libdir)/$(tzsh)/$(VERSION)", "$(libdir)"
 
-    system "make", "install"
-    system "make", "install.info"
+    if build.head?
+      # disable target install.man, because the required yodl comes neither with OS X nor Homebrew
+      # also disable install.runhelp and install.info because they would also fail or have no effect
+      system "make", "install.bin", "install.modules", "install.fns"
+    else
+      system "make", "install"
+      system "make", "install.info"
+    end
   end
 
   def caveats; <<-EOS.undent
+    In order to use this build of zsh as your login shell,
+    it must be added to /etc/shells.
     Add the following to your zshrc to access the online help:
       unalias run-help
       autoload run-help
