dbb2330f5f8b83c1e56d4eb709959f99fc2497f4
waltari <gitting.crew@gmail.com>
2016-05-18 03:13:10 +0900

    wcwidth-cjk: fix revision

--- wcwidth-cjk.rb
+++ wcwidth-cjk.rb
@@ -1,14 +1,17 @@
 class WcwidthCjk < Formula
   homepage 'https://github.com/fumiyas/wcwidth-cjk'
-  url 'https://github.com/fumiyas/wcwidth-cjk.git'
+  url 'https://github.com/fumiyas/wcwidth-cjk.git',
+    :revision => 'ac3a9ceb020c7499da0b347ec9918b87b253b7a8'
   version '0.1'
 
   depends_on 'libtool' => :build
   depends_on 'automake' => :build
   depends_on 'autoconf' => :build
 
+  patch :DATA
+
   def install
-    system "autoreconf", "-vfi"
+    system "autoreconf", "-i"
 
     system "./configure", "--prefix=#{prefix}"
 
@@ -16,3 +19,35 @@ class WcwidthCjk < Formula
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/Makefile.am b/Makefile.am
+index 8df9ea6..c3e73d3 100644
+--- a/Makefile.am
++++ b/Makefile.am
+@@ -1,9 +1,9 @@
+ bin_SCRIPTS = wcwidth-cjk
+ CLEANFILES = $(bin_SCRIPTS)
+ 
+-lib_LTLIBRARIES = wcwidth-cjk.la
+-wcwidth_cjk_la_SOURCES = wcwidth.c
+-wcwidth_cjk_la_LDFLAGS = -module -avoid-version
++lib_LTLIBRARIES = libwcwidth-cjk.la
++libwcwidth_cjk_la_SOURCES = wcwidth.c
++libwcwidth_cjk_la_LDFLAGS = -avoid-version
+ 
+ bin_PROGRAMS = wcwidth
+ wcwidth_SOURCES = wcwidth-cmd.c
+diff --git a/wcwidth-cjk.in b/wcwidth-cjk.in
+index 4510abd..09859f1 100755
+--- a/wcwidth-cjk.in
++++ b/wcwidth-cjk.in
+@@ -5,7 +5,7 @@ exec_prefix=@exec_prefix@
+ bindir=@bindir@
+ libdir=@libdir@
+ 
+-wcwidth_cjk_so="$libdir/wcwidth-cjk.@SHARED_LIB_EXT@"
++wcwidth_cjk_so="$libdir/libwcwidth-cjk.@SHARED_LIB_EXT@"
+ 
+ if [ $# -lt 1 ]; then
+   echo "Usage: $0 COMMAND [ARGUMENT ...]" 1>&2
