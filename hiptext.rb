f16be85778332eb38f213a5aa5245fd5b5c5463b
waltari <gitting.crew@gmail.com>
2016-05-18 14:16:11 +0900

    hiptext: compatible with ffmpeg3

--- hiptext.rb
+++ hiptext.rb
@@ -1,17 +1,23 @@
 class Hiptext < Formula
-  homepage 'https://github.com/jart/hiptext'
-  url 'https://github.com/jart/hiptext.git'
-  version '0.1'
+  desc "Turn images into text better than caca/aalib"
+  homepage "https://github.com/jart/hiptext"
+  url "https://github.com/jart/hiptext.git"
+  version "0.1"
 
-  depends_on 'gflags'
-  depends_on 'glog'
-  depends_on 'libpng'
-  depends_on 'jpeg'
-  depends_on 'ffmpeg'
-  depends_on 'freetype'
-  depends_on 'ragel'
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "ffmpeg"
+  depends_on "freetype"
+  depends_on "ragel"
 
   def install
+    inreplace "movie.cc" do |s|
+      s.gsub! "PIX_FMT_RGB24", "AV_PIX_FMT_RGB24"
+      s.gsub! "avcodec_alloc_frame", "av_frame_alloc"
+    end
+
     system "make"
 
     bin.install "hiptext"
