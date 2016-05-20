e6fa2ff3b40240eb330aabfffdb9ccbd00bd52ed
waltari <gitting.crew@gmail.com>
2016-05-21 02:51:04 +0900

    w3m: fix dependency

--- w3m.rb
+++ w3m.rb
@@ -13,6 +13,7 @@ class W3m < Formula
   depends_on "pkg-config" => :build
   depends_on "bdw-gc"
   depends_on "openssl"
+  depends_on "cmigemo"
   depends_on "homebrew/dupes/ncurses"
 
   fails_with :llvm do
