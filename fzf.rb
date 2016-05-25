21d66c8cbbb5e1649c28d14c01bffebce97f4ec5
waltari <gitting.crew@gmail.com>
2016-05-25 10:17:23 +0900

    fzf: 0.12.2

--- fzf.rb
+++ fzf.rb
@@ -3,15 +3,15 @@ require "language/go"
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.12.1.tar.gz"
-  sha256 "ca39422b14001d1fe47afd10fcf692c8f67f93da1963669157bdc93d39d19a28"
+  url "https://github.com/junegunn/fzf/archive/0.12.2.tar.gz"
+  sha256 "f2095c1c076774a9a9913b0517f562205785c8b105570a5a719cf0ac8d604368"
   head "https://github.com/junegunn/fzf.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ddbb1f8f13e94988d6d0f7fd4a89ea27a853b8d47cd924c29b73c18bacf854a5" => :el_capitan
-    sha256 "e80de592f975e1c5dc4b8217a44c72e46dc45ad131ae1ea1360effdad306c950" => :yosemite
-    sha256 "2c50a3688257fa491eac3d0fa9c59b98ce00758cc3365566c4c71c3af3cfa4ae" => :mavericks
+    sha256 "c3dc9a9a3b80d8223b4d28837b91f0530694be19569d5ce4942a86b787e2dc90" => :el_capitan
+    sha256 "06c9dd92f10c1032957c2ca881bd05edf63f36a6e85fb97b5bfe68ab04f7c4b8" => :yosemite
+    sha256 "99fed60f204b4a49425f3fdc613927e9630d0faaeb18a5ea6e44b4943a89369c" => :mavericks
   end
 
   def pour_bottle?
