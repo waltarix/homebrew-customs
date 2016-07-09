27b7fd0241d29064ba87ffdfd99ffd4fab873a81
waltari <gitting.crew@gmail.com>
2016-07-09 11:18:11 +0900

    fzf: 0.13.2

--- fzf.rb
+++ fzf.rb
@@ -3,15 +3,15 @@ require "language/go"
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.12.2.tar.gz"
-  sha256 "f2095c1c076774a9a9913b0517f562205785c8b105570a5a719cf0ac8d604368"
+  url "https://github.com/junegunn/fzf/archive/0.13.2.tar.gz"
+  sha256 "063c2e0e23944acead08e90a33ebce0d5d1c05b168571f56800b3b2ddf7c5ee9"
   head "https://github.com/junegunn/fzf.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c3dc9a9a3b80d8223b4d28837b91f0530694be19569d5ce4942a86b787e2dc90" => :el_capitan
-    sha256 "06c9dd92f10c1032957c2ca881bd05edf63f36a6e85fb97b5bfe68ab04f7c4b8" => :yosemite
-    sha256 "99fed60f204b4a49425f3fdc613927e9630d0faaeb18a5ea6e44b4943a89369c" => :mavericks
+    sha256 "16df1a31d5e30a505ff67729597b0a0bfa6218b8fdc3a7ac6a3bcbe0d11486ed" => :el_capitan
+    sha256 "77aadf34a0f4724dad5e6fcf91c9e863e859e4cf3f9c063c601d1b0bed4d67f4" => :yosemite
+    sha256 "b2c75a7bae7781d33624e442561ff9203212e202cfa0c6694d8ff921db78ba2e" => :mavericks
   end
 
   def pour_bottle?
@@ -68,33 +68,33 @@ end
 
 __END__
 diff --git a/src/terminal.go b/src/terminal.go
-index 764459f..9446d79 100644
+index ce10ee7..602aad7 100644
 --- a/src/terminal.go
 +++ b/src/terminal.go
-@@ -402,7 +402,7 @@ func (t *Terminal) placeCursor() {
+@@ -504,7 +504,7 @@ func (t *Terminal) placeCursor() {
  func (t *Terminal) printPrompt() {
  	t.move(0, 0, true)
- 	C.CPrint(C.ColPrompt, true, t.prompt)
--	C.CPrint(C.ColNormal, true, string(t.input))
-+	C.CPrint(C.ColNormal, false, string(t.input))
+ 	t.window.CPrint(C.ColPrompt, true, t.prompt)
+-	t.window.CPrint(C.ColNormal, true, string(t.input))
++	t.window.CPrint(C.ColNormal, false, string(t.input))
  }
  
  func (t *Terminal) printInfo() {
-@@ -497,7 +497,7 @@ func (t *Terminal) printItem(item *Item, current bool) {
+@@ -604,7 +604,7 @@ func (t *Terminal) printItem(item *Item, i int, current bool) {
  		} else {
- 			C.CPrint(C.ColCurrent, true, " ")
+ 			t.window.CPrint(C.ColCurrent, true, " ")
  		}
 -		t.printHighlighted(item, true, C.ColCurrent, C.ColCurrentMatch, true)
 +		t.printHighlighted(item, false, C.ColCurrent, C.ColCurrentMatch, true)
  	} else {
- 		C.CPrint(C.ColCursor, true, " ")
  		if selected {
-@@ -556,7 +556,7 @@ func (t *Terminal) printHighlighted(item *Item, bold bool, col1 int, col2 int, c
+ 			t.window.CPrint(C.ColSelected, true, ">")
+@@ -660,7 +660,7 @@ func (t *Terminal) printHighlighted(item *Item, bold bool, col1 int, col2 int, c
  	// Overflow
  	text := make([]rune, len(item.text))
  	copy(text, item.text)
 -	offsets := item.colorOffsets(col2, bold, current)
 +	offsets := item.colorOffsets(col2, true, current)
- 	maxWidth := C.MaxX() - 3 - t.marginInt[1] - t.marginInt[3]
+ 	maxWidth := t.window.Width - 3
+ 	maxe = util.Constrain(maxe+util.Min(maxWidth/2-2, t.hscrollOff), 0, len(text))
  	fullWidth := displayWidth(text)
- 	if fullWidth > maxWidth {
