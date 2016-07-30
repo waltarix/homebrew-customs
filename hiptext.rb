90f51b6e4cc6e2e5f2c7274e48f4b8e064cf85eb
waltari <gitting.crew@gmail.com>
2016-07-30 12:51:56 +0900

    hiptext: 0.2

--- hiptext.rb
+++ hiptext.rb
@@ -1,8 +1,8 @@
 class Hiptext < Formula
   desc "Turn images into text better than caca/aalib"
   homepage "https://github.com/jart/hiptext"
-  url "https://github.com/jart/hiptext.git"
-  version "0.1"
+  url "https://github.com/jart/hiptext/releases/download/0.2/hiptext-0.2.tar.gz"
+  sha256 "7f2217dec8775b445be6745f7bd439c24ce99c4316a9faf657bee7b42bc72e8f"
 
   depends_on "gflags"
   depends_on "glog"
@@ -11,16 +11,21 @@ class Hiptext < Formula
   depends_on "ffmpeg"
   depends_on "freetype"
   depends_on "ragel"
+  depends_on "pkg-config" => :build
 
   def install
-    inreplace "movie.cc" do |s|
+    inreplace "src/movie.cc" do |s|
       s.gsub! "PIX_FMT_RGB24", "AV_PIX_FMT_RGB24"
       s.gsub! "avcodec_alloc_frame", "av_frame_alloc"
     end
 
-    system "make"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["ffmpeg"].opt_lib}/pkgconfig"
+    ENV["LIBGFLAGS_CFLAGS"] = "-I#{Formula["gflags"].opt_include}"
+    ENV["LIBGFLAGS_LIBS"] = "-L#{Formula["gflags"].opt_lib} -lgflags"
 
-    bin.install "hiptext"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
   end
 
   test do
