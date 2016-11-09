41d707637f06409125cdc4b89f2b61e0c828a21f
waltari <gitting.crew@gmail.com>
2016-11-09 18:17:11 +0900

    ripgrep: 0.2.8

--- ripgrep.rb
+++ ripgrep.rb
@@ -1,15 +1,15 @@
 class Ripgrep < Formula
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
-  url "https://github.com/BurntSushi/ripgrep/archive/0.2.3.tar.gz"
-  sha256 "a88531558d2023df76190ea2e52bee50d739eabece8a57df29abbad0c6bdb917"
+  url "https://github.com/BurntSushi/ripgrep/archive/0.2.8.tar.gz"
+  sha256 "0a2a41439572feed787b4e1e3d07d03b8ee6a7dd2dc0eda969eb7d67aed91e52"
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ff0f826ab6e081110a6877dd2afc76381ae282f5984a81bb778858018db4831a" => :sierra
-    sha256 "584ef400386f4cc6b768fc2efe2a3607865744e5d8751c1e2f8e9bb81d7fd806" => :el_capitan
-    sha256 "d6466efbe4564f58f4615a544641a53044fa2d6b6360cb1282167fdb19f62c83" => :yosemite
+    sha256 "d6520d3c3105bcb3bdc095ac9d0ebff5769fed9b4d9f288d003c1378e3b9c518" => :sierra
+    sha256 "bbda30924d09ae3b9c9cdd3fb49ad4b9e5b7f2daa67b7b9af7acb7cd0a579be1" => :el_capitan
+    sha256 "76ae673cd5d24e9ae3a4c0c4e7c8468a85680f02dd25c8b9b43c15f694ad819c" => :yosemite
   end
 
   depends_on "rust" => :build
