3fef65ed123028944de2469dc9ea473398c9c86a
waltari <gitting.crew@gmail.com>
2016-12-17 14:48:20 +0900

    zsh: 5.3

--- zsh.rb
+++ zsh.rb
@@ -1,15 +1,22 @@
 class Zsh < Formula
   desc "UNIX shell (command interpreter)"
-  homepage "http://www.zsh.org/"
-  url "https://downloads.sourceforge.net/project/zsh/zsh/5.2/zsh-5.2.tar.gz"
-  mirror "http://www.zsh.org/pub/zsh-5.2.tar.gz"
-  sha256 "fa924c534c6633c219dcffdcd7da9399dabfb63347f88ce6ddcd5bb441215937"
+  homepage "https://www.zsh.org/"
+
+  stable do
+    url "https://www.zsh.org/pub/zsh-5.3.tar.gz"
+    mirror "https://downloads.sourceforge.net/project/zsh/zsh/5.3/zsh-5.3.tar.gz"
+    sha256 "1da273fa96041b395ee9f628d14f2aff08f9a62e98423a990218e2ea037b9a6d"
+
+    # We cannot build HTML doc on HEAD, because yodl which is required for
+    # building zsh.texi is not available.
+    option "with-texi2html", "Build HTML documentation"
+    depends_on "texi2html" => [:build, :optional]
+  end
 
   bottle do
-    revision 1
-    sha256 "9280222796df420ebd732dae9b7a3b31db131c094ba57da97d10c8c77761ef98" => :el_capitan
-    sha256 "dc37a7a9b7b5ea54b94efb127f1c72262665ed1886054f32bb7770ee98f43d2a" => :yosemite
-    sha256 "4fc05803a2894bf578be48e5c2451ffa1fc0531a0e9fd202821166a51db0a248" => :mavericks
+    sha256 "51270c500dbf6bfc81e1fa1b19ce8b147adbc717ef0c0fe2ed6f41031b056236" => :sierra
+    sha256 "bb54d4104228326c7661ef9a6014b1f2ee00e9f5ae651551dfd56fed700e679d" => :el_capitan
+    sha256 "d5fa17cb9ebf4c9115f66b6b8fb7ef3c535bcf2eb372668936e54591f732fc86" => :yosemite
   end
 
   def pour_bottle?
@@ -19,9 +26,12 @@ class Zsh < Formula
   head do
     url "git://git.code.sf.net/p/zsh/code"
     depends_on "autoconf" => :build
+
+    option "with-unicode9", "Build with Unicode 9 character width support"
   end
 
   option "without-etcdir", "Disable the reading of Zsh rc files in /etc"
+  option "with-unicode9", "Build with Unicode 9 character width support"
 
   deprecated_option "disable-etcdir" => "without-etcdir"
 
@@ -57,6 +67,8 @@ class Zsh < Formula
       --with-term-lib=ncursesw
     ]
 
+    args << "--enable-unicode9" if build.with? "unicode9"
+
     if build.without? "etcdir"
       args << "--disable-etcdir"
     else
@@ -70,22 +82,19 @@ class Zsh < Formula
       "$(libdir)/$(tzsh)/$(VERSION)", "$(libdir)"
 
     if build.head?
-      # disable target install.man, because the required yodl comes neither with OS X nor Homebrew
+      # disable target install.man, because the required yodl comes neither with macOS nor Homebrew
       # also disable install.runhelp and install.info because they would also fail or have no effect
       system "make", "install.bin", "install.modules", "install.fns"
     else
       system "make", "install"
       system "make", "install.info"
+      system "make", "install.html" if build.with? "texi2html"
     end
   end
 
   def caveats; <<-EOS.undent
     In order to use this build of zsh as your login shell,
     it must be added to /etc/shells.
-    Add the following to your zshrc to access the online help:
-      unalias run-help
-      autoload run-help
-      HELPDIR=#{HOMEBREW_PREFIX}/share/zsh/help
     EOS
   end
 
