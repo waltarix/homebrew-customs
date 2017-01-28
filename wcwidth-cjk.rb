66ef59de0649339a883a8dd40a72e4da5799e296
waltari <gitting.crew@gmail.com>
2017-01-28 22:42:35 +0900

    wcwidth-cjk: treat U+2580..U+258F's (half-block family) width as 1

--- wcwidth-cjk.rb
+++ wcwidth-cjk.rb
@@ -4,6 +4,7 @@ class WcwidthCjk < Formula
   url "https://github.com/fumiyas/wcwidth-cjk.git",
     :revision => "ac3a9ceb020c7499da0b347ec9918b87b253b7a8"
   version "0.1"
+  revision 1
 
   depends_on "libtool" => :build
   depends_on "automake" => :build
@@ -25,6 +26,7 @@ class WcwidthCjk < Formula
     ENV["LANG"] = "en_US.UTF-8"
     assert_match /^1\s/, shell_output("#{bin}/wcwidth ☆")
     assert_match /^2\s/, shell_output("#{bin}/wcwidth-cjk #{bin}/wcwidth ☆")
+    assert_match /^1\s/, shell_output("#{bin}/wcwidth-cjk #{bin}/wcwidth ▀")
   end
 end
 
@@ -59,3 +61,16 @@ index 4510abd..09859f1 100755
  
  if [ $# -lt 1 ]; then
    echo "Usage: $0 COMMAND [ARGUMENT ...]" 1>&2
+diff --git a/wcwidth.c b/wcwidth.c
+index c5c62bf..865012a 100644
+--- a/wcwidth.c
++++ b/wcwidth.c
+@@ -257,7 +257,7 @@ int wcwidth_cjk(wchar_t ucs)
+     { 0x2282, 0x2283 }, { 0x2286, 0x2287 }, { 0x2295, 0x2295 },
+     { 0x2299, 0x2299 }, { 0x22a5, 0x22a5 }, { 0x22bf, 0x22bf },
+     { 0x2312, 0x2312 }, { 0x2460, 0x24e9 }, { 0x24eb, 0x254b },
+-    { 0x2550, 0x2573 }, { 0x2580, 0x258f }, { 0x2592, 0x2595 },
++    { 0x2550, 0x2573 }, { 0x2592, 0x2595 },
+     { 0x25a0, 0x25a1 }, { 0x25a3, 0x25a9 }, { 0x25b2, 0x25b3 },
+     { 0x25b6, 0x25b7 }, { 0x25bc, 0x25bd }, { 0x25c0, 0x25c1 },
+     { 0x25c6, 0x25c8 }, { 0x25cb, 0x25cb }, { 0x25ce, 0x25d1 },
