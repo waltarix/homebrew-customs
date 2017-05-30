47dd7f05ac44ae071a3fbcc8a52276e9cb27ba88
waltari <gitting.crew@gmail.com>
2017-05-30 15:25:17 +0900

    unzip-iconv: use Debian patchset instead of AUR

--- unzip-iconv.rb
+++ unzip-iconv.rb
@@ -4,43 +4,61 @@ class UnzipIconv < Formula
   url "https://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz"
   version "6.0"
   sha256 "036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
-  revision 2
+  revision 3
 
-  [
-    { url: "https://bugzilla.redhat.com/attachment.cgi?id=990132",
-      sha256: "1333a0d14e8f59c3a114764bf008ae489d386fd561130a60c1c7f2f4c9386b9b" },
-    { url: "https://bugzilla.redhat.com/attachment.cgi?id=969621",
-      sha256: "1a1390390402e674ef7b143705ade0e9aa082131bb9686e95fb7985310def885",
-      strip: 0 },
-    { url: "https://bugzilla.redhat.com/attachment.cgi?id=969625",
-      sha256: "04e72b17f46bc320fff871f2b99f48dca17befceac83a7caca719bc20dae6268",
-      strip: 0 },
-    { url: "https://bugzilla.redhat.com/attachment.cgi?id=990649",
-      sha256: "c9a863e570bdaf2637c43bf1bba3d97808a1b0504d85418f6a8550ac286788f2" },
-    { url: "https://projects.archlinux.org/svntogit/packages.git/plain/trunk/overflow-fsize.patch?h=packages/unzip&id=15e9a8c67463aaf62a718c6e74b1c972de654346",
-      sha256: "e2a10fa494c39fb3f311d0f2d7db775bdecbc8d5b9d298c7bd035ace1f1713d5" },
-    { url: "http://www.conostix.com/pub/adv/06-unzip60-alt-iconv-utf8_CVE-2015-1315.patch",
-      sha256: "e64c9ddb38c2e7d08bdb80c597f32ee960e18fbe8cb982e444b1ece03ac95cec" },
-    { url: "https://bugzilla.redhat.com/attachment.cgi?id=1073339",
-      sha256: "fc6d36383ba9ca35e888912e5f8fd5178ae7e987c78a25816c2c0b60c3b377ba" },
-    { url: "https://aur.archlinux.org/cgit/aur.git/snapshot/unzip-iconv.tar.gz",
-      sha256: "69fca31a1df97befb3d2e047b034d2308089c30fdd57e5eda1f03833888b6759",
-      apply: ["CVE-2015-7696+CVE-2015-7697_pt2.patch"] },
-  ].each do |resource|
-    patch "p#{resource[:strip] || 1}".to_sym do
-      url resource[:url]
-      sha256 resource[:sha256]
-      apply *Array(resource[:apply]) if resource[:apply]
-    end
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "72cf9820bc8fe8c008bdb1cf7b231afbb0bc6b48511f4d40e5e4840f5bb5df65" => :sierra
+    sha256 "8a2bfa62e728c9a9bc44d7acb9f34698e599b47f8200c45290b62502c682a6ec" => :el_capitan
+    sha256 "3c69150f5a9ad6d1d1737eb17c06315dd7d0bc02b9c1a11e02ecb281c1e5f37f" => :yosemite
+  end
+
+  # Upstream is unmaintained so we use the Debian patchset:
+  # https://packages.debian.org/sid/unzip
+  patch do
+    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/unzip/unzip_6.0-21.debian.tar.xz"
+    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/u/unzip/unzip_6.0-21.debian.tar.xz"
+    sha256 "8accd9d214630a366476437a3ec1842f2e057fdce16042a7b19ee569c33490a3"
+    apply %w[
+      patches/01-manpages-in-section-1-not-in-section-1l.patch
+      patches/02-this-is-debian-unzip.patch
+      patches/03-include-unistd-for-kfreebsd.patch
+      patches/04-handle-pkware-verification-bit.patch
+      patches/05-fix-uid-gid-handling.patch
+      patches/06-initialize-the-symlink-flag.patch
+      patches/07-increase-size-of-cfactorstr.patch
+      patches/08-allow-greater-hostver-values.patch
+      patches/09-cve-2014-8139-crc-overflow.patch
+      patches/10-cve-2014-8140-test-compr-eb.patch
+      patches/11-cve-2014-8141-getzip64data.patch
+      patches/12-cve-2014-9636-test-compr-eb.patch
+      patches/13-remove-build-date.patch
+      patches/14-cve-2015-7696.patch
+      patches/15-cve-2015-7697.patch
+      patches/16-fix-integer-underflow-csiz-decrypted.patch
+      patches/17-restore-unix-timestamps-accurately.patch
+      patches/18-cve-2014-9913-unzip-buffer-overflow.patch
+      patches/19-cve-2016-9844-zipinfo-buffer-overflow.patch
+    ]
+  end
+
+  patch do
+    url "http://www.conostix.com/pub/adv/06-unzip60-alt-iconv-utf8_CVE-2015-1315.patch"
+    sha256 "e64c9ddb38c2e7d08bdb80c597f32ee960e18fbe8cb982e444b1ece03ac95cec"
+  end
+
+  def pour_bottle?
+    false
   end
 
   def install
-    system "make", "LFLAGS1=-liconv", "-f", "unix/Makefile",
+    system "make", "-f", "unix/Makefile",
       "CC=#{ENV.cc}",
       "LOC=-DLARGE_FILE_SUPPORT",
       "D_USE_BZ2=-DUSE_BZIP2",
       "L_BZ2=-lbz2",
-      "macosx"
+      "macosx",
+      "LFLAGS1=-liconv"
     system "make", "prefix=#{prefix}", "MANDIR=#{man1}", "install"
 
     [bin, man1].each do |path|
@@ -50,12 +68,28 @@ class UnzipIconv < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
-    All commands have been installed with the prefix 'i'.
-    EOS
+  def caveats
+    "All commands have been installed with the prefix 'i'."
   end
 
   test do
-    system "#{bin}/iunzip", "--help"
+    (testpath/"test1").write "Hello!"
+    (testpath/"test2").write "Bonjour!"
+    (testpath/"test3").write "Hej!"
+
+    system "/usr/bin/zip", "test.zip", "test1", "test2", "test3"
+    %w[test1 test2 test3].each do |f|
+      rm f
+      refute_predicate testpath/f, :exist?, "Text files should have been removed!"
+    end
+
+    system bin/"iunzip", "test.zip"
+    %w[test1 test2 test3].each do |f|
+      assert_predicate testpath/f, :exist?, "Failure unzipping test.zip!"
+    end
+
+    assert_match "Hello!", File.read(testpath/"test1")
+    assert_match "Bonjour!", File.read(testpath/"test2")
+    assert_match "Hej!", File.read(testpath/"test3")
   end
 end
