5bb0013e094f928562360e6775c1c78acad6d34f
waltari <gitting.crew@gmail.com>
2017-07-10 10:24:34 +0900

    neovim: 0.2.0_1

--- neovim.rb
+++ neovim.rb
@@ -1,110 +1,87 @@
 class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
-  homepage "https://neovim.io"
-  url "https://github.com/neovim/neovim/archive/v0.2.0.tar.gz"
-  sha256 "72e263f9d23fe60403d53a52d4c95026b0be428c1b9c02b80ab55166ea3f62b5"
-  head "https://github.com/neovim/neovim.git", :shallow => false
+  homepage "https://neovim.io/"
+  revision 1
 
-  option "with-dev", "Compile a Dev build. Enables debug information, logging,
-        and optimizations that don't interfere with debugging."
+  stable do
+    url "https://github.com/neovim/neovim/archive/v0.2.0.tar.gz"
+    sha256 "72e263f9d23fe60403d53a52d4c95026b0be428c1b9c02b80ab55166ea3f62b5"
+
+    depends_on "luajit" => :build
+
+    # Remove for > 0.2.0
+    # Upstream commit from 7 Jul 2017 "Prefer the static jemalloc library by default on OSX"
+    # See https://github.com/neovim/neovim/pull/6979
+    patch do
+      url "https://github.com/neovim/neovim/commit/35fad15c8907f741ce21779393e4377de753e4f9.patch?full_index=1"
+      sha256 "b156fdc92a3850eca3047620087f25a021800b729a3c30fd0164404b2ff7848b"
+    end
+  end
+
+  def pour_bottle?
+    false
+  end
+
+  bottle do
+    sha256 "6d0cbbaadd947b428f29fb28ba0f492f2d84a71d981ad22cf00032036118ddf9" => :sierra
+    sha256 "fdb74c70048d2c9232525ab652464d9818d0134442c855168f399ab2dd9504a7" => :el_capitan
+    sha256 "f563c067954a4c5f98ad2b01ee3219e5d1666f7000917474186127122cf5cc76" => :yosemite
+  end
+
+  head do
+    url "https://github.com/neovim/neovim.git"
+
+    depends_on "luajit"
+  end
 
   depends_on "cmake" => :build
-  depends_on "libtool" => :build
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
+  depends_on "lua@5.1" => :build
   depends_on "pkg-config" => :build
-  depends_on "jemalloc" => :recommended
+  depends_on "gettext"
+  depends_on "jemalloc"
+  depends_on "libtermkey"
   depends_on "libuv"
+  depends_on "libvterm"
   depends_on "msgpack"
   depends_on "unibilium"
-  depends_on "libtermkey"
-  depends_on "libvterm"
-  depends_on "gettext"
-  depends_on "gperf" => :recommended if OS.linux?
-  depends_on "unzip" => :recommended if OS.linux?
-  depends_on :python => :recommended if OS.mac? && MacOS.version <= :snow_leopard
+  depends_on :python if MacOS.version <= :snow_leopard
 
-  resource "luv" do
-    url "https://github.com/luvit/luv/archive/146f1ce4c08c3b67f604c9ee1e124b1cf5c15cf3.tar.gz"
-    sha256 "3d537f8eb9fa5adb146a083eae22af886aee324ec268e2aa0fa75f2f1c52ca7a"
+  resource "lpeg" do
+    url "https://luarocks.org/manifests/gvvaughan/lpeg-1.0.1-1.src.rock", :using => :nounzip
+    sha256 "149be31e0155c4694f77ea7264d9b398dd134eca0d00ff03358d91a6cfb2ea9d"
   end
 
-  resource "luajit" do
-    url "https://raw.githubusercontent.com/neovim/deps/master/opt/LuaJIT-2.0.4.tar.gz"
-    sha256 "620fa4eb12375021bef6e4f237cbd2dd5d49e56beb414bee052c746beef1807d"
+  resource "mpack" do
+    url "https://luarocks.org/manifests/tarruda/mpack-1.0.6-0.src.rock", :using => :nounzip
+    sha256 "9068d9d3f407c72a7ea18bc270b0fa90aad60a2f3099fa23d5902dd71ea4cd5f"
   end
-
-  resource "luarocks" do
-    url "https://github.com/keplerproject/luarocks/archive/5d8a16526573b36d5b22aa74866120c998466697.tar.gz"
-    sha256 "cae709111c5701235770047dfd7169f66b82ae1c7b9b79207f9df0afb722bfd9"
-  end
-
-  patch :DATA
 
   def install
-    ENV["HOME"] = buildpath
-
     resources.each do |r|
       r.stage(buildpath/"deps-build/build/src/#{r.name}")
     end
 
+    ENV.prepend_path "LUA_PATH", "#{buildpath}/deps-build/share/lua/5.1/?.lua"
+    ENV.prepend_path "LUA_CPATH", "#{buildpath}/deps-build/lib/lua/5.1/?.so"
+
     cd "deps-build" do
-      ohai "Building third-party dependencies."
-      system "cmake", "../third-party", "-DUSE_BUNDLED_BUSTED=OFF",
-             "-DUSE_BUNDLED_GPERF=OFF",
-             "-DUSE_BUNDLED_LIBUV=OFF",
-             "-DUSE_BUNDLED_MSGPACK=OFF",
-             "-DUSE_BUNDLED_UNIBILIUM=OFF",
-             "-DUSE_BUNDLED_LIBTERMKEY=OFF",
-             "-DUSE_BUNDLED_LIBVTERM=OFF",
-             "-DUSE_BUNDLED_JEMALLOC=OFF",
-             "-DUSE_EXISTING_SRC_DIR=ON", *std_cmake_args
-      system "make", "VERBOSE=1"
+      system "luarocks-5.1", "build", "build/src/lpeg/lpeg-1.0.1-1.src.rock", "--tree=."
+      system "luarocks-5.1", "build", "build/src/mpack/mpack-1.0.6-0.src.rock", "--tree=."
+      system "cmake", "../third-party", "-DUSE_BUNDLED=OFF", *std_cmake_args
+      system "make"
     end
 
     mkdir "build" do
-      ohai "Building Neovim."
-
-      build_type = build.with?("dev") ? "Dev" : "RelWithDebInfo"
-      cmake_args = std_cmake_args + ["-DDEPS_PREFIX=../deps-build/usr",
-                                     "-DCMAKE_BUILD_TYPE=#{build_type}"]
-
-      cmake_args += ["-DENABLE_JEMALLOC=OFF"] if build.without?("jemalloc")
-
-      if OS.mac?
-        cmake_args += ["-DJEMALLOC_LIBRARY=#{Formula["jemalloc"].opt_lib}/libjemalloc.a"] if build.with?("jemalloc")
-        cmake_args += ["-DMSGPACK_LIBRARY=#{Formula["msgpack"].opt_lib}/libmsgpackc.2.dylib"]
-        cmake_args += ["-DIconv_INCLUDE_DIRS:PATH=/usr/include",
-                       "-DIconv_LIBRARIES:PATH=/usr/lib/libiconv.dylib"]
-      end
-
-      system "cmake", "..", *cmake_args
-      system "make", "VERBOSE=1", "install"
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
     end
-  end
-
-  def caveats; <<-EOS.undent
-      To run Neovim, use the "nvim" command (not "neovim"):
-          nvim
-
-      After installing or upgrading, run the "CheckHealth" command:
-          :CheckHealth
-
-      See ':help nvim-from-vim' for information about how to use
-      your existing Vim configuration with Neovim.
-
-      Breaking changes (if any) are documented at:
-          https://github.com/neovim/neovim/wiki/Following-HEAD
-
-      For other questions:
-          https://github.com/neovim/neovim/wiki/FAQ
-    EOS
   end
 
   test do
     (testpath/"test.txt").write("Hello World from Vim!!")
-    system bin/"nvim", "--headless", "-i", "NONE", "-u", "NONE", "+s/Vim/Neovim/g", "+wq", "test.txt"
-    assert_equal "Hello World from Neovim!!", File.read("test.txt").strip
+    system bin/"nvim", "--headless", "-i", "NONE", "-u", "NONE",
+                       "+s/Vim/Neovim/g", "+wq", "test.txt"
+    assert_equal "Hello World from Neovim!!", (testpath/"test.txt").read.chomp
   end
 end
 
