83fa0136a1dcd000b4e6ee3e1e184900ff592fde
waltari <gitting.crew@gmail.com>
2017-11-11 11:02:53 +0900

    et: 4.1.1

--- et.rb
+++ et.rb
@@ -1,9 +1,9 @@
 class Et < Formula
   desc "Remote terminal with IP roaming"
   homepage "https://mistertea.github.io/EternalTCP/"
-  url "https://github.com/MisterTea/EternalTCP/archive/et-v4.1.0.tar.gz"
-  sha256 "a6a99e425da3354c1845c5b8eb1644ffc0ee96b8644f2b905191e7102703a18b"
-  revision 3
+  url "https://github.com/MisterTea/EternalTCP/archive/et-v4.1.1.tar.gz"
+  version "4.1.1"
+  sha256 "db62c323d978509eae0e5c3427299f7e38287b2595a52f34db9d45a375493c67"
 
   depends_on "cmake" => :build
 
@@ -69,22 +69,24 @@ end
 
 __END__
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 99a53eb..0dc716a 100644
+index b03aab6..1a6b552 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -28,7 +28,6 @@ find_package(Glog REQUIRED)
+@@ -33,9 +33,6 @@ find_package(Glog REQUIRED)
  find_package(GFlags REQUIRED)
  find_package(Protobuf REQUIRED)
  find_package(Sodium REQUIRED)
+-IF(LINUX)
 -find_package(SELinux)
+-ENDIF()
  find_package(UTempter)
  
  IF(SELINUX_FOUND)
 diff --git a/README.md b/README.md
-index 0f7e47a..1b97298 100644
+index 8ecb958..3ec5fcf 100644
 --- a/README.md
 +++ b/README.md
-@@ -68,14 +68,14 @@ You are ready to start using ET!
+@@ -68,17 +68,17 @@ You are ready to start using ET!
  
  ET uses ssh for handshaking and encryption, so you must be able to ssh into the machine from the client. Make sure that you can `ssh user@hostname`.
  
@@ -92,24 +94,27 @@ index 0f7e47a..1b97298 100644
 +ET uses TCP, so you need an open port on your server. By default, it uses 25253.
  
  
--Once you have an open port, the syntax is shown below. You can specify a jumphost and the port et is running on jumphost using `--jumphost` and `--jport`. If no `--port/--jport` is given, et will try to connect to default port 2022.
-+Once you have an open port, the syntax is shown below. You can specify a jumphost and the port et is running on jumphost using `--jumphost` and `--jport`. If no `--port/--jport` is given, et will try to connect to default port 25253.
+ Once you have an open port, the syntax is similar to ssh. Username is default to the current username starting the et process, use `-u` or `user@` to specify a different if necessary.
  ```
--et --host hostname (etserver running on port 2022)
-+et --host hostname (etserver running on port 25253)
- et --host hostname --port 8000
--et --host hostname --jumphost (etserver running on port 2022 on both hostname and jumphost)
-+et --host hostname --jumphost (etserver running on port 25253 on both hostname and jumphost)
- et --host hostname --port 8888 --jumphost jump_hostname --jport 9999
+-et hostname (etserver running on default port 2022, username is the same as current)
++et hostname (etserver running on default port 25253, username is the same as current)
+ et user@hostname:8000 (etserver running on port 8000, different user)
  ```
- Additional arguments that et accept are port forwarding pairs with option `--portforward="18000:8000, 18001-18003:8001-8003"`, a command to run immediately after the connection is setup through `--command`. Username is default to the current username starting the et process, use `--user` to specify a different if necessary.
-@@ -98,7 +98,7 @@ Host dev
+-You can specify a jumphost and the port et is running on jumphost using `-jumphost` and `-jport`. If no `-jport` is given, et will try to connect to default port 2022.
++You can specify a jumphost and the port et is running on jumphost using `-jumphost` and `-jport`. If no `-jport` is given, et will try to connect to default port 25253.
+ ```
+-et hostname -jumphost jump_hostname (etserver running on port 2022 on both hostname and jumphost)
++et hostname -jumphost jump_hostname (etserver running on port 25253 on both hostname and jumphost)
+ et hostname:8888 -jumphost jump_hostname -jport 9999
+ ```
+ Additional arguments that et accept are port forwarding pairs with option `-t="18000:8000, 18001-18003:8001-8003"`, a command to run immediately after the connection is setup through `-c`.
+@@ -101,7 +101,7 @@ Host dev
  With the ssh config file set as above, you can simply call et with
  
  ```
--et --host dev (etserver running on port 2022 on both hostname and jumphost)
-+et --host dev (etserver running on port 25253 on both hostname and jumphost)
- et --host dev --port 8000 --jport 9000 (etserver not running on default port)
+-et dev (etserver running on port 2022 on both hostname and jumphost)
++et dev (etserver running on port 25253 on both hostname and jumphost)
+ et dev:8000 -jport 9000 (etserver running on port 9000 on jumphost)
  ```
  
 diff --git a/etc/et.cfg b/etc/et.cfg
@@ -122,32 +127,53 @@ index 403ab02..cc4b8c4 100644
  [Networking]
 -port = 2022
 +port = 25253
+diff --git a/replace_default_port.zsh b/replace_default_port.zsh
+new file mode 100755
+index 0000000..ef3e7f4
+--- /dev/null
++++ b/replace_default_port.zsh
+@@ -0,0 +1,17 @@
++#!/usr/bin/env zsh
++
++local default_port=25253
++local new_port=$1
++
++[[ -z $new_port ]] && { echo 'invalid arguments.'; return 1 }
++
++function replace_port {
++	perl -i -pe "s/\b${default_port}\b/${new_port}/g" $1
++}
++
++local -a target_files
++target_files=($(git grep --name-only -w 25253))
++
++for target_file ($target_files); do
++	replace_port $target_file
++done
 diff --git a/src/UnixSocketHandler.cpp b/src/UnixSocketHandler.cpp
-index b9acde0..17bfd2e 100644
+index b9acde0..e9b8f37 100644
 --- a/src/UnixSocketHandler.cpp
 +++ b/src/UnixSocketHandler.cpp
-@@ -98,6 +98,9 @@ int UnixSocketHandler::connect(const std::string &hostname, int port) {
+@@ -70,7 +70,7 @@ int UnixSocketHandler::connect(const std::string &hostname, int port) {
+   addrinfo *p = NULL;
+   addrinfo hints;
+   memset(&hints, 0, sizeof(addrinfo));
+-  hints.ai_family = AF_UNSPEC;
++  hints.ai_family = AF_INET;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_flags = (AI_CANONNAME | AI_V4MAPPED | AI_ADDRCONFIG | AI_ALL);
+   std::string portname = std::to_string(port);
+@@ -204,7 +204,7 @@ void UnixSocketHandler::createServerSockets(int port) {
+   int rc;
  
-   // loop through all the results and connect to the first we can
-   for (p = results; p != NULL; p = p->ai_next) {
-+    if (p->ai_family == AF_INET6) {
-+      continue;
-+    }
-     if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-       LOG(INFO) << "Error creating socket: " << errno << " " << strerror(errno);
-       continue;
-@@ -219,6 +222,9 @@ void UnixSocketHandler::createServerSockets(int port) {
-   set<int> serverSockets;
-   // loop through all the results and bind to the first we can
-   for (p = servinfo; p != NULL; p = p->ai_next) {
-+    if (p->ai_family == AF_INET6) {
-+      continue;
-+    }
-     int sockfd;
-     if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-       LOG(INFO) << "Error creating socket " << p->ai_family << "/"
+   memset(&hints, 0, sizeof hints);
+-  hints.ai_family = AF_UNSPEC;
++  hints.ai_family = AF_INET;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_flags = AI_PASSIVE;  // use my IP address
+ 
 diff --git a/terminal/SshSetupHandler.cpp b/terminal/SshSetupHandler.cpp
-index 3a482dd..f61c5c5 100644
+index 5056e63..1a73547 100644
 --- a/terminal/SshSetupHandler.cpp
 +++ b/terminal/SshSetupHandler.cpp
 @@ -61,11 +61,11 @@ string SshSetupHandler::SetupSsh(string user, string host, int port,
@@ -164,7 +190,7 @@ index 3a482dd..f61c5c5 100644
              (SSH_SCRIPT_DST).c_str(), NULL);
      }
  
-@@ -124,7 +124,7 @@ string SshSetupHandler::SetupSsh(string user, string host, int port,
+@@ -123,7 +123,7 @@ string SshSetupHandler::SetupSsh(string user, string host, int port,
          cmdoptions +=
              " --jump --dsthost=" + host + " --dstport=" + to_string(port);
          string SSH_SCRIPT_JUMP = SSH_SCRIPT_PREFIX + cmdoptions + ";true";
@@ -174,12 +200,12 @@ index 3a482dd..f61c5c5 100644
        } else {
          close(link_jump[1]);
 diff --git a/terminal/TerminalClient.cpp b/terminal/TerminalClient.cpp
-index 22c1e00..63cddb1 100644
+index e71dfe7..1f6471b 100644
 --- a/terminal/TerminalClient.cpp
 +++ b/terminal/TerminalClient.cpp
 @@ -33,14 +33,14 @@ termios terminal_backup;
  
- DEFINE_string(user, "", "username to login");
+ DEFINE_string(u, "", "username to login");
  DEFINE_string(host, "localhost", "host to join");
 -DEFINE_int32(port, 2022, "port to connect on");
 +DEFINE_int32(port, 25253, "port to connect on");
@@ -196,18 +222,18 @@ index 22c1e00..63cddb1 100644
    string id = "", passkey = "";
 @@ -121,7 +121,7 @@ int main(int argc, char** argv) {
        cout << "et (options) [user@]hostname[:port]\n"
- "Options:\n"
- "-h Basic usage\n"
--"-p Port for etserver to run on.  Default: 2022\n"
-+"-p Port for etserver to run on.  Default: 25253\n"
- "-u Username to connect to ssh & ET\n"
- "-v=9 verbose log files\n"
- "-c Initial command to execute upon connecting\n"
+               "Options:\n"
+               "-h Basic usage\n"
+-              "-p Port for etserver to run on.  Default: 2022\n"
++              "-p Port for etserver to run on.  Default: 25253\n"
+               "-u Username to connect to ssh & ET\n"
+               "-v=9 verbose log files\n"
+               "-c Initial command to execute upon connecting\n"
 diff --git a/terminal/TerminalServer.cpp b/terminal/TerminalServer.cpp
-index 25dec94..154edd6 100644
+index f016dd4..0f2cd32 100644
 --- a/terminal/TerminalServer.cpp
 +++ b/terminal/TerminalServer.cpp
-@@ -59,7 +59,7 @@ DEFINE_string(cfgfile, "", "Location of the config file");
+@@ -60,7 +60,7 @@ DEFINE_string(cfgfile, "", "Location of the config file");
  DEFINE_bool(jump, false,
              "If set, forward all packets between client and dst terminal");
  DEFINE_string(dsthost, "", "Must be set if jump is set to true");
@@ -216,7 +242,7 @@ index 25dec94..154edd6 100644
  
  shared_ptr<ServerConnection> globalServer;
  shared_ptr<UserTerminalRouter> terminalRouter;
-@@ -235,12 +235,7 @@ void runTerminal(shared_ptr<ServerClientConnection> serverClientState) {
+@@ -236,12 +236,7 @@ void runTerminal(shared_ptr<ServerClientConnection> serverClientState) {
                LOG(INFO) << "Got new port forward";
                PortForwardRequest pfr =
                    serverClientState->readProto<PortForwardRequest>();
@@ -230,7 +256,7 @@ index 25dec94..154edd6 100644
                PortForwardResponse pfresponse;
                pfresponse.set_clientfd(pfr.fd());
                if (fd == -1) {
-@@ -576,7 +571,7 @@ int main(int argc, char **argv) {
+@@ -577,7 +572,7 @@ int main(int argc, char **argv) {
    }
  
    if (FLAGS_port == 0) {
