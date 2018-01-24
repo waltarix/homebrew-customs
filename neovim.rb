0341fb85e947ced19a24fd7cdee9491d5f823962
waltari <gitting.crew@gmail.com>
2018-01-24 16:17:01 +0900

    neovim: fix brew audit warning

--- neovim.rb
+++ neovim.rb
@@ -22,7 +22,7 @@ class Neovim < Formula
   depends_on "luajit"
   depends_on "msgpack"
   depends_on "unibilium"
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
 
   resource "lpeg" do
     url "https://luarocks.org/manifests/gvvaughan/lpeg-1.0.1-1.src.rock", :using => :nounzip
