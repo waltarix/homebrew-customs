ec95eab32c7cb10cd3dc585a407882750d65389d
waltari <gitting.crew@gmail.com>
2018-03-03 22:53:26 +0900

    mosh: use inline wcwidth9 instead of shared wcwidth-cjk

--- mosh.rb
+++ mosh.rb
@@ -4,7 +4,7 @@ class Mosh < Formula
   url "https://github.com/mobile-shell/mosh.git", :shallow => false
   version "1.3.2"
   sha256 "da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216"
-  revision 4
+  revision 5
 
   bottle do
     sha256 "a6978eda44965301af1ca77cec8cdcbda2ccb123ae43959ecb9a143fb745b0cd" => :high_sierra
@@ -21,7 +21,6 @@ class Mosh < Formula
   depends_on "tmux" => :build if build.with?("test") || build.bottle?
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "waltarix/customs/wcwidth-cjk"
   depends_on "perl" => :build if
     OS.linux? &&
     begin
@@ -37,12 +36,17 @@ class Mosh < Formula
 
   patch :DATA
 
-  def install
-    ENV.append "LDFLAGS", "-L#{Formula["wcwidth-cjk"].lib} -lwcwidth-cjk"
+  resource "wcwidth9.h" do
+    url "https://gist.githubusercontent.com/waltarix/7a36cc9f234a4a2958a24927696cf87c/raw/d4a38bc596f798b0344d06e9c831677f194d8148/wcwidth9.h"
+    sha256 "50b5f30757ed9e1f9bece87dec4d70e32eee780f42b558242e4e76b1f9b334c8"
+  end
 
+  def install
     # teach mosh to locate mosh-client without referring
     # PATH to support launching outside shell e.g. via launcher
     inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
+
+    buildpath.install resource("wcwidth9.h")
 
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--enable-completion"
@@ -56,6 +60,18 @@ class Mosh < Formula
 end
 
 __END__
+diff --git a/configure.ac b/configure.ac
+index ef18743..65bbd8f 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -263,7 +263,6 @@ AC_CHECK_FUNCS(m4_normalize([
+   strtok
+   strerror
+   strtol
+-  wcwidth
+   cfmakeraw
+   pselect
+   getaddrinfo
 diff --git a/src/frontend/mosh-server.cc b/src/frontend/mosh-server.cc
 index 7918c74..ae5fca4 100644
 --- a/src/frontend/mosh-server.cc
@@ -76,6 +92,77 @@ index 7918c74..ae5fca4 100644
      /* ask ncurses to send UTF-8 instead of ISO 2022 for line-drawing chars */
      if ( setenv( "NCURSES_NO_UTF8_ACS", "1", true ) < 0 ) {
        perror( "setenv" );
+diff --git a/src/frontend/terminaloverlay.cc b/src/frontend/terminaloverlay.cc
+index b1c906a..6dcfad9 100644
+--- a/src/frontend/terminaloverlay.cc
++++ b/src/frontend/terminaloverlay.cc
+@@ -38,6 +38,8 @@
+ 
+ #include "terminaloverlay.h"
+ 
++#include "wcwidth9.h"
++
+ using namespace Overlay;
+ using std::max;
+ using std::mem_fun_ref;
+@@ -264,7 +266,7 @@ void NotificationEngine::apply( Framebuffer &fb ) const
+     }
+ 
+     wchar_t ch = *i;
+-    int chwidth = ch == L'\0' ? -1 : wcwidth( ch );
++    int chwidth = ch == L'\0' ? -1 : wcwidth9( ch );
+     Cell *this_cell = 0;
+ 
+     switch ( chwidth ) {
+@@ -300,7 +302,7 @@ void NotificationEngine::apply( Framebuffer &fb ) const
+     case -1: /* unprintable character */
+       break;
+     default:
+-      assert( !"unexpected character width from wcwidth()" );
++      assert( !"unexpected character width from wcwidth9()" );
+     }
+   }
+ }
+@@ -735,7 +737,7 @@ void PredictionEngine::new_user_byte( char the_byte, const Framebuffer &fb )
+ 	    }
+ 	  }
+ 	}
+-      } else if ( (ch < 0x20) || (wcwidth( ch ) != 1) ) {
++      } else if ( (ch < 0x20) || (wcwidth9( ch ) != 1) ) {
+ 	/* unknown print */
+ 	become_tentative();
+ 	//	fprintf( stderr, "Unknown print 0x%x\n", ch );
+diff --git a/src/terminal/terminal.cc b/src/terminal/terminal.cc
+index 057b3d0..66b2e73 100644
+--- a/src/terminal/terminal.cc
++++ b/src/terminal/terminal.cc
+@@ -38,6 +38,8 @@
+ 
+ #include "terminal.h"
+ 
++#include "wcwidth9.h"
++
+ using namespace Terminal;
+ 
+ Emulator::Emulator( size_t s_width, size_t s_height )
+@@ -66,7 +68,7 @@ void Emulator::print( const Parser::Print *act )
+    * Check for printing ISO 8859-1 first, it's a cheap way to detect
+    * some common narrow characters.
+    */
+-  const int chwidth = ch == L'\0' ? -1 : ( Cell::isprint_iso8859_1( ch ) ? 1 : wcwidth( ch ));
++  const int chwidth = ch == L'\0' ? -1 : ( Cell::isprint_iso8859_1( ch ) ? 1 : wcwidth9( ch ));
+ 
+   Cell *this_cell = fb.get_mutable_cell();
+ 
+@@ -142,7 +144,7 @@ void Emulator::print( const Parser::Print *act )
+   case -1: /* unprintable character */
+     break;
+   default:
+-    assert( !"unexpected character width from wcwidth()" );
++    assert( !"unexpected character width from wcwidth9()" );
+     break;
+   }
+ }
 diff --git a/src/terminal/terminaldisplayinit.cc b/src/terminal/terminaldisplayinit.cc
 index 54dfcc9..9720f26 100644
 --- a/src/terminal/terminaldisplayinit.cc
