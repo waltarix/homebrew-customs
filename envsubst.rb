a4faa27b3ad9c7a16faeec7b20688d935dfb9996
waltari <gitting.crew@gmail.com>
2019-07-01 20:12:52 +0900

    envsubst: 0.20.1

--- envsubst.rb
+++ envsubst.rb
@@ -2,7 +2,7 @@ class Envsubst < Formula
   desc "Substitutes environment variables in shell format strings"
   homepage "https://www.gnu.org/software/gettext/"
   url "file:///dev/null"
-  version "0.19.8.1"
+  version "0.20.1"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "gettext"
