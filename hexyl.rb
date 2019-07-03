fb513c3c046a6142bec64597e66e8dc6cb3bc06e
waltari <gitting.crew@gmail.com>
2019-07-04 08:20:29 +0900

    hexyl: 0.5.1

--- hexyl.rb
+++ hexyl.rb
@@ -1,14 +1,14 @@
 class Hexyl < Formula
   desc "Command-line hex viewer"
   homepage "https://github.com/sharkdp/hexyl"
-  url "https://github.com/sharkdp/hexyl/archive/v0.5.0.tar.gz"
-  sha256 "6241a4cf3e3ec2f32164539ef158c84ad29c53511cd1e3c0148776b8ce5234d4"
+  url "https://github.com/sharkdp/hexyl/archive/v0.5.1.tar.gz"
+  sha256 "9c12bc6377d1efedc4a1731547448f7eb6ed17ee1c267aad9a35995b42091163"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4662a02fa6c3e47e9d1376a177c74c7127dc16a7ee6939538d01ef425b2cbf3f" => :mojave
-    sha256 "652e4e4a0a661d3dd97aaab96a4d33ffc8096a58773621e612e80f22a9df32f7" => :high_sierra
-    sha256 "2b52aef13a792296811d7ec74719b56f0aec3e3302b19d48f82731ab952aeda4" => :sierra
+    sha256 "2ea951a75e72e97075b3d4db45edbfc6cbb27b67c4655fcd1483ce6ad2c4165d" => :mojave
+    sha256 "52d0981ef2edc478f11e8c66a959f2868c0ee7e20a3178064ec289dfaa71e397" => :high_sierra
+    sha256 "688108b44f0fb529f1085a1e7416d445b6b6c12885a9a941727c950233f8b6eb" => :sierra
   end
 
   depends_on "rust" => :build
@@ -32,7 +32,7 @@ end
 
 __END__
 diff --git a/src/main.rs b/src/main.rs
-index 99a3d0a..8bb8849 100644
+index ecfa444..b974230 100644
 --- a/src/main.rs
 +++ b/src/main.rs
 @@ -77,8 +77,8 @@ impl Byte {
@@ -41,8 +41,8 @@ index 99a3d0a..8bb8849 100644
              AsciiWhitespace => '_',
 -            AsciiOther => '•',
 -            NonAscii => '×',
-+            AsciiOther => '+',
-+            NonAscii => '*',
++            AsciiOther => '✳',
++            NonAscii => '✖',
          }
      }
  }
