7cd589452a026fa8dfa545e471b955b62adc122d
waltari <gitting.crew@gmail.com>
2019-08-18 20:31:23 +0900

    timg: 0.9.5-beta4

--- timg.rb
+++ timg.rb
@@ -2,10 +2,10 @@ class Timg < Formula
   desc "Terminal Image Viewer"
   homepage "https://github.com/hzeller/timg"
   url "https://github.com/hzeller/timg.git",
-    :revision => "dcd0c4d3d3d20280726314811ade4b74731b921c"
-  version "0.9.5-beta3"
+    :revision => "4a300cfe9c159ffb5d357b41b3c69c57219da9e8"
+  version "0.9.5-beta4"
 
-  depends_on "graphicsmagick" => :build
+  depends_on "graphicsmagick"
 
   patch :DATA
 
@@ -32,7 +32,7 @@ index bd66f93..b5ce20d 100644
  #include <vector>
  
 -#define TIMG_VERSION "0.9.1beta"
-+#define TIMG_VERSION "0.9.5-beta3"
++#define TIMG_VERSION "0.9.5-beta4"
  
  volatile bool interrupt_received = false;
  static void InterruptHandler(int signo) {
