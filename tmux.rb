894dec8e1c5f2605dddb7a459141c24b70bfe4ce
waltari <gitting.crew@gmail.com>
2020-03-02 18:26:13 +0900

    tmux: 3.0a_1

--- tmux.rb
+++ tmux.rb
@@ -3,6 +3,7 @@ class Tmux < Formula
   homepage "https://tmux.github.io/"
   url "https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz"
   sha256 "4ad1df28b4afa969e59c08061b45082fdc49ff512f30fc8e43217d7b0e5f8db9"
+  revision 1
 
   bottle do
     cellar :any
@@ -33,8 +34,8 @@ class Tmux < Formula
   end
 
   resource "wcwidth9.h" do
-    url "https://gist.githubusercontent.com/waltarix/7a36cc9f234a4a2958a24927696cf87c/raw/d4a38bc596f798b0344d06e9c831677f194d8148/wcwidth9.h"
-    sha256 "50b5f30757ed9e1f9bece87dec4d70e32eee780f42b558242e4e76b1f9b334c8"
+    url "https://gist.githubusercontent.com/waltarix/7a36cc9f234a4a2958a24927696cf87c/raw/1b1065607bfc722da982d4e16fc10039d5124494/wcwidth9.h"
+    sha256 "be41464e5830e41bf075a394a05f811d515f52e99adc1e01156ba932c58fd124"
   end
 
   patch :DATA
