d00c957a6259fcb5cc6880605fd5802e7b3438b3
waltari <gitting.crew@gmail.com>
2020-03-02 18:26:33 +0900

    mosh: 1.3.2_11

--- mosh.rb
+++ mosh.rb
@@ -3,7 +3,7 @@ class Mosh < Formula
   homepage "https://mosh.org"
   url "https://mosh.org/mosh-1.3.2.tar.gz"
   sha256 "da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216"
-  revision 10
+  revision 11
 
   bottle do
     cellar :any
@@ -30,8 +30,8 @@ class Mosh < Formula
   depends_on "protobuf"
 
   resource "wcwidth9.h" do
-    url "https://gist.githubusercontent.com/waltarix/7a36cc9f234a4a2958a24927696cf87c/raw/d4a38bc596f798b0344d06e9c831677f194d8148/wcwidth9.h"
-    sha256 "50b5f30757ed9e1f9bece87dec4d70e32eee780f42b558242e4e76b1f9b334c8"
+    url "https://gist.githubusercontent.com/waltarix/7a36cc9f234a4a2958a24927696cf87c/raw/1b1065607bfc722da982d4e16fc10039d5124494/wcwidth9.h"
+    sha256 "be41464e5830e41bf075a394a05f811d515f52e99adc1e01156ba932c58fd124"
   end
 
   # Fix mojave build.
