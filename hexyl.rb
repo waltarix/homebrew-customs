fd30aa228691641b9a9bf8eee181ab1e2753e55c
waltari <gitting.crew@gmail.com>
2020-03-11 17:14:45 +0900

    hexyl: 0.7.0

--- hexyl.rb
+++ hexyl.rb
@@ -1,14 +1,15 @@
 class Hexyl < Formula
   desc "Command-line hex viewer"
   homepage "https://github.com/sharkdp/hexyl"
-  url "https://github.com/sharkdp/hexyl/archive/v0.5.1.tar.gz"
-  sha256 "9c12bc6377d1efedc4a1731547448f7eb6ed17ee1c267aad9a35995b42091163"
+  url "https://github.com/sharkdp/hexyl/archive/v0.7.0.tar.gz"
+  sha256 "92aa86fc2b482d2d7abf07565ea3587767a9beb9135a307aadeba61cc84f4b34"
 
   bottle do
-    root_url "https://gist.githubusercontent.com/waltarix/eacc34005669406dca03b51ff6af2617/raw/983a779dc43a4cb55eb322e8766b24515598870e"
+    root_url "https://gist.githubusercontent.com/waltarix/eacc34005669406dca03b51ff6af2617/raw/64fcae039fa401bd7cb66a1094ae847d7477a78f"
     cellar :any_skip_relocation
-    sha256 "2f7f792960f2ee0cc84bb42f73560452374b3ebcf7949dcb2119d01e12ac025d" => :mojave
-    sha256 "2f7f792960f2ee0cc84bb42f73560452374b3ebcf7949dcb2119d01e12ac025d" => :high_sierra
+    sha256 "cb431f80f345c0cc67a29311954fa85f6573830e734c7c372eedd6e30dee0b53" => :catalina
+    sha256 "cb431f80f345c0cc67a29311954fa85f6573830e734c7c372eedd6e30dee0b53" => :mojave
+    sha256 "cb431f80f345c0cc67a29311954fa85f6573830e734c7c372eedd6e30dee0b53" => :high_sierra
   end
 
   depends_on "rustup-init" => :build
@@ -21,7 +22,7 @@ class Hexyl < Formula
       ENV["RUSTUP_HOME"] = user_home/".rustup"
     end
 
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
   test do
@@ -32,11 +33,11 @@ class Hexyl < Formula
 end
 
 __END__
-diff --git a/src/main.rs b/src/main.rs
-index ecfa444..b974230 100644
---- a/src/main.rs
-+++ b/src/main.rs
-@@ -77,8 +77,8 @@ impl Byte {
+diff --git a/src/lib.rs b/src/lib.rs
+index 08fd4d8..528b153 100644
+--- a/src/lib.rs
++++ b/src/lib.rs
+@@ -65,8 +65,8 @@ impl Byte {
              AsciiPrintable => self.0 as char,
              AsciiWhitespace if self.0 == 0x20 => ' ',
              AsciiWhitespace => '_',
