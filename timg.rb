d73649edca1b5b7875fdbd5333d324feaf842cb5
waltari <gitting.crew@gmail.com>
2020-05-09 14:35:11 +0900

    timg: 0.9.9-094da0b

--- timg.rb
+++ timg.rb
@@ -2,16 +2,20 @@ class Timg < Formula
   desc "Terminal Image Viewer"
   homepage "https://github.com/hzeller/timg"
   url "https://github.com/hzeller/timg.git",
-    :revision => "4a300cfe9c159ffb5d357b41b3c69c57219da9e8"
-  version "0.9.5-beta4"
+    :revision => "094da0b984f6943c1b52b4c7a81966378709466b"
+  version "0.9.9-094da0b"
 
+  depends_on "ffmpeg"
   depends_on "graphicsmagick"
 
-  patch :DATA
-
   def install
+    inreplace "src/Makefile", " .FORCE", ""
+    (buildpath/"src/timg-version.h").write <<~EOS
+      #define TIMG_VERSION "0.9.9-094da0b"
+    EOS
+
     cd "src" do
-      system "make"
+      system "make", "WITH_VIDEO_DECODING=1"
 
       bin.install "timg"
     end
@@ -21,18 +25,3 @@ class Timg < Formula
     system "#{bin}/timg", "-v"
   end
 end
-
-__END__
-diff --git a/src/timg.cc b/src/timg.cc
-index bd66f93..b5ce20d 100644
---- a/src/timg.cc
-+++ b/src/timg.cc
-@@ -34,7 +34,7 @@
- 
- #include <vector>
- 
--#define TIMG_VERSION "0.9.1beta"
-+#define TIMG_VERSION "0.9.5-beta4"
- 
- volatile bool interrupt_received = false;
- static void InterruptHandler(int signo) {
