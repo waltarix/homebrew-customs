847183c7b98b73181076618f17906a4dda21d547
waltari <gitting.crew@gmail.com>
2020-05-20 15:44:48 +0900

    neovim: Unicode 13.0.0

--- neovim.rb
+++ neovim.rb
@@ -3,17 +3,14 @@ class Neovim < Formula
   homepage "https://neovim.io/"
   url "https://github.com/neovim/neovim/archive/v0.4.3.tar.gz"
   sha256 "91a0b5d32204a821bf414690e6b48cf69224d1961d37158c2b383f6a6cf854d2"
-
-  bottle do
-    sha256 "389ab254794ea04df8a106bf281c227c4fdee27c609d52951f32b9a0239031d6" => :catalina
-    sha256 "fb6cc38c45be4650a4a750a314cec27f6f7643dc7c22a1f5abae347e1b39b388" => :mojave
-    sha256 "b2f7450e005f4eb22bfaf650fe7741fa8629393a947b4dd48c9f91e7fc258113" => :high_sierra
-  end
+  revision 1
 
   head do
     url "https://github.com/neovim/neovim.git"
     depends_on "utf8proc"
   end
+
+  bottle :unneeded
 
   depends_on "cmake" => :build
   depends_on "luarocks" => :build
@@ -25,10 +22,6 @@ class Neovim < Formula
   depends_on "luajit"
   depends_on "msgpack"
   depends_on "unibilium"
-
-  def pour_bottle?
-    false
-  end
 
   resource "mpack" do
     url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.7/libmpack-lua-1.0.7.tar.gz"
@@ -55,12 +48,16 @@ class Neovim < Formula
     sha256 "5cc75a012bfa9a5a1543d0167952676474f31c2d7fd8d450b56d8929dbebb5ef"
   end
 
-  patch :DATA
+  resource "unicode" do
+    url "https://github.com/waltarix/neovim/releases/download/unicode%2F13.0.0/unicode.tar.xz"
+    sha256 "4775f4648519e42739f80ccdb24ee978c25086751fe0e698e43404497cce4d83"
+  end
 
   def install
-    resources.each do |r|
+    resources.reject { |r| r.name == "unicode" }.each do |r|
       r.stage(buildpath/"deps-build/build/src/#{r.name}")
     end
+    resource("unicode").stage(buildpath/"unicode")
 
     ENV.prepend_path "LUA_PATH", "#{buildpath}/deps-build/share/lua/5.1/?.lua"
     ENV.prepend_path "LUA_CPATH", "#{buildpath}/deps-build/lib/lua/5.1/?.so"
@@ -115,40 +112,3 @@ class Neovim < Formula
     assert_equal "Hello World from Neovim!!", (testpath/"test.txt").read.chomp
   end
 end
-
-__END__
-diff --git a/unicode/EastAsianWidth.txt b/unicode/EastAsianWidth.txt
-index 94d55d665..d8641d709 100644
---- a/unicode/EastAsianWidth.txt
-+++ b/unicode/EastAsianWidth.txt
-@@ -1135,14 +1135,14 @@
- 2460..249B;A     # No    [60] CIRCLED DIGIT ONE..NUMBER TWENTY FULL STOP
- 249C..24E9;A     # So    [78] PARENTHESIZED LATIN SMALL LETTER A..CIRCLED LATIN SMALL LETTER Z
- 24EA;N           # No         CIRCLED DIGIT ZERO
--24EB..24FF;A     # No    [21] NEGATIVE CIRCLED NUMBER ELEVEN..NEGATIVE CIRCLED DIGIT ZERO
--2500..254B;A     # So    [76] BOX DRAWINGS LIGHT HORIZONTAL..BOX DRAWINGS HEAVY VERTICAL AND HORIZONTAL
-+24EB..24FF;H     # No    [21] NEGATIVE CIRCLED NUMBER ELEVEN..NEGATIVE CIRCLED DIGIT ZERO
-+2500..254B;H     # So    [76] BOX DRAWINGS LIGHT HORIZONTAL..BOX DRAWINGS HEAVY VERTICAL AND HORIZONTAL
- 254C..254F;N     # So     [4] BOX DRAWINGS LIGHT DOUBLE DASH HORIZONTAL..BOX DRAWINGS HEAVY DOUBLE DASH VERTICAL
--2550..2573;A     # So    [36] BOX DRAWINGS DOUBLE HORIZONTAL..BOX DRAWINGS LIGHT DIAGONAL CROSS
-+2550..2573;H     # So    [36] BOX DRAWINGS DOUBLE HORIZONTAL..BOX DRAWINGS LIGHT DIAGONAL CROSS
- 2574..257F;N     # So    [12] BOX DRAWINGS LIGHT LEFT..BOX DRAWINGS HEAVY UP AND LIGHT DOWN
--2580..258F;A     # So    [16] UPPER HALF BLOCK..LEFT ONE EIGHTH BLOCK
-+2580..258F;H     # So    [16] UPPER HALF BLOCK..LEFT ONE EIGHTH BLOCK
- 2590..2591;N     # So     [2] RIGHT HALF BLOCK..LIGHT SHADE
--2592..2595;A     # So     [4] MEDIUM SHADE..RIGHT ONE EIGHTH BLOCK
-+2592..2595;H     # So     [4] MEDIUM SHADE..RIGHT ONE EIGHTH BLOCK
- 2596..259F;N     # So    [10] QUADRANT LOWER LEFT..QUADRANT UPPER RIGHT AND LOWER LEFT AND LOWER RIGHT
- 25A0..25A1;A     # So     [2] BLACK SQUARE..WHITE SQUARE
- 25A2;N           # So         WHITE SQUARE WITH ROUNDED CORNERS
-@@ -1657,7 +1657,9 @@ D7CB..D7FB;N     # Lo    [49] HANGUL JONGSEONG NIEUN-RIEUL..HANGUL JONGSEONG PHI
- D800..DB7F;N     # Cs   [896] <surrogate-D800>..<surrogate-DB7F>
- DB80..DBFF;N     # Cs   [128] <surrogate-DB80>..<surrogate-DBFF>
- DC00..DFFF;N     # Cs  [1024] <surrogate-DC00>..<surrogate-DFFF>
--E000..F8FF;A     # Co  [6400] <private-use-E000>..<private-use-F8FF>
-+E000..E09F;A     # Co   [160] <private-use-E000>..<private-use-E09F>
-+E0A0..E0D7;H     # Co    [56] <powerline-symbols-E0A0>..<powerline-symbols-E0D7>
-+E0D8..F8FF;A     # Co  [6184] <private-use-E0D8>..<private-use-F8FF>
- F900..FA6D;W     # Lo   [366] CJK COMPATIBILITY IDEOGRAPH-F900..CJK COMPATIBILITY IDEOGRAPH-FA6D
- FA6E..FA6F;W     # Cn     [2] <reserved-FA6E>..<reserved-FA6F>
- FA70..FAD9;W     # Lo   [106] CJK COMPATIBILITY IDEOGRAPH-FA70..CJK COMPATIBILITY IDEOGRAPH-FAD9
