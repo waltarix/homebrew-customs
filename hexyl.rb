3e92eb7848d90fcb77eed02ce01c8ff471213b21
waltari <gitting.crew@gmail.com>
2020-05-22 09:46:52 +0900

    hexyl: fix binary

--- hexyl.rb
+++ hexyl.rb
@@ -3,10 +3,10 @@ class Hexyl < Formula
   homepage "https://github.com/sharkdp/hexyl"
   if OS.linux?
     url "https://github.com/waltarix/hexyl/releases/download/v0.7.0-custom/hexyl-0.7.0-linux.tar.xz"
-    sha256 "f6f90fadb3b300d2c8d23f2d122f2a9eeb269512193413c2816785ebb038a30c"
+    sha256 "ef5653af71e0cd95a56babe9feb8fb59171748e75033ded07ded3f07a787554a"
   else
     url "https://github.com/waltarix/hexyl/releases/download/v0.7.0-custom/hexyl-0.7.0-darwin.tar.xz"
-    sha256 "19db60331c199c5176d1bcfc4cb5fc256fd670730b1a5dfff29cd39cb4fb9660"
+    sha256 "4f90548c251b483498b2e7f242168b9527637eef783702450babfabb29b67b68"
   end
 
   bottle :unneeded
