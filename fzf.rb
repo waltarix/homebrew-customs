2b67673ebefe4eba507f41fc23f873dca46beb70
waltari <gitting.crew@gmail.com>
2020-05-22 14:08:25 +0900

    fzf: fix binary

--- fzf.rb
+++ fzf.rb
@@ -4,17 +4,17 @@ class Fzf < Formula
   url "https://github.com/junegunn/fzf/archive/0.21.1.tar.gz"
   sha256 "47adf138f17c45d390af81958bdff6f92157d41e2c4cb13773df078b905cdaf4"
   head "https://github.com/junegunn/fzf.git"
-  revision 1
+  revision 2
 
   bottle :unneeded
 
   resource "binary" do
     if OS.linux?
       url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-linux_amd64.tar.xz"
-      sha256 "16fd48ccf7d28b73ac852e2fae2986a00addc34ecfd9662e56ed13a14090cbc4"
+      sha256 "71070204f6e75bc1e0d5b43fc21efd5c129240b9102e260a9c48dea93c4158e4"
     else
       url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-darwin_amd64.tar.xz"
-      sha256 "dacd910b8a7a66a49a0b6c21b5f25e04ee070e405b16c87fb04083b60ad4a334"
+      sha256 "2caa489f7f83342c1774ff5b38e9291be0b638b78104e20de399415c334855cf"
     end
   end
 
