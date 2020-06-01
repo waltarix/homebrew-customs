b7f2d1e905326fd397978c44af0fbadd420aa4c2
waltari <gitting.crew@gmail.com>
2020-06-01 14:34:43 +0900

    fzf: chafanize

--- fzf.rb
+++ fzf.rb
@@ -3,18 +3,18 @@ class Fzf < Formula
   homepage "https://github.com/junegunn/fzf"
   url "https://github.com/junegunn/fzf/archive/0.21.1.tar.gz"
   sha256 "47adf138f17c45d390af81958bdff6f92157d41e2c4cb13773df078b905cdaf4"
+  revision 3
   head "https://github.com/junegunn/fzf.git"
-  revision 2
 
   bottle :unneeded
 
   resource "binary" do
     if OS.linux?
       url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-linux_amd64.tar.xz"
-      sha256 "71070204f6e75bc1e0d5b43fc21efd5c129240b9102e260a9c48dea93c4158e4"
+      sha256 "e84fd3aad18175ae03a724168bb4d51456c2128e3df518a22e19f461ebf58601"
     else
       url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-darwin_amd64.tar.xz"
-      sha256 "2caa489f7f83342c1774ff5b38e9291be0b638b78104e20de399415c334855cf"
+      sha256 "87f2b4ed1db8869ef09641b7adcf981c7ce0579dee56f7e79bca619a8726908d"
     end
   end
 
