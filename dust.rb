2044e4b32eedbc84808b11d600c8aa89736dc64f
waltari <gitting.crew@gmail.com>
2020-06-17 20:35:04 +0900

    dust: fix binary

--- dust.rb
+++ dust.rb
@@ -3,10 +3,10 @@ class Dust < Formula
   homepage "https://github.com/bootandy/dust"
   if OS.linux?
     url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom/dust-0.5.1-linux.tar.xz"
-    sha256 "8e6b0988caa4b4a6565c3e3c3db8a11162d0143026e323a20b9771ed95e603a9"
+    sha256 "16845434352c97fa0d46313ffdc8b700927712d11f44abb3ae37594ed6d3b885"
   else
     url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom/dust-0.5.1-darwin.tar.xz"
-    sha256 "f5b682e60c675d8245c11ec488390402ba5a8fe4df395c00f48bd6d2cca9f1b7"
+    sha256 "10f34bb0ce7b47e97d3a2a60a2ac60803cf6d23757be56a5f0400c8eab9e8f72"
   end
 
   bottle :unneeded
