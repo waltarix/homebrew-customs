bfda7554e0d13c7f7baded93a71fa43ecacb6076
waltari <gitting.crew@gmail.com>
2020-07-07 18:28:27 +0900

    ssrc: 1.33

--- ssrc.rb
+++ ssrc.rb
@@ -1,15 +1,17 @@
 class Ssrc < Formula
   desc "High Quality Audio Sampling Rate Converter"
   homepage "https://shibatch.sourceforge.io/"
-  url "http://shibatch.sourceforge.net/download/ssrc-1.30.tgz"
-  sha256 "088286a2806153c3360a84c160540405b4d07fa6af5991cde4d84e8566ee1faa"
+  url "http://shibatch.sourceforge.net/download/ssrc-1.33.tar.gz"
+  sha256 "a663082ca11d049a20d7598328e1646c9220d68005d793f992fc0618806d713a"
 
   def install
+    ENV.deparallelize
     system "make"
     bin.install %w[ssrc ssrc_hp]
   end
 
   test do
-    assert_match "version 1.30", shell_output("#{bin}/ssrc", 255)
+    assert_match "version 1.33(single precision)", shell_output("#{bin}/ssrc", 255)
+    assert_match "version 1.33(double precision)", shell_output("#{bin}/ssrc_hp", 255)
   end
 end
