1592195d114ac5b62090232142dddf7fbbac998e
waltari <gitting.crew@gmail.com>
2020-07-08 13:39:54 +0900

    dust: 0.5.1-r1

--- dust.rb
+++ dust.rb
@@ -2,12 +2,13 @@ class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
   if OS.linux?
-    url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom/dust-0.5.1-linux.tar.xz"
-    sha256 "16845434352c97fa0d46313ffdc8b700927712d11f44abb3ae37594ed6d3b885"
+    url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom-r1/dust-0.5.1-linux.tar.xz"
+    sha256 "718d5686fc7b22de7108cadfd8ceb8ce439b2f8c493bd13e28c9fbaa29f19b43"
   else
-    url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom/dust-0.5.1-darwin.tar.xz"
-    sha256 "10f34bb0ce7b47e97d3a2a60a2ac60803cf6d23757be56a5f0400c8eab9e8f72"
+    url "https://github.com/waltarix/dust/releases/download/v0.5.1-custom-r1/dust-0.5.1-darwin.tar.xz"
+    sha256 "58563b3b159d23c4c56eb800f2a986b79e7025bd53b270a1e6a307a7101b95d5"
   end
+  revision 1
 
   bottle :unneeded
 
