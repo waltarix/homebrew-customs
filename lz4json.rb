397d6fb818b801453c81d6555a0b1052307b8422
waltari <gitting.crew@gmail.com>
2020-07-08 15:04:07 +0900

    lz4json: use revision instead of tag

--- lz4json.rb
+++ lz4json.rb
@@ -2,7 +2,8 @@ class Lz4json < Formula
   desc "C decompress tool for mozilla lz4json format"
   homepage "https://github.com/andikleen/lz4json"
   url "https://github.com/andikleen/lz4json.git",
-    :tag => "v2"
+    :revision => "c44c51005c505de2636cc1e59cde764490de7632"
+  version "2"
 
   depends_on "lz4"
 
