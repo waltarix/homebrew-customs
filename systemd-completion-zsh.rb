6c9e3a8f9abf19e08b3e80e4ca89e7705eb7c7c4
waltari <gitting.crew@gmail.com>
2020-07-10 14:54:49 +0900

    add systemd-completion-zsh formula

---
+++ systemd-completion-zsh.rb
@@ -0,0 +1,17 @@
+class SystemdCompletionZsh < Formula
+  desc "System and service manager"
+  homepage "https://wiki.freedesktop.org/www/Software/systemd/"
+  url "https://github.com/systemd/systemd/archive/v245.tar.gz"
+  sha256 "f34f1dc52b2dc60563c2deb6db86d78f6a97bceb29aa0511436844b2fc618040"
+
+  bottle :unneeded
+
+  def install
+    zsh_completion.install Dir["shell-completion/zsh/_*"]
+  end
+
+  test do
+    assert_match "_journalctl is an autoload shell function",
+      shell_output("zsh -c 'source #{zsh_completion}/_journalctl && whence -v _journalctl'")
+  end
+end
