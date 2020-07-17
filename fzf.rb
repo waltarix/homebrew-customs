b65d8ab3a7b30eca4516be60d3dc30b7429a3b25
waltari <gitting.crew@gmail.com>
2020-07-17 21:38:52 +0900

    fzf: 0.21.1-r1

--- fzf.rb
+++ fzf.rb
@@ -3,30 +3,31 @@ class Fzf < Formula
   homepage "https://github.com/junegunn/fzf"
   url "https://github.com/junegunn/fzf/archive/0.21.1.tar.gz"
   sha256 "47adf138f17c45d390af81958bdff6f92157d41e2c4cb13773df078b905cdaf4"
-  revision 3
+  revision 4
   head "https://github.com/junegunn/fzf.git"
 
   bottle :unneeded
 
   resource "binary" do
     if OS.linux?
-      url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-linux_amd64.tar.xz"
-      sha256 "e84fd3aad18175ae03a724168bb4d51456c2128e3df518a22e19f461ebf58601"
+      url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom-r1/fzf-0.21.1-linux_amd64.tar.xz"
+      sha256 "91c636f6cf29c97a3e1e1cc5e0468c37c04f829df132feb4373b23ddb57c3b4b"
     else
-      url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom/fzf-0.21.1-darwin_amd64.tar.xz"
-      sha256 "87f2b4ed1db8869ef09641b7adcf981c7ce0579dee56f7e79bca619a8726908d"
+      url "https://github.com/waltarix/fzf/releases/download/0.21.1-custom-r1/fzf-0.21.1-darwin_amd64.tar.xz"
+      sha256 "86621f5a8a51d3043b2470b159fcda6398b04a00f49c39c1626863dc8aac288b"
     end
   end
 
   def install
-    bin.install resource("binary").files("fzf")
+    resource("binary").stage do
+      bin.install ["bin/fzf", "bin/fzf-tmux"]
+      (prefix/"plugin").install "plugin/fzf.vim"
+    end
 
     prefix.install "install", "uninstall"
     (prefix/"shell").install %w[bash zsh fish].map { |s| "shell/key-bindings.#{s}" }
     (prefix/"shell").install %w[bash zsh].map { |s| "shell/completion.#{s}" }
-    (prefix/"plugin").install "plugin/fzf.vim"
     man1.install "man/man1/fzf.1", "man/man1/fzf-tmux.1"
-    bin.install "bin/fzf-tmux"
   end
 
   def caveats
