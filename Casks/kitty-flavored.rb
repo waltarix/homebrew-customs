680447d5a03aac22290eb07523dcfb0b022e76d7
waltari <gitting.crew@gmail.com>
2020-08-12 14:01:04 +0900

    kitty: 0.18.3-custom

--- Casks/kitty-flavored.rb
+++ Casks/kitty-flavored.rb
@@ -1,23 +1,23 @@
-cask 'kitty-flavored' do
-  version '0.18.2'
-  sha256 '64f3753f284635890c4554cbaecc81616670e58fc97bb3e3a3566a4bffee6d22'
+cask "kitty-flavored" do
+  version "0.18.3"
+  sha256 "c79dd06996f93b86d2e1fd714c6627f487084b36db75595573552573305526cc"
 
   url "https://github.com/waltarix/kitty/releases/download/v#{version.before_comma}-custom/kitty-#{version.before_comma}.dmg"
-  appcast 'https://github.com/waltarix/kitty/releases.atom'
-  name 'kitty flavored'
-  homepage 'https://github.com/kovidgoyal/kitty'
+  appcast "https://github.com/waltarix/kitty/releases.atom"
+  name "kitty flavored"
+  homepage "https://github.com/kovidgoyal/kitty"
 
-  conflicts_with cask: 'kitty'
+  conflicts_with cask: "kitty"
 
-  depends_on macos: '>= :sierra'
-  depends_on formula: 'harfbuzz'
-  depends_on formula: 'libpng'
-  depends_on formula: 'python'
+  depends_on macos: ">= :sierra"
+  depends_on formula: "harfbuzz"
+  depends_on formula: "libpng"
+  depends_on formula: "python"
 
-  app 'kitty.app'
+  app "kitty.app"
   # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/kitty.wrapper.sh"
-  binary shimscript, target: 'kitty'
+  binary shimscript, target: "kitty"
 
   preflight do
     IO.write shimscript, <<~EOS
@@ -27,8 +27,8 @@ cask 'kitty-flavored' do
   end
 
   zap trash: [
-               '~/.config/kitty',
-               '~/Library/Preferences/kitty',
-               '~/Library/Saved Application State/net.kovidgoyal.kitty.savedState',
-             ]
+    "~/.config/kitty",
+    "~/Library/Preferences/kitty",
+    "~/Library/Saved Application State/net.kovidgoyal.kitty.savedState",
+  ]
 end
