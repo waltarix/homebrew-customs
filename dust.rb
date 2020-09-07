c47b75052397867f16c90d322e7d4270c5383617
waltari <gitting.crew@gmail.com>
2020-09-07 15:05:50 +0900

    dust: 0.5.3

--- dust.rb
+++ dust.rb
@@ -2,13 +2,19 @@ class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
   if OS.linux?
-    url "https://github.com/waltarix/dust/releases/download/v0.5.2-custom/dust-0.5.2-linux.tar.xz"
-    sha256 "be0900103a06688b440109c6825c5de32a75a1f9465ea7024b0d1fc256aaeb98"
+    url "https://github.com/waltarix/dust/releases/download/v0.5.3-custom/dust-0.5.3-linux.tar.xz"
+    sha256 "5ae8136d6e9cbe30b12b59ac4a724c379ab6df5c1896f217244ebe9d1f7297f0"
   else
-    url "https://github.com/waltarix/dust/releases/download/v0.5.2-custom/dust-0.5.2-darwin.tar.xz"
-    sha256 "3f0aebe8adc937b94c2115de26f39b2927d7cfbb642785ab4dbd130b1fc3361a"
+    url "https://github.com/waltarix/dust/releases/download/v0.5.3-custom/dust-0.5.3-darwin.tar.xz"
+    sha256 "e6156a7b0f1682545d5ac463009068fe65c2ee5c7c5e2ef7c973b153fa69b2a0"
   end
   license "Apache-2.0"
+  head "https://github.com/bootandy/dust.git"
+
+  livecheck do
+    url :head
+    regex(/v([\d.]+)/i)
+  end
 
   bottle :unneeded
 
@@ -17,6 +23,6 @@ class Dust < Formula
   end
 
   test do
-    assert_match /\d+.+?\./, shell_output("#{bin}/dust -n 1")
+    assert_match(/\d+.+?\./, shell_output("#{bin}/dust -n 1"))
   end
 end
