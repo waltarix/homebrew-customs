3c577c102f73aebfe2e69883c64eb47cd53e5271
waltari <gitting.crew@gmail.com>
2020-10-02 16:35:36 +0900

    vivid: 0.6.0

--- vivid.rb
+++ vivid.rb
@@ -2,12 +2,13 @@ class Vivid < Formula
   desc "Generator for LS_COLORS with support for multiple color themes"
   homepage "https://github.com/sharkdp/vivid"
   if OS.linux?
-    url "https://github.com/sharkdp/vivid/releases/download/v0.5.0/vivid-v0.5.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "c287833452183c5d54e1424cfd92a98d61274130126892c4ef80758bc48c2d31"
+    url "https://github.com/sharkdp/vivid/releases/download/v0.6.0/vivid-v0.6.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "cdd8c196516606b69e06a75cf8d6611515757f732c00abdb044c6d2c9797b5f6"
   else
-    url "https://github.com/sharkdp/vivid/releases/download/v0.5.0/vivid-v0.5.0-x86_64-apple-darwin.tar.gz"
-    sha256 "c9664aea84caa7ad07a74d01aded1465e3553ecdfdd49da546a5b86b76495f9f"
+    url "https://github.com/sharkdp/vivid/releases/download/v0.6.0/vivid-v0.6.0-x86_64-apple-darwin.tar.gz"
+    sha256 "2416526d44f256097f20fced8d7a343a52e315167d0cfd128378a9c45bc4679e"
   end
+  license any_of: ["MIT", "Apache-2.0"]
 
   bottle :unneeded
 
@@ -25,7 +26,6 @@ class Vivid < Formula
   end
 
   test do
-    output = shell_output("#{bin}/vivid -V")
-    assert_match "vivid #{version}", output
+    assert_includes shell_output("#{bin}/vivid preview molokai"), "archives.images: \e[4;38;2;249;38;114m*.bin\e[0m\n"
   end
 end
