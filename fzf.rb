edeeab98c0d688fefb10760ca3cdc7693d151708
waltari <gitting.crew@gmail.com>
2020-11-04 13:41:21 +0900

    fzf: 0.24.2

--- fzf.rb
+++ fzf.rb
@@ -1,8 +1,8 @@
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.24.1.tar.gz"
-  sha256 "35e8f57319d4b0ad3297251f4487b15203d288a081c1019d67f80758264b9d45"
+  url "https://github.com/junegunn/fzf/archive/0.24.2.tar.gz"
+  sha256 "8a07554bade6b3064531689d4da829accc843e28ea1ae3229d3bd6d2ebe4a84e"
   license "MIT"
   head "https://github.com/junegunn/fzf.git"
 
@@ -10,11 +10,11 @@ class Fzf < Formula
 
   resource "binary" do
     if OS.linux?
-      url "https://github.com/waltarix/fzf/releases/download/0.24.1-custom/fzf-0.24.1-linux_amd64.tar.xz"
-      sha256 "4236eba7d6d0e6954ccb0da72715bef0dd521abf168bd350fb89213359eac06e"
+      url "https://github.com/waltarix/fzf/releases/download/0.24.2-custom/fzf-0.24.2-linux_amd64.tar.xz"
+      sha256 "dbe15aaa78c5911ab07d747d3177279f440b807f168912f1db3f5f41f5f63166"
     else
-      url "https://github.com/waltarix/fzf/releases/download/0.24.1-custom/fzf-0.24.1-darwin_amd64.tar.xz"
-      sha256 "dee2adae7fb36ed2bb539e5e4a33a7ed29cd7a615b721c503a7bc121a7c8eac9"
+      url "https://github.com/waltarix/fzf/releases/download/0.24.2-custom/fzf-0.24.2-darwin_amd64.tar.xz"
+      sha256 "6fb1980e03a6f4254dd8053e2d41a3fe4b7116adaf7250a10b3b7d41b1693e7d"
     end
   end
 
