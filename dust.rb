d281f299dcf6c625338f8dd056ea4d3692feba7a
waltari <gitting.crew@gmail.com>
2021-01-23 18:16:30 +0900

    dust: add ARM version

--- dust.rb
+++ dust.rb
@@ -2,11 +2,16 @@ class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
   if OS.linux?
-    url "https://github.com/waltarix/dust/releases/download/v0.5.4-custom/dust-0.5.4-linux.tar.xz"
-    sha256 "9564362bd0f7f04deac2abb12d46242ac75d489aeffab94f1ae5b4978b4dd98b"
+    url "https://github.com/waltarix/dust/releases/download/v0.5.4-custom-r1/dust-0.5.4-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "b7fb11b9bcc8ac03d08277ab93f206aac006affaf91b71d77f0b7d6d8b00b494"
   else
-    url "https://github.com/waltarix/dust/releases/download/v0.5.4-custom/dust-0.5.4-darwin.tar.xz"
-    sha256 "817bc534330da270541cc1fda87215b4ab3817b264c4d92ed9e99de5029d0482"
+    if Hardware::CPU.arm?
+      url "https://github.com/waltarix/dust/releases/download/v0.5.4-custom-r1/dust-0.5.4-aarch64-apple-darwin.tar.xz"
+      sha256 "c4486e1c3caab5e8e2ea8d3647b97dd22aa308b0e2837756c8010d438ab539f6"
+    else
+      url "https://github.com/waltarix/dust/releases/download/v0.5.4-custom-r1/dust-0.5.4-x86_64-apple-darwin.tar.xz"
+      sha256 "7ac513ef6a3e6b1a3c515b7005869e0a17e6e62a3557d3c5e568a58a3d58fd94"
+    end
   end
   license "Apache-2.0"
   head "https://github.com/bootandy/dust.git"
