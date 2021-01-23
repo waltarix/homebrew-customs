d221f8e442b5d1cee0daba9ddc52f28e4d4476c2
waltari <gitting.crew@gmail.com>
2021-01-23 17:34:32 +0900

    choose: add ARM version

--- choose.rb
+++ choose.rb
@@ -2,13 +2,18 @@ class Choose < Formula
   desc "Human-friendly and fast alternative to cut and (sometimes) awk"
   homepage "https://github.com/theryangeary/choose"
   if OS.linux?
-    url "https://github.com/waltarix/choose/releases/download/v1.3.1-custom/choose-1.3.1-linux.tar.xz"
-    sha256 "7d001996edbec89d21e9102c53f634d7b87b0a1b0c80588cc851d3560c19498e"
+    url "https://github.com/waltarix/choose/releases/download/v1.3.1-custom-r1/choose-1.3.1-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "ab1bfb229b54b76b0bbf17af269ef20b4f9df556edcc1d8055cf62a82a673209"
   else
-    url "https://github.com/waltarix/choose/releases/download/v1.3.1-custom/choose-1.3.1-darwin.tar.xz"
-    sha256 "60457a5ed109587a4eae4808dfab794abbce7ff581f6f5274172834bd5268f2e"
+    if Hardware::CPU.arm?
+      url "https://github.com/waltarix/choose/releases/download/v1.3.1-custom-r1/choose-1.3.1-aarch64-apple-darwin.tar.xz"
+      sha256 "1bd14f2b01d6d790ac74f770c7b49a38a941067f11f44d3a4dae8f03f534ba4b"
+    else
+      url "https://github.com/waltarix/choose/releases/download/v1.3.1-custom-r1/choose-1.3.1-x86_64-apple-darwin.tar.xz"
+      sha256 "20c9c3a90324a12f39a5954fe5c704ed579096c8db7920d398ae48b9717b7111"
+    end
   end
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   bottle :unneeded
 
