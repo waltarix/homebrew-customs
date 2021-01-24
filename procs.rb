1a8e8688242ae82c4642098f736476cc481266dc
waltari <gitting.crew@gmail.com>
2021-01-24 17:18:32 +0900

    procs: add ARM version

--- procs.rb
+++ procs.rb
@@ -2,11 +2,16 @@ class Procs < Formula
   desc "Modern replacement for ps written by Rust"
   homepage "https://github.com/dalance/procs"
   if OS.linux?
-    url "https://github.com/waltarix/procs/releases/download/v0.10.10-custom/procs-0.10.10-linux.tar.xz"
-    sha256 "0c9e5696ada22d94071e1e8aca5630d3c8a334dafe7f35311463f859541a48be"
+    url "https://github.com/waltarix/procs/releases/download/v0.10.10-custom-r1/procs-0.10.10-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "482c14804affa9583d686924f47ce78e3c896d971706c9c9c3b5befda0f7137b"
   else
-    url "https://github.com/waltarix/procs/releases/download/v0.10.10-custom/procs-0.10.10-darwin.tar.xz"
-    sha256 "baf3955e6cd67849f9ba842427839df2c4944cb5b2d9601d3593e66bdfc535a9"
+    if Hardware::CPU.arm?
+      url "https://github.com/waltarix/procs/releases/download/v0.10.10-custom-r1/procs-0.10.10-aarch64-apple-darwin.tar.xz"
+      sha256 "f542ded44a8e30a0a94e82015e509f1cc3ddb5f81eba708304c7df526588fbda"
+    else
+      url "https://github.com/waltarix/procs/releases/download/v0.10.10-custom-r1/procs-0.10.10-x86_64-apple-darwin.tar.xz"
+      sha256 "483785bab6e57d9c34dc6d7256bc857f8cdc5d7eca68e1f74e6ed7a8d4c524fb"
+    end
   end
   license "MIT"
 
