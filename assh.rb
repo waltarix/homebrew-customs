5a7c637794ef248fa91e503c44e3bb94c7b3a4af
waltari <gitting.crew@gmail.com>
2021-02-02 15:20:27 +0900

    assh: 2.11.0

--- assh.rb
+++ assh.rb
@@ -2,17 +2,18 @@ class Assh < Formula
   desc "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts"
   homepage "https://manfred.life/assh"
   if OS.linux?
-    url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r1/assh-2.11.0-linux_amd64.tar.xz"
-    sha256 "dd41da7754a071ddf08b3020f5b5dd7a9d949da92affc5de28edff763265421b"
+    url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r2/assh-2.11.0-linux_amd64.tar.xz"
+    sha256 "ffeee53ac56c0c919c4c0862f9c26cf01902c6dcabfb59f04984637ddb2f3275"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r1/assh-2.11.0-darwin_arm64.tar.xz"
-      sha256 "f8ce5207fdeac2136287199664f6e3218df8e3d219d2eec8e646772e5632645d"
+      url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r2/assh-2.11.0-darwin_arm64.tar.xz"
+      sha256 "89ae8c6b0623a0aef25368e60fdd2ee55ffc83c9ff3d0b5a6a096c42a512b40e"
     else
-      url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r1/assh-2.11.0-darwin_amd64.tar.xz"
-      sha256 "974efddfb8d3a905fb48458a853e2c8f7032cfb9621babb256a659f6d67d7dd0"
+      url "https://github.com/waltarix/assh/releases/download/v2.11.0-custom-r2/assh-2.11.0-darwin_amd64.tar.xz"
+      sha256 "78afeb6f83b9ff74c071a10682254f200bc90cd91e109ea0d5910e9b935c535c"
     end
   end
+  version "2.11.0"
   license "MIT"
 
   bottle :unneeded
