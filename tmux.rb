e8a6ad60afbb6f59ad6ea676dc95c44cd283d33c
waltari <gitting.crew@gmail.com>
2021-03-07 15:11:22 +0900

    tmux: 3.2-rc4

--- tmux.rb
+++ tmux.rb
@@ -1,8 +1,8 @@
 class Tmux < Formula
   desc "Terminal multiplexer"
   homepage "https://tmux.github.io/"
-  url "https://github.com/waltarix/tmux/releases/download/3.2-rc-custom/tmux-3.2-rc3.tar.xz"
-  sha256 "e6b2a17a675467120afbe06dd55e65f4111cf85e800015c37f84cd8889dc5b78"
+  url "https://github.com/waltarix/tmux/releases/download/3.2-rc4-custom/tmux-3.2-rc4.tar.xz"
+  sha256 "0f6b494f638cf2866051f777fbc094d59251eb726ed72dd6d8f1f05752731118"
   license "ISC"
 
   livecheck do
@@ -29,6 +29,8 @@ class Tmux < Formula
     url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/f5d53239f7658f8e8fbaf02535cc369009c436d6/completions/tmux"
     sha256 "b5f7bbd78f9790026bbff16fc6e3fe4070d067f58f943e156bd1a8c3c99f6a6f"
   end
+
+  patch :DATA
 
   def install
     args = %W[
@@ -65,3 +67,18 @@ class Tmux < Formula
     system "#{bin}/tmux", "-V"
   end
 end
+
+__END__
+diff --git a/cmd-display-menu.c b/cmd-display-menu.c
+index 5a5aabcd..6886d443 100644
+--- a/cmd-display-menu.c
++++ b/cmd-display-menu.c
+@@ -50,7 +50,7 @@ const struct cmd_entry cmd_display_popup_entry = {
+ 	.name = "display-popup",
+ 	.alias = "popup",
+ 
+-	.args = { "Cc:d:Eh:t:w:x:y:", 0, -1 },
++	.args = { "Cc:d:Eh:t:w:x:y:KR", 0, -1 },
+ 	.usage = "[-CE] [-c target-client] [-d start-directory] [-h height] "
+ 	         CMD_TARGET_PANE_USAGE " [-w width] "
+ 	         "[-x position] [-y position] [command]",
