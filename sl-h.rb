a3191cdff7e8cf5861b7e81c24e967d28d3d8f71
waltari <gitting.crew@gmail.com>
2021-04-02 16:54:01 +0900

    sl-h: use debian instead of ubuntu

--- sl-h.rb
+++ sl-h.rb
@@ -1,8 +1,8 @@
 class SlH < Formula
   desc "Prints a steam locomotive if you type sl instead of ls"
-  homepage "https://github.com/mtoyoda/sl"
+  homepage "https://packages.debian.org/stretch/sl"
   url "https://deb.debian.org/debian/pool/main/s/sl/sl_3.03.orig.tar.gz"
-  version "3.03-17build2"
+  version "3.03-17+b2"
   sha256 "5986d9d47ea5e812d0cbd54a0fc20f127a02d13b45469bb51ec63856a5a6d3aa"
 
   bottle :unneeded
@@ -10,13 +10,13 @@ class SlH < Formula
   depends_on "ncurses"
 
   resource "debian" do
-    url "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/sl/3.03-17build2/sl_3.03-17build2.debian.tar.xz"
-    sha256 "7a5ad04027f1ad902c67800f97982e2ebdbf75b39989408b66db817dba56a44e"
+    url "https://deb.debian.org/debian/pool/main/s/sl/sl_3.03-17.debian.tar.gz"
+    sha256 "f51b223c84729f48633b78e784c72752a333c3a1ff74441cf661f7db65bb008e"
   end
 
   patch do
-    url "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/sl/3.03-17build2/sl_3.03-17build2.debian.tar.xz"
-    sha256 "7a5ad04027f1ad902c67800f97982e2ebdbf75b39989408b66db817dba56a44e"
+    url "https://deb.debian.org/debian/pool/main/s/sl/sl_3.03-17.debian.tar.gz"
+    sha256 "f51b223c84729f48633b78e784c72752a333c3a1ff74441cf661f7db65bb008e"
     apply "patches/modify_Makefile.patch",
           "patches/remove_SIGINT.patch",
           "patches/add_-e_option.patch",
@@ -25,9 +25,6 @@ class SlH < Formula
   end
 
   def install
-    ncurses = Formula["ncurses"]
-    ENV.append "CFLAGS", "-I#{ncurses.opt_include}"
-    ENV.append "LDFLAGS", "-L#{ncurses.opt_lib}"
     ENV.append "LDFLAGS", "-lncursesw"
 
     system "make", "-e"
@@ -37,6 +34,6 @@ class SlH < Formula
   end
 
   test do
-    system "#{bin}/sl", "-c"
+    system "#{bin}/sl-h", "-c"
   end
 end
