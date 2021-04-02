f274ed7b6b09fbb097f4aba93e8188f7ffdbc416
waltari <gitting.crew@gmail.com>
2021-04-02 17:25:28 +0900

    docker-completion-zsh: 20.10.5

--- docker-completion-zsh.rb
+++ docker-completion-zsh.rb
@@ -1,26 +1,19 @@
 class DockerCompletionZsh < Formula
-  desc "Zsh completion for Docker"
+  desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
-  url "https://raw.githubusercontent.com/docker/docker-ce/v19.03.12/components/cli/contrib/completion/zsh/_docker"
-  sha256 "cdecf8b89d8fc5e796266782a59fd21adb69340a47513746f15b1ba0cc196725"
+  url "https://raw.githubusercontent.com/docker/cli/v20.10.5/contrib/completion/zsh/_docker"
+  sha256 "22d7922aca0c389d8980bec08426764e75fa63883d114069e24e460651e0e9ba"
   license "Apache-2.0"
 
   bottle :unneeded
 
-  conflicts_with "docker",
-    because: "docker already includes this completion script"
-  conflicts_with "docker-completion",
-    because: "docker-completion already includes this completion script"
+  conflicts_with "docker", because: "docker already includes this completion script"
+  conflicts_with "docker-completion", because: "docker-completion already includes this completion script"
 
   patch :DATA
 
   def install
     zsh_completion.install "_docker"
-  end
-
-  test do
-    assert_match "-F _docker",
-      shell_output("bash -c 'source #{bash_completion}/docker && complete -p docker'")
   end
 end
 
