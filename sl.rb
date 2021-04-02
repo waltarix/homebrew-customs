ab5292b819a7ca93aba910c67e0bd3937a374d4d
waltari <gitting.crew@gmail.com>
2021-04-02 16:53:47 +0900

    sl: update homepage

--- sl.rb
+++ sl.rb
@@ -1,6 +1,6 @@
 class Sl < Formula
   desc "Prints a steam locomotive if you type sl instead of ls"
-  homepage "https://github.com/mtoyoda/sl"
+  homepage "https://packages.debian.org/sid/sl"
   url "https://deb.debian.org/debian/pool/main/s/sl/sl_5.02.orig.tar.gz"
   version "5.02-1"
   sha256 "1e5996757f879c81f202a18ad8e982195cf51c41727d3fea4af01fdcbbb5563a"
