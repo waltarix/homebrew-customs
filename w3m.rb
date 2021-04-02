d0987c4c39cb173dc7a36928e46077dd712c6a91
waltari <gitting.crew@gmail.com>
2021-04-02 17:01:25 +0900

    w3m: 0.5.3+git20210102-6

--- w3m.rb
+++ w3m.rb
@@ -1,9 +1,23 @@
 class W3m < Formula
   desc "Pager/text based browser"
-  homepage "https://w3m.sourceforge.io/"
-  url "http://deb.debian.org/debian/pool/main/w/w3m/w3m_0.5.3+git20210102.orig.tar.xz"
-  version "0.5.3+git20210102-2"
+  homepage "https://packages.debian.org/sid/w3m"
+  url "https://deb.debian.org/debian/pool/main/w/w3m/w3m_0.5.3+git20210102.orig.tar.xz"
+  version "0.5.3+git20210102-6"
   sha256 "32fcf47999a4fab59021382d382add86fe87159d9e3a95bddafda246ae12f5f9"
+
+  patch do
+    url "https://deb.debian.org/debian/pool/main/w/w3m/w3m_0.5.3+git20210102-6.debian.tar.xz"
+    sha256 "ef6f835be35815580bc0f4683e7dd2e6f4ce411072d12dbccbd52051b5fa8770"
+    apply "patches/010_section.patch",
+          "patches/030_str-overflow.patch",
+          "patches/040_libwc-overflow.patch"
+
+  end
+
+  livecheck do
+    url "https://deb.debian.org/debian/pool/main/w/w3m/"
+    regex(/href=.*?w3m[._-]v?(\d+(?:\.\d+)+)\.orig\.t/i)
+  end
 
   bottle :unneeded
 
@@ -13,15 +27,10 @@ class W3m < Formula
   depends_on "openssl@1.1"
   depends_on "waltarix/customs/cmigemo"
   depends_on "zlib"
+
   on_linux do
     depends_on "gettext"
     depends_on "libbsd"
-  end
-
-  patch do
-    url "http://deb.debian.org/debian/pool/main/w/w3m/w3m_0.5.3+git20210102-2.debian.tar.xz"
-    sha256 "ac9a1035bb354387dd618c3610ce98ab4d3e43163ea3561643fa613e781e3244"
-    apply "patches/010_section.patch"
   end
 
   def install
@@ -29,16 +38,18 @@ class W3m < Formula
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
 
     system "./configure", "--prefix=#{prefix}",
-                          "--with-gc",
-                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}",
-                          "--with-termlib=ncursesw",
-                          "--with-migemo='cmigemo -c -q'",
                           "--disable-image",
-                          "--enable-m17n", "--enable-unicode", "--enable-nls"
+                          "--enable-m17n",
+                          "--enable-nls",
+                          "--enable-unicode",
+                          "--with-gc",
+                          "--with-migemo='cmigemo -c -q'",
+                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}",
+                          "--with-with-termlib=ncursesw"
     system "make", "install"
   end
 
   test do
-    assert_match /DuckDuckGo/, shell_output("#{bin}/w3m -dump https://duckduckgo.com")
+    assert_match "DuckDuckGo", shell_output("#{bin}/w3m -dump https://duckduckgo.com")
   end
 end
