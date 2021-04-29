8f5ac37d10a728d1a635c21e38a1ff58ef284698
waltari <gitting.crew@gmail.com>
2021-04-29 17:14:37 +0900

    less: 581.2

--- less.rb
+++ less.rb
@@ -1,13 +1,13 @@
 class Less < Formula
   desc "Pager program similar to more"
   homepage "https://www.greenwoodsoftware.com/less/index.html"
-  url "https://github.com/waltarix/less/releases/download/v581-custom/less-581.tar.gz"
-  sha256 "3408f64db3cfe2472c251ce424ed1ed0c3ac7ef5b65335006a5e1219ac55dee5"
+  url "https://github.com/waltarix/less/releases/download/v581.2-custom/less-581.2.tar.gz"
+  sha256 "9231bd41c39d657a4e9688b259f5b69db4a5d59d45f82e166948dfc9cca5e67e"
   license "GPL-3.0-or-later"
 
   livecheck do
     url :homepage
-    regex(/less[._-]v?(\d+).+?released.+?general use/i)
+    regex(/less[._-]v?(\d+(?:\.\d+)*).+?released.+?general use/i)
   end
 
   bottle :unneeded
