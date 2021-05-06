ac2c238ef1c47fceb022f14ce2ab2832576fcbeb
waltari <gitting.crew@gmail.com>
2021-05-06 20:19:37 +0900

    procs: 0.11.5

--- procs.rb
+++ procs.rb
@@ -2,15 +2,15 @@ class Procs < Formula
   desc "Modern replacement for ps written by Rust"
   homepage "https://github.com/dalance/procs"
   if OS.linux?
-    url "https://github.com/waltarix/procs/releases/download/v0.11.4-custom/procs-0.11.4-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "75d9930d251e43bceca20e6ac58539f30878a92f52a42a905d12775ca1f68266"
+    url "https://github.com/waltarix/procs/releases/download/v0.11.5-custom/procs-0.11.5-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "cd2f5e58de47454e68bd8542b8ca60545a97f355a546b99bb19c8f5fe0b491f4"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/procs/releases/download/v0.11.4-custom/procs-0.11.4-aarch64-apple-darwin.tar.xz"
-      sha256 "ccbd16c0dd2ef7957a489de314cc3fc65c601d24230629d849e32f0ab2da3526"
+      url "https://github.com/waltarix/procs/releases/download/v0.11.5-custom/procs-0.11.5-aarch64-apple-darwin.tar.xz"
+      sha256 "2cfafd8f666e748fcd655f86da175787a86ca60ba0d9c8f76538785c1b12c9fb"
     else
-      url "https://github.com/waltarix/procs/releases/download/v0.11.4-custom/procs-0.11.4-x86_64-apple-darwin.tar.xz"
-      sha256 "7a9edd482bf8ff9c27c217da0f59deacda4ffbd037af55bcdf3ed542bdf93e4e"
+      url "https://github.com/waltarix/procs/releases/download/v0.11.5-custom/procs-0.11.5-x86_64-apple-darwin.tar.xz"
+      sha256 "fce44cb978f6e3964e63a5258e465d0083eccc09e4c48acc280a2f00eb61c5e9"
     end
   end
   license "MIT"
@@ -19,6 +19,13 @@ class Procs < Formula
 
   def install
     bin.install "procs"
+
+    system "#{bin}/procs", "--completion", "bash"
+    system "#{bin}/procs", "--completion", "fish"
+    system "#{bin}/procs", "--completion", "zsh"
+    bash_completion.install "procs.bash" => "procs"
+    fish_completion.install "procs.fish"
+    zsh_completion.install "_procs"
   end
 
   test do
