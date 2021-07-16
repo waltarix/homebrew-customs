5ce72bc2c5a355890b73201d0f94d347c5ee2a30
waltari <gitting.crew@gmail.com>
2021-07-16 17:56:42 +0900

    add openssh-minimal formula

---
+++ openssh-minimal.rb
@@ -0,0 +1,61 @@
+class OpensshMinimal < Formula
+  desc "OpenBSD freely-licensed SSH connectivity tools"
+  homepage "https://www.openssh.com/"
+  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.6p1.tar.gz"
+  mirror "https://mirror.vdms.io/pub/OpenBSD/OpenSSH/portable/openssh-8.6p1.tar.gz"
+  version "8.6p1"
+  sha256 "c3e6e4da1621762c850d03b47eed1e48dff4cc9608ddeb547202a234df8ed7ae"
+  license "SSH-OpenSSH"
+
+  livecheck do
+    url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/"
+    regex(/href=.*?openssh[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
+  end
+
+  bottle :unneeded
+
+  conflicts_with "openssh", because: "both install the same binaries"
+
+  # Please don't resubmit the keychain patch option. It will never be accepted.
+  # https://archive.is/hSB6d#10%25
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+  depends_on "zlib"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}/ssh
+      --with-ssl-dir=#{Formula["openssl@1.1"].opt_prefix}
+      --without-ldns
+      --without-libedit
+      --without-kerberos5
+      --without-pam
+      --without-security-key-builtin
+    ]
+
+    on_linux do
+      args << "--with-privsep-path=#{var}/lib/sshd"
+    end
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+
+    # This was removed by upstream with very little announcement and has
+    # potential to break scripts, so recreate it for now.
+    # Debian have done the same thing.
+    bin.install_symlink bin/"ssh" => "slogin"
+  end
+
+  test do
+    assert_match "OpenSSH_", shell_output("#{bin}/ssh -V 2>&1")
+
+    port = free_port
+    fork { exec sbin/"sshd", "-D", "-p", port.to_s }
+    sleep 2
+    assert_match "sshd", shell_output("lsof -i :#{port}")
+  end
+end
