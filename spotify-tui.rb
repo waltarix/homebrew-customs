70f9e2c52dc18b6f3ff7019b3100ad7ee7ddf14b
waltari <gitting.crew@gmail.com>
2021-08-25 11:01:29 +0900

    spotify-tui: 0.25.0

--- spotify-tui.rb
+++ spotify-tui.rb
@@ -2,32 +2,39 @@ class SpotifyTui < Formula
   desc "Terminal-based client for Spotify"
   homepage "https://github.com/Rigellute/spotify-tui"
   if OS.linux?
-    url "https://github.com/waltarix/spotify-tui/releases/download/v0.24.0-custom/spotify-tui-0.24.0-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "2bd1959a07cb4b53fbb048bdfa9274acb34603721a82577b5772d115e5aa3dfb"
+    url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom/spotify-tui-0.25.0-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "69d231190445cef0af45acbecfceab27edcde49dd69410ff432cb5349f6179b3"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/spotify-tui/releases/download/v0.24.0-custom/spotify-tui-0.24.0-aarch64-apple-darwin.tar.xz"
-      sha256 "70d95fab1a0ffa14c68a3903a81872829a1704db7c8fe60b56bbd84d00f7cfbd"
+      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom/spotify-tui-0.25.0-aarch64-apple-darwin.tar.xz"
+      sha256 "b24d2efecef122db480cef91ce8a74b270833b45cb4c1f1daa20f353566538b7"
     else
-      url "https://github.com/waltarix/spotify-tui/releases/download/v0.24.0-custom/spotify-tui-0.24.0-x86_64-apple-darwin.tar.xz"
-      sha256 "7e36fb383eafcdfab9694417a0f73904c008b335586d4adbe633b610083a5c56"
+      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom/spotify-tui-0.25.0-x86_64-apple-darwin.tar.xz"
+      sha256 "f07611e29e1a3b20b6672dca930b95425a274cee0ebd6ed1dc762dc6c03b46a2"
     end
   end
   license "MIT"
-  head "https://github.com/Rigellute/spotify-tui.git"
+  head "https://github.com/Rigellute/spotify-tui.git", branch: "master"
 
   bottle :unneeded
+
+  on_linux do
+    depends_on "libxcb"
+    depends_on "openssl@1.1"
+  end
 
   def install
     bin.install "spt"
   end
 
   test do
-    pid = fork { exec "#{bin}/spt -c #{testpath/"client.yml"} 2>&1 > output" }
+    output = testpath/"output"
+    fork do
+      $stdout.reopen(output)
+      $stderr.reopen(output)
+      exec "#{bin}/spt -c #{testpath}/client.yml"
+    end
     sleep 10
-    assert_match "Enter your Client ID", File.read("output")
-  ensure
-    Process.kill "TERM", pid
-    quiet_system "pkill", "-9", "-f", "spt"
+    assert_match "Enter your Client ID", output.read
   end
 end
