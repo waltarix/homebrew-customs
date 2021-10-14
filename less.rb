db66f9fec076d45b346174e5edbf2fa227ab396a
waltari <gitting.crew@gmail.com>
2021-10-14 21:04:54 +0900

    less: 590_1

--- less.rb
+++ less.rb
@@ -1,9 +1,10 @@
 class Less < Formula
   desc "Pager program similar to more"
   homepage "https://www.greenwoodsoftware.com/less/index.html"
-  url "https://github.com/waltarix/less/releases/download/v590-custom/less-590.tar.gz"
-  sha256 "408037d32a1cdc58af7c840007bd0549513b155d5f862c158a392f25b9ce154d"
+  url "https://github.com/waltarix/less/releases/download/v590-custom-r1/less-590.tar.gz"
+  sha256 "e03e26845bc9d2a5e2f1055b2faf3c11d026598f6fb33e1324185d813861151f"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
