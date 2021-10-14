9403e11f1cfe31ea47801b9cb9cfaecfc6d69894
waltari <gitting.crew@gmail.com>
2021-10-14 20:13:25 +0900

    fzf: 0.27.2_1

--- fzf.rb
+++ fzf.rb
@@ -4,28 +4,29 @@ class Fzf < Formula
   url "https://github.com/junegunn/fzf/archive/0.27.2.tar.gz"
   sha256 "7798a9e22fc363801131456dc21026ccb0f037aed026d17df60b1178b3f24111"
   license "MIT"
+  revision 1
   head "https://github.com/junegunn/fzf.git"
 
   bottle :unneeded
 
   resource "binary" do
     if OS.linux?
-      url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom/fzf-0.27.2-linux_amd64.tar.xz"
-      sha256 "6cf1e32e8676d2baf29ca41cc133a3381013e9d989d5f133c4082ee9031beae4"
+      url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom-r3/fzf-0.27.2-linux_amd64.tar.xz"
+      sha256 "991858baf6591fb579a1d2f32d64b8f9f2583dc5a2da89e521dbb00e75000820"
     else
       if Hardware::CPU.arm?
-        url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom/fzf-0.27.2-darwin_arm64.tar.xz"
-        sha256 "f21060bf0622940d161954e1b0851aee0559e50a39e6ebf8f0fa8cc135aff918"
+        url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom-r3/fzf-0.27.2-darwin_arm64.tar.xz"
+        sha256 "d59a9fce44ed4b36128c60f091cf2409e187f2edf88657e5bdba0014d8d98c60"
       else
-        url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom/fzf-0.27.2-darwin_amd64.tar.xz"
-        sha256 "eaf90eafb722f9ffb73c3b84e373b74c7c66ac1bc54f0427d0e1775b7bcaa780"
+        url "https://github.com/waltarix/fzf/releases/download/0.27.2-custom-r3/fzf-0.27.2-darwin_amd64.tar.xz"
+        sha256 "d5cb9e8169597900eacb35c2986ed124d3157a6425593bfc130ee69c1c8903b3"
       end
     end
   end
 
   def install
     resource("binary").stage do
-      bin.install ["bin/fzf", "bin/fzf-tmux"]
+      bin.install ["fzf", "bin/fzf-tmux"]
       (prefix/"plugin").install "plugin/fzf.vim"
     end
 
