46512c6af9669661fc25db810bf808afb23bd918
waltari <gitting.crew@gmail.com>
2021-12-13 20:21:12 +0900

    mosh: 1.3.2_19

--- mosh.rb
+++ mosh.rb
@@ -1,10 +1,10 @@
 class Mosh < Formula
   desc "Remote terminal application"
   homepage "https://mosh.org"
-  url "https://github.com/waltarix/mosh/releases/download/mosh-1.3.2-custom/mosh-1.3.2-custom.tar.xz"
-  sha256 "768b9e093d62224641711078eb920acc8c82bd101c62b3b515f28f6dcc50e367"
+  url "https://github.com/waltarix/mosh/releases/download/mosh-1.3.2-custom-r1/mosh-1.3.2-custom.tar.xz"
+  sha256 "f0a0166d89566dd190dc8e1dcd15bc3234df4ef3c4ba57991c34c3b42fc70624"
   license "GPL-3.0-or-later"
-  revision 18
+  revision 19
 
   depends_on "pkg-config" => :build
   depends_on "protobuf"
@@ -23,7 +23,16 @@ class Mosh < Formula
     # PATH to support launching outside shell e.g. via launcher
     inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
 
-    system "./configure", "--prefix=#{prefix}", "--enable-completion"
+    args = %W[
+      --prefix=#{prefix}
+      --enable-completion
+    ]
+
+    on_linux do
+      args << "--with-utempter"
+    end
+
+    system "./configure", *args
     system "make", "install"
   end
 
