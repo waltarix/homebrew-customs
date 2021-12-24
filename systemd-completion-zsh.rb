51a022571c07b9e54a0ff0d4ec92ec5720616ef2
waltari <gitting.crew@gmail.com>
2021-12-25 08:38:00 +0900

    systemd-completion-zsh: 249

--- systemd-completion-zsh.rb
+++ systemd-completion-zsh.rb
@@ -1,8 +1,8 @@
 class SystemdCompletionZsh < Formula
   desc "System and service manager"
   homepage "https://wiki.freedesktop.org/www/Software/systemd/"
-  url "https://github.com/systemd/systemd/archive/v246.tar.gz"
-  sha256 "4268bd88037806c61c5cd1c78d869f7f20bf7e7368c63916d47b5d1c3411bd6f"
+  url "https://github.com/systemd/systemd/archive/v249.tar.gz"
+  sha256 "174091ce5f2c02123f76d546622b14078097af105870086d18d55c1c2667d855"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   def install
