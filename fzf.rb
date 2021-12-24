8a01fae38da4678986228d7d9324251e0da0c7d4
waltari <gitting.crew@gmail.com>
2021-12-25 08:50:17 +0900

    fzf: 0.29.0

--- fzf.rb
+++ fzf.rb
@@ -1,22 +1,22 @@
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.28.0.tar.gz"
-  sha256 "05bbfa4dd84b72e55afc3fe56c0fc185d6dd1fa1c4eef56a1559b68341f3d029"
+  url "https://github.com/junegunn/fzf/archive/0.29.0.tar.gz"
+  sha256 "a287a8806ce56d764100c5a6551721e16649fd98325f6bf112e96fb09fe3031b"
   license "MIT"
   head "https://github.com/junegunn/fzf.git"
 
   resource "binary" do
     if OS.linux?
-      url "https://github.com/waltarix/fzf/releases/download/0.28.0-custom/fzf-0.28.0-linux_amd64.tar.xz"
-      sha256 "941ea4e40e8861467d64ea0fc502124f9e8190120e291c630a60f82e83597f20"
+      url "https://github.com/waltarix/fzf/releases/download/0.29.0-custom/fzf-0.29.0-linux_amd64.tar.xz"
+      sha256 "f9f95f50f7297aecd3c47928d57bbedbb35512a1d62611d85dd20e8b1567ea88"
     else
       if Hardware::CPU.arm?
-        url "https://github.com/waltarix/fzf/releases/download/0.28.0-custom/fzf-0.28.0-darwin_arm64.tar.xz"
-        sha256 "4caeff4fe205ed9641265ae683e8efd52c7de94d668ede5b96874d2f63812953"
+        url "https://github.com/waltarix/fzf/releases/download/0.29.0-custom/fzf-0.29.0-darwin_arm64.tar.xz"
+        sha256 "759551380916bb9b666fa52a06487eb98c5cb98131bafd0ca5dfffab1e332545"
       else
-        url "https://github.com/waltarix/fzf/releases/download/0.28.0-custom/fzf-0.28.0-darwin_amd64.tar.xz"
-        sha256 "c242166ca3a8af5b66c10364cff822855a75c15b6f165a45ead5b07efc2bec46"
+        url "https://github.com/waltarix/fzf/releases/download/0.29.0-custom/fzf-0.29.0-darwin_amd64.tar.xz"
+        sha256 "3e92d5ab92d0c85eafeb02f5a7e0014fa0ac4c5af13311448fc164efe227669a"
       end
     end
   end
@@ -45,6 +45,6 @@ class Fzf < Formula
 
   test do
     (testpath/"list").write %w[hello world].join($INPUT_RECORD_SEPARATOR)
-    assert_equal "world", shell_output("cat #{testpath}/list | #{bin}/fzf -f wld").chomp
+    assert_equal "world", pipe_output("#{bin}/fzf -f wld", (testpath/"list").read).chomp
   end
 end
