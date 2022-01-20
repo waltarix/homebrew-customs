5fe9a1df848a58a8ed10a0bcfc147a62e1d922cd
waltari <gitting.crew@gmail.com>
2022-01-20 09:41:08 +0900

    miller: 6.0.0_1

--- miller.rb
+++ miller.rb
@@ -2,22 +2,24 @@ class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
   if OS.linux?
-    url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom/mlr-6.0.0-linux_amd64.tar.xz"
-    sha256 "1d02565d551606054f63a756a25220521f9be4dd525a2f8b4e2b57d5314aa7d2"
+    url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom-r1/mlr-6.0.0-linux_amd64.tar.xz"
+    sha256 "d565677c5b016dce89afdc9d61c58327f39695634156f098a32a42f782b24b38"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom/mlr-6.0.0-darwin_arm64.tar.xz"
-      sha256 "78fe26b578fd2a6b42dc1b3d50330d9353ef39907634b2971ccbe54919d1e5cc"
+      url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom-r1/mlr-6.0.0-darwin_arm64.tar.xz"
+      sha256 "2cc5e0ca10825b9972e7c608fd61264bb3c3d6f627cdc61f7d2e544c7f61fd15"
     else
-      url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom/mlr-6.0.0-darwin_amd64.tar.xz"
-      sha256 "411bba6ba03c236ef8ff62da0557696a217f7c6701789429a76bdf1e553eb699"
+      url "https://github.com/waltarix/miller/releases/download/v6.0.0-custom-r1/mlr-6.0.0-darwin_amd64.tar.xz"
+      sha256 "3fa5fea57b2af50358eb45fc5ea8d5ac5492ceaf1f539606beed04ab25cef8b2"
     end
   end
   version "6.0.0"
   license "BSD-2-Clause"
+  revision 1
 
   def install
     bin.install "mlr"
+    man1.install "mlr.1"
   end
 
   test do
