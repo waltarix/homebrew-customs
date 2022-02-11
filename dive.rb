a4f38ac6e156645786d758b4326b640649858bd9
waltari <gitting.crew@gmail.com>
2022-02-11 18:34:27 +0900

    dive: 0.10.0_1

--- dive.rb
+++ dive.rb
@@ -2,19 +2,20 @@ class Dive < Formula
   desc "Tool for exploring each layer in a docker image"
   homepage "https://github.com/wagoodman/dive"
   if OS.linux?
-    url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom/dive-0.10.0-linux_amd64.tar.xz"
-    sha256 "6262aea4c8665381e37614473f3618345e9aa53444819ff89903d15e7fde2141"
+    url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom-r1/dive-0.10.0-linux_amd64.tar.xz"
+    sha256 "dca975f5c8a9d37526784a3361151726263458b587c67e96c4c9822d1e623ba9"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom/dive-0.10.0-darwin_arm64.tar.xz"
-      sha256 "7eaf11a3bf843580e42ef6a585dcac8abd40583f62a2abdc0a472bd85318859a"
+      url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom-r1/dive-0.10.0-darwin_arm64.tar.xz"
+      sha256 "d00d1072a4c4fec862bc92402985f3712ba9ff9a13355706f33a89969607db72"
     else
-      url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom/dive-0.10.0-darwin_amd64.tar.xz"
-      sha256 "6011a3345ea792af0950cbda8701546c0e178f92504f2f7eb30c336e050e2901"
+      url "https://github.com/waltarix/dive/releases/download/v0.10.0-custom-r1/dive-0.10.0-darwin_amd64.tar.xz"
+      sha256 "7c0a83ea1c68bc0994d1d1a97347730971b3c1e36a92010ba649faa08cf3d108"
     end
   end
   version "0.10.0"
   license "MIT"
+  revision 1
 
   on_linux do
     depends_on "device-mapper"
