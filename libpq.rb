476339fa8de058e44b5087689e99c5a05afa0a81
waltari <gitting.crew@gmail.com>
2022-02-13 16:36:59 +0900

    add custom libpq formula

---
+++ libpq.rb
@@ -0,0 +1,86 @@
+class Libpq < Formula
+  desc "Postgres C API library"
+  homepage "https://www.postgresql.org/docs/14/libpq.html"
+  url "https://ftp.postgresql.org/pub/source/v14.2/postgresql-14.2.tar.bz2"
+  sha256 "2cf78b2e468912f8101d695db5340cf313c2e9f68a612fb71427524e8c9a977a"
+  license "PostgreSQL"
+
+  livecheck do
+    formula "postgresql"
+  end
+
+  keg_only "conflicts with postgres formula"
+
+  # GSSAPI provided by Kerberos.framework crashes when forked.
+  # See https://github.com/Homebrew/homebrew-core/issues/47494.
+  depends_on "krb5"
+
+  depends_on "openssl@1.1"
+
+  on_linux do
+    depends_on "readline"
+  end
+
+  resource "wcwidth9.h" do
+    url "https://github.com/waltarix/localedata/releases/download/14.0.0-r2/wcwidth9.h"
+    sha256 "8ce9e402611a0f8c2a44130571d9043144d43463893e13a6459d0b2c22b67eb2"
+  end
+
+  def install
+    resource("wcwidth9.h").stage(buildpath/"src/common")
+    inreplace "src/common/wchar.c" do |s|
+      s.gsub! %r@(?<=^#include "mb/pg_wchar\.h")@, %@\n#include "wcwidth9.h"@
+      s.gsub! /(?<=return )ucs_wcwidth/, "wcwidth9"
+    end
+
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-gssapi",
+                          "--with-openssl",
+                          "--libdir=#{opt_lib}",
+                          "--includedir=#{opt_include}"
+    dirs = %W[
+      libdir=#{lib}
+      includedir=#{include}
+      pkgincludedir=#{include}/postgresql
+      includedir_server=#{include}/postgresql/server
+      includedir_internal=#{include}/postgresql/internal
+    ]
+    system "make"
+    system "make", "-C", "src/bin", "install", *dirs
+    system "make", "-C", "src/include", "install", *dirs
+    system "make", "-C", "src/interfaces", "install", *dirs
+    system "make", "-C", "src/common", "install", *dirs
+    system "make", "-C", "src/port", "install", *dirs
+    system "make", "-C", "doc", "install", *dirs
+  end
+
+  test do
+    (testpath/"libpq.c").write <<~EOS
+      #include <stdlib.h>
+      #include <stdio.h>
+      #include <libpq-fe.h>
+
+      int main()
+      {
+          const char *conninfo;
+          PGconn     *conn;
+
+          conninfo = "dbname = postgres";
+
+          conn = PQconnectdb(conninfo);
+
+          if (PQstatus(conn) != CONNECTION_OK) // This should always fail
+          {
+              printf("Connection to database attempted and failed");
+              PQfinish(conn);
+              exit(0);
+          }
+
+          return 0;
+        }
+    EOS
+    system ENV.cc, "libpq.c", "-L#{lib}", "-I#{include}", "-lpq", "-o", "libpqtest"
+    assert_equal "Connection to database attempted and failed", shell_output("./libpqtest")
+  end
+end
