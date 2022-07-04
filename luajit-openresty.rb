e273be46b37003d44b5f3f5d46ac1563291c52a5
waltari <gitting.crew@gmail.com>
2022-07-04 17:04:08 +0900

    add custom luajit-openresty formula

---
+++ luajit-openresty.rb
@@ -0,0 +1,71 @@
+class LuajitOpenresty < Formula
+  desc "OpenResty's Branch of LuaJIT 2"
+  homepage "https://github.com/openresty/luajit2"
+  url "https://github.com/openresty/luajit2/archive/refs/tags/v2.1-20220411.tar.gz"
+  sha256 "d3f2c870f8f88477b01726b32accab30f6e5d57ae59c5ec87374ff73d0794316"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/openresty/luajit2.git", branch: "v2.1-agentzh"
+
+  # The latest LuaJIT release is unstable (2.1.0-beta3, from 2017-05-01) and
+  # OpenResty is making releases using the latest LuaJIT Git commits. With this
+  # in mind, the regex below is very permissive and will match any tags
+  # starting with a numeric version, ensuring that we match unstable versions.
+  # We should consider restricting the regex to stable versions if it ever
+  # becomes feasible in the future.
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:[.-]\d+)+[^{}]*)/i)
+  end
+
+  keg_only "it conflicts with the LuaJIT formula"
+
+  def install
+    ENV["HOMEBREW_OPTIMIZATION_LEVEL"] = "O3"
+
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.change_make_var! "CCOPT_x86", ""
+    end
+
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    args = %W[
+      PREFIX=#{prefix}
+      XCFLAGS=-DLUAJIT_ENABLE_GC64
+    ]
+
+    system "make", "amalg", *args
+    system "make", "install", *args
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib" if OS.mac?
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+    end
+
+    # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_rf d }
+  end
+
+  test do
+    system "#{bin}/luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+  end
+end
