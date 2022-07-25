48a2b3f300779025ac0bdf6555c9c68917219e27
waltari <gitting.crew@gmail.com>
2022-07-25 18:12:43 +0900

    less: 590_2

--- less.rb
+++ less.rb
@@ -1,10 +1,10 @@
 class Less < Formula
   desc "Pager program similar to more"
   homepage "https://www.greenwoodsoftware.com/less/index.html"
-  url "https://github.com/waltarix/less/releases/download/v590-custom-r1/less-590.tar.gz"
-  sha256 "e03e26845bc9d2a5e2f1055b2faf3c11d026598f6fb33e1324185d813861151f"
+  url "https://github.com/waltarix/less/releases/download/v590-custom-r2/less-590.tar.gz"
+  sha256 "cb2c6e353e53a1149f72d12c39fdb8f86436e4497a93e457e55ffd3d1efc6782"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   livecheck do
     url :homepage
@@ -16,6 +16,9 @@ class Less < Formula
   depends_on "waltarix/customs/cmigemo"
 
   def install
+    ENV["HOMEBREW_OPTIMIZATION_LEVEL"] = "O3"
+    ENV.append "CFLAGS", "-flto"
+
     system "./configure", "--prefix=#{prefix}", "--with-regex=pcre2"
     system "make", "install"
   end
