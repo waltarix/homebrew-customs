b694de0e04b836ca2aafdc3f55530f616914a260
waltari <gitting.crew@gmail.com>
2022-10-23 21:23:37 +0900

    choose 1.4.0

--- choose.rb
+++ choose.rb
@@ -2,15 +2,15 @@ class Choose < Formula
   desc "Human-friendly and fast alternative to cut and (sometimes) awk"
   homepage "https://github.com/theryangeary/choose"
   if OS.linux?
-    url "https://github.com/waltarix/choose/releases/download/v1.3.4-custom/choose-1.3.4-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "43a38d0e2fae46ccf38fc689d34d2395192da74ea1be993befa65e1750206d05"
+    url "https://github.com/waltarix/choose/releases/download/v1.4.0-custom/choose-1.4.0-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "588c9e90aa2a0ff6e31be325a33fad3df879ed3001a13e204012646bf13360e8"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/choose/releases/download/v1.3.4-custom/choose-1.3.4-aarch64-apple-darwin.tar.xz"
-      sha256 "1a783888d95a3fd822b3d23412de346bde3493bc345e33838c46b18993d345b4"
+      url "https://github.com/waltarix/choose/releases/download/v1.4.0-custom/choose-1.4.0-aarch64-apple-darwin.tar.xz"
+      sha256 "f2a54e46c37eb2ab286bb184f5612b33217f5d39bec9432d65fb886a9112d05e"
     else
-      url "https://github.com/waltarix/choose/releases/download/v1.3.4-custom/choose-1.3.4-x86_64-apple-darwin.tar.xz"
-      sha256 "912456ea7f8199cce0da99f786aece76260c14b78ff3b59ba5b74c448a1bae1b"
+      url "https://github.com/waltarix/choose/releases/download/v1.4.0-custom/choose-1.4.0-x86_64-apple-darwin.tar.xz"
+      sha256 "9eb919bd2ef189d0c4a5f885343acb3121e32873e716032bcfc48d87d5ed5d1b"
     end
   end
   license "GPL-3.0-or-later"
@@ -20,6 +20,10 @@ class Choose < Formula
 
   def install
     bin.install "choose"
+
+    generate_completions_from_executable(bin/"choose", "--completion")
+
+    (man1/"choose.1").write Utils.safe_popen_read(bin/"choose", "--manpage")
   end
 
   test do
