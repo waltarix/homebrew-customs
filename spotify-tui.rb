f98c1ac4261d5ff8852bc65446bdb2a434c3a800
waltari <gitting.crew@gmail.com>
2022-11-13 20:51:43 +0900

    spotify-tui 0.25.0_2

--- spotify-tui.rb
+++ spotify-tui.rb
@@ -2,19 +2,19 @@ class SpotifyTui < Formula
   desc "Terminal-based client for Spotify"
   homepage "https://github.com/Rigellute/spotify-tui"
   if OS.linux?
-    url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r1/spotify-tui-0.25.0-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "8883240c297bbf14968d45449bf4fd2e8e4d08effd3ab6714fb24eb1fc8a2cac"
+    url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r2/spotify-tui-0.25.0-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "69fa4f3c4a15f25bf9fcd937fea62094422fcc35ed34749590ae7a2ee99ea3b0"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r1/spotify-tui-0.25.0-aarch64-apple-darwin.tar.xz"
-      sha256 "f6391cec604d26a33dccd32a6416fd73156014b6af6b826a2c3b6a987ba1ed75"
+      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r2/spotify-tui-0.25.0-aarch64-apple-darwin.tar.xz"
+      sha256 "21e5447a78273b248a1db6bcae02b703483c5c9a6a9c22d1c2cd9492d2086482"
     else
-      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r1/spotify-tui-0.25.0-x86_64-apple-darwin.tar.xz"
-      sha256 "03c5df0eda05d63cc33fab162ee4d9596f074e64311536246047495d86107293"
+      url "https://github.com/waltarix/spotify-tui/releases/download/v0.25.0-custom-r2/spotify-tui-0.25.0-x86_64-apple-darwin.tar.xz"
+      sha256 "e8498b70a4ab6f588e951d491104aa93100a1f8843e9bd460849c6e9a6d980f7"
     end
   end
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/Rigellute/spotify-tui.git", branch: "master"
 
   on_linux do
