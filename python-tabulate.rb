03fa51cda4e02199052c73f7202c3a3f3c33d65a
waltari <gitting.crew@gmail.com>
2022-11-29 18:20:45 +0900

    python-tabulate 0.9.0

--- python-tabulate.rb
+++ python-tabulate.rb
@@ -1,27 +1,41 @@
 class PythonTabulate < Formula
   desc "Pretty-print tabular data in Python"
-  homepage "https://pypi.org/project/tabulate/"
-  url "https://github.com/waltarix/python-tabulate/archive/v0.8.10-custom-r1.tar.gz"
-  version "0.8.10"
-  sha256 "a8fd8689cb647fce7927ea9cc1c62b23020cb34d935179663fb4fcb6e04b21cd"
+  homepage "https://github.com/astanin/python-tabulate"
+  url "https://github.com/waltarix/python-tabulate/releases/download/v0.9.0-custom-r1/tabulate-0.9.0.tar.gz"
+  sha256 "729256af87086e35d830cb033a072ced1ad835b40cc7a6ca92200f40379d267d"
   license "MIT"
-  revision 3
 
-  depends_on "python@3.11"
-  depends_on "waltarix/customs/libpython-tabulate"
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+  depends_on "waltarix/customs/libpython-wcwidth"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
 
   def install
-    # Install the binary only, the lib part is provided by libpython-tabulate
-    system "python3.11", "setup.py", "--no-user-cfg", "install_scripts", "--install-dir=#{bin}", "--skip-build"
+    inreplace "pyproject.toml", /^dynamic .+$/, "version = \"#{version}\""
+
+    pythons.each do |python|
+      system python, "-m", "pip", "install", "--prefix=#{prefix}", "--no-deps", "."
+    end
   end
 
   test do
-    input = <<~EOS
+    pythons.each do |python|
+      system python, "-c", "from tabulate import tabulate"
+    end
+
+    (testpath/"in.txt").write <<~EOS
       one,two,three
       …,……,………
       ...,......,.........
     EOS
-    expected_output = <<~EOS
+
+    (testpath/"out.txt").write <<~EOS
       ╭─────┬────────┬───────────╮
       │ one │ two    │ three     │
       ├─────┼────────┼───────────┤
@@ -29,6 +43,8 @@ class PythonTabulate < Formula
       │ ... │ ...... │ ......... │
       ╰─────┴────────┴───────────╯
     EOS
-    assert_equal expected_output, pipe_output("#{bin}/tabulate -s, -1 -g0 -f fancy_outline_rounded", input)
+
+    assert_equal (testpath/"out.txt").read,
+      shell_output("#{bin}/tabulate -s, -1 -g0 -f rounded_outline #{testpath}/in.txt")
   end
 end
