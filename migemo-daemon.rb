e14728e55d6aab393eaddfab9e412a0069c906c2
waltari <gitting.crew@gmail.com>
2022-12-19 16:21:46 +0900

    migemo-daemon: rebuild
    * migemo-daemon: use service block
    * migemo-daemon: depends on ruby

--- migemo-daemon.rb
+++ migemo-daemon.rb
@@ -5,41 +5,23 @@ class MigemoDaemon < Formula
   version "20200605"
   sha256 "0e15bbd80877ae7485e1eedd225badc02b86804367866a05fd167dc7d76acc9f"
 
+  depends_on "ruby"
   depends_on "waltarix/customs/cmigemo"
 
   def install
     libexec.install "migemo_daemon.rb" => "migemo-daemon"
-
     chmod 0555, libexec/"migemo-daemon"
-    env = {
-      PATH: "#{Formula["cmigemo"].opt_bin}:$PATH",
-    }
-    (bin/"migemo-daemon").write_env_script(libexec/"migemo-daemon", env)
+    (bin/"migemo-daemon").write_env_script libexec/"migemo-daemon",
+      PATH: "#{Formula["cmigemo"].opt_bin}:#{Formula["ruby"].opt_bin}:$PATH"
   end
 
-  plist_options manual: "#{HOMEBREW_PREFIX}/opt/migemo_daemon/bin/migemo-daemon"
+  plist_options startup: true
 
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>KeepAlive</key>
-        <true/>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{bin}/migemo-daemon</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{HOMEBREW_PREFIX}</string>
-      </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"migemo-daemon"]
+    run_type :immediate
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
   end
 
   test do
