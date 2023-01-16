8e42eed16c2461fc3a44bfa6f96ddbd8b1173a88
waltari <gitting.crew@gmail.com>
2023-01-16 09:11:55 +0900

    busybox-manpage 1.36.0

--- busybox-manpage.rb
+++ busybox-manpage.rb
@@ -1,13 +1,13 @@
 class BusyboxManpage < Formula
   desc "Man page for Busybox"
   homepage "https://busybox.net/"
-  url "https://busybox.net/downloads/busybox-1.35.0.tar.bz2"
-  sha256 "faeeb244c35a348a334f4a59e44626ee870fb07b6884d68c10ae8bc19f83a694"
+  url "https://busybox.net/downloads/busybox-1.36.0.tar.bz2"
+  sha256 "542750c8af7cb2630e201780b4f99f3dcceeb06f505b479ec68241c1e6af61a5"
   license "GPL-2.0"
 
   livecheck do
     url :homepage
-    regex(/BusyBox\s+(\d+(?:\.\d+)*)\s+\(stable\)/i)
+    regex(/BusyBox\s+(\d+(?:\.\d+)*)\s+\((?:un)?stable\)/i)
   end
 
   on_macos do
