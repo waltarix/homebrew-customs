209d235b7a7a7f6776e3ad0cd2f536b5df882bf3
waltari <gitting.crew@gmail.com>
2023-01-30 16:07:02 +0900

    dust 0.8.4

--- dust.rb
+++ dust.rb
@@ -2,19 +2,18 @@ class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
   if OS.linux?
-    url "https://github.com/waltarix/dust/releases/download/v0.8.3-custom/dust-0.8.3-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "2161f0324999ce50027684e6cdc73c3338252df85f46ee3d0765f8348def42ea"
+    url "https://github.com/waltarix/dust/releases/download/v0.8.4-custom/dust-0.8.4-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "6e1cfdc0981063654a7c33565140f787a10fda75c2230f263897eb8f8f53501e"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/dust/releases/download/v0.8.3-custom/dust-0.8.3-aarch64-apple-darwin.tar.xz"
-      sha256 "8de0e5a00e9af4341393f00dbbef2ec2ecb63c3af03057b41c911b4e0d31fc8e"
+      url "https://github.com/waltarix/dust/releases/download/v0.8.4-custom/dust-0.8.4-aarch64-apple-darwin.tar.xz"
+      sha256 "9a97cb601f35de5b2ce83c6d37a1d2ab4965db8b2c2dbb7866ee1351998df366"
     else
-      url "https://github.com/waltarix/dust/releases/download/v0.8.3-custom/dust-0.8.3-x86_64-apple-darwin.tar.xz"
-      sha256 "e342fb5513d504245e15e742bbef4fed8757bad7244e6e5419a126e95c0291c3"
+      url "https://github.com/waltarix/dust/releases/download/v0.8.4-custom/dust-0.8.4-x86_64-apple-darwin.tar.xz"
+      sha256 "ab4bcfcbcb1c94ae635019e4a2fb15e2af2fa804da6891a82e851a07a98d5581"
     end
   end
   license "Apache-2.0"
-  head "https://github.com/bootandy/dust.git", branch: "master"
 
   livecheck do
     url :stable
@@ -29,6 +28,12 @@ class Dust < Formula
   end
 
   test do
-    assert_match(/\d+.+?\./, shell_output("#{bin}/dust -n 1"))
+    # failed with Linux CI run, but works with local run
+    # https://github.com/Homebrew/homebrew-core/pull/121789#issuecomment-1407749790
+    if OS.linux?
+      system bin/"dust", "-n", "1"
+    else
+      assert_match(/\d+.+?\./, shell_output("#{bin}/dust -n 1"))
+    end
   end
 end
