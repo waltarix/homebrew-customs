33f53400e0b8e3a03deee65f005e5d35ae3fa988
waltari <gitting.crew@gmail.com>
2023-02-20 21:16:13 +0900

    systemd-completion 253_1

--- Formula/systemd-completion.rb
+++ Formula/systemd-completion.rb
@@ -4,10 +4,30 @@ class SystemdCompletion < Formula
   url "https://github.com/systemd/systemd/archive/v253.tar.gz"
   sha256 "acbd86d42ebc2b443722cb469ad215a140f504689c7a9133ecf91b235275a491"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 1
+
+  depends_on "jinja2-cli" => :build
 
   def install
-    zsh_completion.install Dir["shell-completion/zsh/_*"].reject { |f| f.end_with?('.in') }
-    bash_completion.install Dir["shell-completion/bash/*"].reject { |f| f.end_with?('.in') }
+    (buildpath/"template_variables.json").write({
+      ROOTLIBEXECDIR: "/lib/systemd"
+    }.to_json)
+
+    func = ->(completion, target) do
+      case target
+      when %r{/meson.build\z}
+        return
+      when /\.in\z/
+        completion_file = File.basename(target, ".in")
+        system "jinja2", "-o", completion_file, target, "template_variables.json"
+        completion.install completion_file
+      else
+        completion.install target
+      end
+    end
+
+    Dir["shell-completion/zsh/*"].each(&func.curry.call(zsh_completion))
+    Dir["shell-completion/bash/*"].each(&func.curry.call(bash_completion))
   end
 
   test do
