e5ea2920b2ee9b6b8c4ad702e7ea4b6dafb7014c
waltari <gitting.crew@gmail.com>
2023-03-18 11:26:06 +0900

    dufs 0.33.0

--- Formula/dufs.rb
+++ Formula/dufs.rb
@@ -2,15 +2,15 @@ class Dufs < Formula
   desc "Static file server"
   homepage "https://github.com/sigoden/dufs"
   if OS.linux?
-    url "https://github.com/waltarix/dufs/releases/download/v0.32.0-custom/dufs-0.32.0-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "eedfb933f669c508c0d89757773961c162a52fb46c922381da5a052c17514fcb"
+    url "https://github.com/waltarix/dufs/releases/download/v0.33.0-custom/dufs-0.33.0-x86_64-unknown-linux-musl.tar.xz"
+    sha256 "43ae4f4549ad9a86dfacfe651b97c2fc7ab9f00825f70963bfa413745cf0bc70"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/waltarix/dufs/releases/download/v0.32.0-custom/dufs-0.32.0-aarch64-apple-darwin.tar.xz"
-      sha256 "21381d0a767968912c47abf1c832cb21d35d5adccc518920e38f649d664e2d3b"
+      url "https://github.com/waltarix/dufs/releases/download/v0.33.0-custom/dufs-0.33.0-aarch64-apple-darwin.tar.xz"
+      sha256 "ba86ccea091fb74d712a69ea82314c70afb87d8d5bbbc5b4b24a9cc701093fdd"
     else
-      url "https://github.com/waltarix/dufs/releases/download/v0.32.0-custom/dufs-0.32.0-x86_64-apple-darwin.tar.xz"
-      sha256 "e95ba5d0f0f8c1fb69b94267289d91dfe8ae6b766642ba237add1c330dc364a5"
+      url "https://github.com/waltarix/dufs/releases/download/v0.33.0-custom/dufs-0.33.0-x86_64-apple-darwin.tar.xz"
+      sha256 "f3084ae456190808a1996e408d4261e8c2726b9c0419ddba7452e5dcab6863b7"
     end
   end
   license any_of: ["Apache-2.0", "MIT"]
