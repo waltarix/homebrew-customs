7e18417906a6654ba4d8ae5edb84b12ca15d17b3
waltari <gitting.crew@gmail.com>
2023-04-08 09:48:15 +0900

    neovim 0.9.0

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -1,8 +1,8 @@
 class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
-  url "https://github.com/neovim/neovim/archive/v0.8.2.tar.gz"
-  sha256 "c516c8db73e1b12917a6b2e991b344d0914c057cef8266bce61a2100a28ffcc9"
+  url "https://github.com/neovim/neovim/archive/v0.9.0.tar.gz"
+  sha256 "39d79107c54d2f3babcad2cd157c399241c04f6e75e98c18e8afaf2bb5e82937"
   license "Apache-2.0"
 
   livecheck do
@@ -12,29 +12,24 @@ class Neovim < Formula
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
   depends_on "gettext"
-  depends_on "libtermkey"
-  depends_on "libuv"
   depends_on "msgpack"
-  depends_on "unibilium"
   depends_on "waltarix/customs/libtree-sitter"
   depends_on "waltarix/customs/libvterm"
   depends_on "waltarix/customs/luajit"
-  depends_on "waltarix/customs/luv"
 
   uses_from_macos "unzip" => :build
 
-  on_linux do
-    depends_on "libnsl"
+  on_macos do
+    depends_on "libiconv"
   end
 
   # Keep resources updated according to:
   # https://github.com/neovim/neovim/blob/v#{version}/cmake.deps/cmake/BuildLuarocks.cmake
 
   resource "mpack" do
-    url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.9/libmpack-lua-1.0.9.tar.gz"
-    sha256 "0fd07e709c3f6f201c2ffc9f77cef1b303b02c12413f0c15670a32bf6c959e9e"
+    url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.10/libmpack-lua-1.0.10.tar.gz"
+    sha256 "18e202473c9a255f1d2261b019874522a4f1c6b6f989f80da93d7335933e8119"
   end
 
   resource "lpeg" do
@@ -43,19 +38,20 @@ class Neovim < Formula
   end
 
   resource "luarocks" do
-    url "https://luarocks.org/releases/luarocks-3.9.1.tar.gz"
-    sha256 "ffafd83b1c42aa38042166a59ac3b618c838ce4e63f4ace9d961a5679ef58253"
+    url "https://luarocks.org/releases/luarocks-3.9.2.tar.gz"
+    sha256 "bca6e4ecc02c203e070acdb5f586045d45c078896f6236eb46aa33ccd9b94edb"
   end
 
   resource "wcwidth9.h" do
-    url "https://github.com/waltarix/localedata/releases/download/15.0.0/wcwidth9.h"
-    sha256 "a18bd4ddc6a27e9f7a9c9ba273bf3a120846f31fe32f00972aa7987d21e3154d"
+    url "https://github.com/waltarix/localedata/releases/download/15.0.0-r4/wcwidth9.h"
+    sha256 "81974cfee64faece46162923a3ed3a70b9dfb7723005103730718bf2dded6ab5"
   end
 
   patch :DATA
 
   def install
     ENV["HOMEBREW_OPTIMIZATION_LEVEL"] = "O3"
+    ENV.append "LDFLAGS", "-Wl,-s"
 
     resource("luarocks").stage do
       system "./configure", "--prefix=#{buildpath}/luarocks",
@@ -84,7 +80,7 @@ class Neovim < Formula
 
     cd "deps-build/build/src" do
       %w[
-        mpack/mpack-1.0.9-0.rockspec
+        mpack/mpack-1.0.10-0.rockspec
         lpeg/lpeg-1.0.2-1.src.rock
       ].each do |rock|
         dir, rock = rock.split("/")
@@ -92,10 +88,6 @@ class Neovim < Formula
           output = Utils.safe_popen_read("luarocks", "unpack", lua_path, rock, "--tree=#{buildpath}/deps-build")
           unpack_dir = output.split("\n")[-2]
           cd unpack_dir do
-            on_macos do
-              inreplace "lmpack.c", "#define _XOPEN_SOURCE 500", "#define _C99_SOURCE 1" if dir == "mpack"
-            end
-
             system "luarocks", "make", lua_path, "--tree=#{buildpath}/deps-build"
           end
         end
@@ -111,19 +103,23 @@ class Neovim < Formula
       end
     end
 
+    ENV.deparallelize
+
+    system "cmake", "-S", "cmake.deps", "-B", ".deps", "-G", "Ninja",
+                    "-DUSE_BUNDLED=OFF",
+                    "-DUSE_BUNDLED_LIBTERMKEY=ON",
+                    "-DUSE_BUNDLED_LIBUV=ON",
+                    "-DUSE_BUNDLED_LUV=ON",
+                    "-DUSE_BUNDLED_UNIBILIUM=ON",
+                    *std_cmake_args
+    system "cmake", "--build", ".deps"
+
     system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
-                    "-DLIBLUV_LIBRARY=#{Formula["luv"].opt_lib/shared_library("libluv")}",
-                    "-DLIBUV_LIBRARY=#{Formula["libuv"].opt_lib/shared_library("libuv")}",
+                    "-DNVIM_VERSION_MEDIUM=v#{version}",
                     *std_cmake_args
 
-    # Patch out references to Homebrew shims
-    inreplace "build/cmake.config/auto/versiondef.h", Superenv.shims_path/ENV.cc, ENV.cc
-
-    ENV.deparallelize
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-
-    rm share/"nvim/runtime/filetype.vim"
   end
 
   test do
@@ -135,57 +131,145 @@ class Neovim < Formula
 end
 
 __END__
+diff --git a/runtime/lua/vim/lsp.lua b/runtime/lua/vim/lsp.lua
+index 2d39f2d45..b72edede4 100644
+--- a/runtime/lua/vim/lsp.lua
++++ b/runtime/lua/vim/lsp.lua
+@@ -59,6 +59,7 @@ lsp._request_name_to_capability = {
+   ['textDocument/references'] = { 'referencesProvider' },
+   ['textDocument/rangeFormatting'] = { 'documentRangeFormattingProvider' },
+   ['textDocument/formatting'] = { 'documentFormattingProvider' },
++  ['textDocument/diagnostic'] = { 'diagnosticProvider' },
+   ['textDocument/completion'] = { 'completionProvider' },
+   ['textDocument/documentHighlight'] = { 'documentHighlightProvider' },
+   ['textDocument/semanticTokens/full'] = { 'semanticTokensProvider' },
+@@ -607,6 +608,9 @@ do
+           },
+           contentChanges = changes,
+         })
++        vim.schedule(function()
++          vim.lsp.diagnostic.pull_diagnostic(bufnr, client)
++        end)
+       end
+     end
+   end
+@@ -722,6 +726,8 @@ local function text_document_did_open_handler(bufnr, client)
+   client.notify('textDocument/didOpen', params)
+   util.buf_versions[bufnr] = params.textDocument.version
+ 
++  vim.lsp.diagnostic.pull_diagnostic(bufnr, client)
++
+   -- Next chance we get, we should re-do the diagnostics
+   vim.schedule(function()
+     -- Protect against a race where the buffer disappears
+diff --git a/runtime/lua/vim/lsp/diagnostic.lua b/runtime/lua/vim/lsp/diagnostic.lua
+index 3efa5c51f..fd18323b7 100644
+--- a/runtime/lua/vim/lsp/diagnostic.lua
++++ b/runtime/lua/vim/lsp/diagnostic.lua
+@@ -236,6 +236,25 @@ function M.on_publish_diagnostics(_, result, ctx, config)
+   vim.diagnostic.set(namespace, bufnr, diagnostic_lsp_to_vim(diagnostics, bufnr, client_id))
+ end
+ 
++function M.pull_diagnostic(bufnr, client)
++  if not client.supports_method('textDocument/diagnostic') then
++    return
++  end
++
++  client.request(
++    'textDocument/diagnostic',
++    { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
++    function(err, result)
++      if err or result == nil then
++        return
++      end
++
++      local namespace = M.get_namespace(client.id)
++      vim.diagnostic.set(namespace, bufnr, diagnostic_lsp_to_vim(result.items, bufnr, client.id))
++    end
++  )
++end
++
+ --- Clear diagnostics and diagnostic cache.
+ ---
+ --- Diagnostic producers should prefer |vim.diagnostic.reset()|. However,
+diff --git a/runtime/lua/vim/lsp/protocol.lua b/runtime/lua/vim/lsp/protocol.lua
+index f4489ad17..04372cbfa 100644
+--- a/runtime/lua/vim/lsp/protocol.lua
++++ b/runtime/lua/vim/lsp/protocol.lua
+@@ -807,6 +807,10 @@ function protocol.make_client_capabilities()
+           end)(),
+         },
+       },
++      diagnostic = {
++        dynamicRegistration = false,
++        relatedDocumentSupport = false,
++      },
+       callHierarchy = {
+         dynamicRegistration = false,
+       },
 diff --git a/scripts/download-unicode-files.sh b/scripts/download-unicode-files.sh
-index 4482cefa3..4830eddd0 100755
+index f0fd4c66e..c4938a537 100755
 --- a/scripts/download-unicode-files.sh
 +++ b/scripts/download-unicode-files.sh
-@@ -1,11 +1,12 @@
+@@ -1,14 +1,15 @@
  #!/bin/sh
  
  set -e
 -data_files="UnicodeData.txt CaseFolding.txt EastAsianWidth.txt"
 +data_files="UnicodeData.txt CaseFolding.txt"
  emoji_files="emoji-data.txt"
+ files="'$data_files $emoji_files'"
  
  UNIDIR_DEFAULT=src/unicode
 -DOWNLOAD_URL_BASE_DEFAULT='http://unicode.org/Public'
-+UNICODE_VERSION="15.0.0"
-+DOWNLOAD_URL_BASE_DEFAULT="https://unicode.org/Public/${UNICODE_VERSION}/ucd"
++UNIDIR_VERSION=15.0.0
++DOWNLOAD_URL_BASE_DEFAULT="https://www.unicode.org/Public/$UNIDIR_VERSION/ucd"
  
- if test x$1 = 'x--help' ; then
+-if test "$1" = '--help' ; then
++if test "$1" = '--help'; then
    echo 'Usage:'
-@@ -22,22 +23,12 @@ UNIDIR=${1:-$UNIDIR_DEFAULT}
+   echo "  $0[ TARGET_DIRECTORY[ URL_BASE]]"
+   echo
+@@ -23,14 +24,13 @@ fi
+ UNIDIR=${1:-$UNIDIR_DEFAULT}
  DOWNLOAD_URL_BASE=${2:-$DOWNLOAD_URL_BASE_DEFAULT}
  
- for filename in $data_files ; do
+-for filename in $data_files ; do
 -  curl -L -o "$UNIDIR/$filename" "$DOWNLOAD_URL_BASE/UNIDATA/$filename"
--  (
--    cd "$UNIDIR"
--    git add $filename
--  )
+-  git -C "$UNIDIR" add "$filename"
++for filename in $data_files; do
 +  curl -# -L -o "$UNIDIR/$filename" "$DOWNLOAD_URL_BASE/$filename"
  done
  
- for filename in $emoji_files ; do
+-for filename in $emoji_files ; do
 -  curl -L -o "$UNIDIR/$filename" "$DOWNLOAD_URL_BASE/UNIDATA/emoji/$filename"
--  (
--    cd "$UNIDIR"
--    git add $filename
--  )
+-  git -C "$UNIDIR" add "$filename"
++for filename in $emoji_files; do
 +  curl -# -L -o "$UNIDIR/$filename" "$DOWNLOAD_URL_BASE/emoji/$filename"
  done
  
--(
--  cd "$UNIDIR"
--  git commit -m "feat: update unicode tables" -- $files
--)
+-git -C "$UNIDIR" commit -m "feat: update unicode tables" .
 +curl -# -L -o "$UNIDIR/EastAsianWidth.txt" \
-+  "https://github.com/waltarix/localedata/releases/download/15.0.0/EastAsianWidth.txt"
++  "https://github.com/waltarix/localedata/releases/download/${UNIDIR_VERSION}-r4/EastAsianWidth.txt"
+diff --git a/src/nvim/api/ui.c b/src/nvim/api/ui.c
+index 5dd858a6a..ee4459961 100644
+--- a/src/nvim/api/ui.c
++++ b/src/nvim/api/ui.c
+@@ -874,9 +874,6 @@ void remote_ui_raw_line(UI *ui, Integer grid, Integer row, Integer startcol, Int
+       remote_ui_cursor_goto(ui, row, startcol + i);
+       remote_ui_highlight_set(ui, attrs[i]);
+       remote_ui_put(ui, (const char *)chunk[i]);
+-      if (utf_ambiguous_width(utf_ptr2char((char *)chunk[i]))) {
+-        data->client_col = -1;  // force cursor update
+-      }
+     }
+     if (endcol < clearcol) {
+       remote_ui_cursor_goto(ui, row, endcol);
 diff --git a/src/nvim/generators/gen_unicode_tables.lua b/src/nvim/generators/gen_unicode_tables.lua
-index 36553f464..6c5cef62a 100644
+index 9ad99c802..e6c3569b1 100644
 --- a/src/nvim/generators/gen_unicode_tables.lua
 +++ b/src/nvim/generators/gen_unicode_tables.lua
-@@ -317,8 +317,7 @@ eaw_fp:close()
+@@ -318,8 +318,7 @@ eaw_fp:close()
  
  local doublewidth = build_width_table(ut_fp, dataprops, widthprops,
                                        {W=true, F=true}, 'doublewidth')
@@ -196,27 +280,32 @@ index 36553f464..6c5cef62a 100644
  local emoji_fp = io.open(emoji_fname, 'r')
  local emojiprops = parse_emoji_props(emoji_fp)
 diff --git a/src/nvim/mbyte.c b/src/nvim/mbyte.c
-index 33d652a51..827c15b9f 100644
+index 6ab7aee29..b9a79203a 100644
 --- a/src/nvim/mbyte.c
 +++ b/src/nvim/mbyte.c
-@@ -74,6 +74,8 @@ struct interval {
- # include "unicode_tables.generated.h"
+@@ -87,6 +87,8 @@ struct interval {
  #endif
+ // uncrustify:on
  
 +#include "wcwidth9.h"
 +
- static char e_list_item_nr_is_not_list[]
+ static const char e_list_item_nr_is_not_list[]
    = N_("E1109: List item %d is not a List");
- static char e_list_item_nr_does_not_contain_3_numbers[]
-@@ -469,30 +471,16 @@ static bool intable(const struct interval *table, size_t n_items, int c)
+ static const char e_list_item_nr_does_not_contain_3_numbers[]
+@@ -477,33 +479,17 @@ static bool intable(const struct interval *table, size_t n_items, int c)
+ ///       gen_unicode_tables.lua, which must be manually invoked as needed.
  int utf_char2cells(int c)
  {
-   if (c >= 0x100) {
+-  // Use the value from setcellwidths() at 0x80 and higher, unless the
+-  // character is not printable.
+-  if (c >= 0x80 && vim_isprintc(c)) {
 -    int n = cw_value(c);
 -    if (n != 0) {
 -      return n;
 -    }
+-  }
 -
+   if (c >= 0x100) {
 -    if (!utf_printable(c)) {
 +    int n = wcwidth9(c);
 +    if (n < 0) {
@@ -228,27 +317,57 @@ index 33d652a51..827c15b9f 100644
 -    if (p_emoji && intable(emoji_wide, ARRAY_SIZE(emoji_wide), c)) {
 -      return 2;
 -    }
+-  } else if (c >= 0x80 && !vim_isprintc(c)) {
+-    // Characters below 0x100 are influenced by 'isprint' option.
+-    return 4;                   // unprintable, displays <xx>
 +    return n;
-   } else if (c >= 0x80 && !vim_isprintc(c)) {
-     // Characters below 0x100 are influenced by 'isprint' option.
-     return 4;                   // unprintable, displays <xx>
    }
  
 -  if (c >= 0x80 && *p_ambw == 'd'
 -      && intable(ambiguous, ARRAY_SIZE(ambiguous), c)) {
 -    return 2;
--  }
--
++  if (c >= 0x80 && !vim_isprintc(c)) {
++    // Characters below 0x100 are influenced by 'isprint' option.
++    return 4;                   // unprintable, displays <xx>
+   }
+ 
    return 1;
+@@ -1162,12 +1148,6 @@ int utf_class_tab(const int c, const uint64_t *const chartab)
+   return 2;
  }
  
-@@ -1162,8 +1150,7 @@ int utf_class_tab(const int c, const uint64_t *const chartab)
- 
- bool utf_ambiguous_width(int c)
- {
+-bool utf_ambiguous_width(int c)
+-{
 -  return c >= 0x80 && (intable(ambiguous, ARRAY_SIZE(ambiguous), c)
 -                       || intable(emoji_all, ARRAY_SIZE(emoji_all), c));
-+  return c >= 0x80 && (intable(emoji_all, ARRAY_SIZE(emoji_all), c));
+-}
+-
+ // Generic conversion function for case operations.
+ // Return the converted equivalent of "a", which is a UCS-4 character.  Use
+ // the given conversion "table".  Uses binary search on "table".
+diff --git a/src/nvim/tui/tui.c b/src/nvim/tui/tui.c
+index df7c87ad6..f60ddc756 100644
+--- a/src/nvim/tui/tui.c
++++ b/src/nvim/tui/tui.c
+@@ -836,8 +836,7 @@ static void print_cell_at_pos(TUIData *tui, int row, int col, UCell *cell, bool
+ 
+   cursor_goto(tui, row, col);
+ 
+-  bool is_ambiwidth = utf_ambiguous_width(utf_ptr2char(cell->data));
+-  if (is_ambiwidth && is_doublewidth) {
++  if (is_doublewidth) {
+     // Clear the two screen cells.
+     // If the character is single-width in the host terminal it won't change the second cell.
+     update_attrs(tui, cell->attr);
+@@ -846,11 +845,6 @@ static void print_cell_at_pos(TUIData *tui, int row, int col, UCell *cell, bool
+   }
+ 
+   print_cell(tui, cell);
+-
+-  if (is_ambiwidth) {
+-    // Force repositioning cursor after printing an ambiguous-width character.
+-    grid->row = -1;
+-  }
  }
  
- // Generic conversion function for case operations.
+ static void clear_region(TUIData *tui, int top, int bot, int left, int right, int attr_id)
