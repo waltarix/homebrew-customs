6268eb61d1f3d6372f8fdc748b08dbaf77e3afb8
waltari <gitting.crew@gmail.com>
2023-06-10 19:24:19 +0900

    mosh: merge with upstream

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -27,10 +27,12 @@ class Mosh < Formula
 
     # https://github.com/protocolbuffers/protobuf/issues/9947
     ENV.append_to_cflags "-DNDEBUG"
+    # Keep C++ standard in sync with abseil.rb
+    ENV.append "CXXFLAGS", "-std=c++17"
 
     # teach mosh to locate mosh-client without referring
     # PATH to support launching outside shell e.g. via launcher
-    inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
+    inreplace "scripts/mosh.pl", "'mosh-client", "'#{bin}/mosh-client"
 
     args = %W[
       --prefix=#{prefix}
