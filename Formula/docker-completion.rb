0353793f12ba193dda1adfecd6f905e6649cf23c
waltari <gitting.crew@gmail.com>
2023-07-25 21:13:53 +0900

    docker-completion 24.0.5

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -1,17 +1,17 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
-  "24.0.4".tap do |v|
+  "24.0.5".tap do |v|
     if OS.linux?
       url "https://download.docker.com/linux/static/stable/x86_64/docker-#{v}.tgz"
-      sha256 "0ab79ae5f19e2ef5bdc3c3009c8b770dea6189e0f1e0ef4935d78fd30519b11d"
+      sha256 "0a5f3157ce25532c5c1261a97acf3b25065cfe25940ef491fa01d5bea18ddc86"
     else
       if Hardware::CPU.arm?
         url "https://download.docker.com/mac/static/stable/aarch64/docker-#{v}.tgz"
-        sha256 "d99ce023f984b07a57621d804f226bfeedea513ce708aba480a62f5b63631367"
+        sha256 "7a912495c00632d6982b993570893706f4299c395d162c56ed5f233559fc4644"
       else
         url "https://download.docker.com/mac/static/stable/x86_64/docker-#{v}.tgz"
-        sha256 "a1016b319d8fb5b92e6a4f9ae4082b0fe934bcec4a18f4ddba9b6a5778af230c"
+        sha256 "3ba59a8f40b77272c6e8df83f2a38ea13f48d9935e3479e5307239fccd717100"
       end
     end
   end
