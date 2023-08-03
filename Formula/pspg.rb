2748250a12aedee9d66eed8fc4e9b721744d362b
waltari <gitting.crew@gmail.com>
2023-08-03 21:32:09 +0900

    pspg 5.8.0

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/5.7.8.tar.gz"
-  sha256 "efa9225f717eef25e5e59e8957a0a9720a111f7d1e5f02c38bd36148a31dffae"
+  url "https://github.com/okbob/pspg/archive/5.8.0.tar.gz"
+  sha256 "e1043e61aa18916f0c02627a32059e0f0d68f5050a38fc36ea578d517ebb12ce"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git", branch: "master"
 
@@ -22,7 +22,7 @@ class Pspg < Formula
     ENV.append "LDFLAGS", "-Wl,-s"
 
     resource("wcwidth9.h").stage(buildpath/"src")
-    inreplace "src/unicode.c", /(?<=^#include "string\.h")/, %(\n#include "wcwidth9.h")
+    inreplace "src/unicode.c", /(?<=^#include <string\.h>)/, %(\n#include "wcwidth9.h")
     inreplace "src/unicode.c", /(?<=return )ucs_wcwidth/, "wcwidth9"
 
     system "./configure", "--disable-debug",
