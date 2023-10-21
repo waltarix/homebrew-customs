eeff2e667c6dcebbb7f6e827c931fcf1c0c73053
waltari <gitting.crew@gmail.com>
2023-10-21 22:02:46 +0900

    procs 0.14.3

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -1,17 +1,17 @@
 class Procs < Formula
   desc "Modern replacement for ps written by Rust"
   homepage "https://github.com/dalance/procs"
-  "0.14.1".tap do |v|
+  "0.14.3".tap do |v|
     if OS.linux?
       url "https://github.com/waltarix/procs/releases/download/v#{v}-custom/procs-#{v}-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "15eaa9216bd9dfd4e83f6013fa194b154cbd3dc82c0f4602d4c54a7ce4f30fc9"
+      sha256 "c8bb36a00986085f91fd0a0a514b4ba6001e8586394dd4d491c8a128875da412"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/procs/releases/download/v#{v}-custom/procs-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "1b70526a201e26a3581fdd5eedb669b70870002157e1520754883ab38ad3ecfa"
+        sha256 "3200590026066cf663cd371bfd090ad1b285ce8b326f07ed5c074ede80f74d12"
       else
         url "https://github.com/waltarix/procs/releases/download/v#{v}-custom/procs-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "af9d71c844fe668b4639124535d6b6628d0a24599d4f8d0c3079a3ea30a73374"
+        sha256 "46258fc8bfc6eafd82873c512b46b7e78507b3903bd1ec67d61267fe181781dd"
       end
     end
   end
