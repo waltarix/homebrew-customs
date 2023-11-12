b66a2f4683ed77544d588165ef3a3c030b027a7f
waltari <gitting.crew@gmail.com>
2023-11-12 10:06:22 +0900

    sd 1.0.0

--- Formula/sd.rb
+++ Formula/sd.rb
@@ -1,25 +1,28 @@
 class Sd < Formula
   desc "Intuitive find & replace CLI"
   homepage "https://github.com/chmln/sd"
-  if OS.linux?
-    url "https://github.com/waltarix/sd/releases/download/v0.8.1-custom/sd-0.8.1-x86_64-unknown-linux-musl.tar.xz"
-    sha256 "5ed1d121973318c9e28d77e4beaf594d075a651f9a1b78025f8b2c64ddae0a7b"
-  else
-    if Hardware::CPU.arm?
-      url "https://github.com/waltarix/sd/releases/download/v0.8.1-custom/sd-0.8.1-aarch64-apple-darwin.tar.xz"
-      sha256 "f257f6beb2b7bccd6b06413032b32bcc10f7e5b81ad954a1aac11877879631f4"
+  "1.0.0".tap do |v|
+    if OS.linux?
+      url "https://github.com/waltarix/sd/releases/download/v#{v}-custom/sd-#{v}-x86_64-unknown-linux-musl.tar.xz"
+      sha256 "ad4062e6c1c518d4cefb18114be07ee7ec3f6c904fbafc696209da01644218ca"
     else
-      url "https://github.com/waltarix/sd/releases/download/v0.8.1-custom/sd-0.8.1-x86_64-apple-darwin.tar.xz"
-      sha256 "c1b469d557814121759fcecb74bc3082db6fce3e085105fcd5663e0e29c68011"
+      if Hardware::CPU.arm?
+        url "https://github.com/waltarix/sd/releases/download/v#{v}-custom/sd-#{v}-aarch64-apple-darwin.tar.xz"
+        sha256 "8351212e40ae367519d3d709cdd81105c1a14c692091fe315bd69844f8aa4427"
+      else
+        url "https://github.com/waltarix/sd/releases/download/v#{v}-custom/sd-#{v}-x86_64-apple-darwin.tar.xz"
+        sha256 "9f89b87bc59b48cc647338395760e339380f7b463ba9ddfabf139f84ed573c62"
+      end
     end
   end
   license "MIT"
 
   def install
     bin.install "sd"
-    man1.install "manual/sd.1"
-
-    generate_completions_from_executable(bin/"sd", "--completion")
+    man1.install "gen/sd.1"
+    bash_completion.install "gen/completions/sd.bash" => "sd"
+    fish_completion.install "gen/completions/sd.fish"
+    zsh_completion.install "gen/completions/_sd"
   end
 
   test do
