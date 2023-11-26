c4ebd29f4671deb671f9469235d63f7bca906a0b
waltari <gitting.crew@gmail.com>
2023-11-26 16:09:51 +0900

    dprint 0.43.1

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -1,17 +1,17 @@
 class Dprint < Formula
   desc "Pluggable and configurable code formatting platform written in Rust"
   homepage "https://dprint.dev/"
-  "0.43.0".tap do |v|
+  "0.43.1".tap do |v|
     if OS.linux?
       url "https://github.com/waltarix/dprint/releases/download/#{v}-custom/dprint-#{v}-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "c098c8efc1160aaea7bdea9fb214ed110f736a7c5185325cf5609c13098bd414"
+      sha256 "4d96407e23a72af43df6279317ea9b5d3d812e695a7a528dc9543f2e0500ed14"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/dprint/releases/download/#{v}-custom/dprint-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "b524e9a20a91bbf2441feb0447ae5a54435e02838ca52b7275db599f831e0eee"
+        sha256 "f2aea43cc8e1e4f6587c2b72fbca86497f5374161e54d454c4dfd5098bab0ab3"
       else
         url "https://github.com/waltarix/dprint/releases/download/#{v}-custom/dprint-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "6f5cfb7da9c5b0dee2dad4f6b18fddca2ba75c9a0192a96fc5916616f02df624"
+        sha256 "db9fb025698c55ef6317cb93ec36339bfaa7a058536ace7ae5cd7695be45b3d6"
       end
     end
   end
