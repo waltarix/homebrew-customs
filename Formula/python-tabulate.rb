4348fba67ac001c97b481666036ffb5455638203
waltari <gitting.crew@gmail.com>
2024-07-11 18:16:39 +0900

    python-tabulate 0.9.0_1

--- Formula/python-tabulate.rb
+++ Formula/python-tabulate.rb
@@ -1,33 +1,33 @@
 class PythonTabulate < Formula
   desc "Pretty-print tabular data in Python"
   homepage "https://github.com/astanin/python-tabulate"
-  url "https://github.com/waltarix/python-tabulate/releases/download/v0.9.0-custom-r1/tabulate-0.9.0.tar.gz"
-  sha256 "729256af87086e35d830cb033a072ced1ad835b40cc7a6ca92200f40379d267d"
+  url "https://github.com/waltarix/python-tabulate/releases/download/v0.9.0-custom-r2/tabulate-0.9.0.tar.gz"
+  sha256 "f5c8bc48780a17933e60b042b0919f2b0c90c187413b1299cdabdebf4326b3fe"
   license "MIT"
+  revision 1
 
-  depends_on "python@3.10" => [:build, :test]
+  depends_on "python-setuptools" => :build
   depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.12" => [:build, :test]
-  depends_on "waltarix/customs/libpython-wcwidth"
+  depends_on "python@3.12" => [:build, :test] # FIXME: should be runtime dependency
+  depends_on "waltarix/customs/python-wcwidth"
 
   def pythons
     deps.map(&:to_formula)
         .select { |f| f.name.match?(/^python@\d\.\d+$/) }
         .sort_by(&:version)
-        .map { |f| f.opt_libexec/"bin/python" }
   end
 
   def install
-    inreplace "pyproject.toml", /^dynamic .+$/, "version = \"#{version}\""
-
     pythons.each do |python|
-      system python, "-m", "pip", "install", "--prefix=#{prefix}", "--no-deps", "."
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
     end
   end
 
   test do
     pythons.each do |python|
-      system python, "-c", "from tabulate import tabulate"
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "from tabulate import tabulate"
     end
 
     (testpath/"in.txt").write <<~EOS
