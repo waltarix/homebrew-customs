b98d9abafd24784207f54c82349590615c751f54
waltari <gitting.crew@gmail.com>
2024-07-11 18:23:59 +0900

    python-wcwidth 0.2.13

--- Formula/python-wcwidth.rb
+++ Formula/python-wcwidth.rb
@@ -1,26 +1,23 @@
 class PythonWcwidth < Formula
-  desc "Pretty-print tabular data in Python"
-  homepage "https://pypi.org/project/tabulate/"
-  url "https://github.com/waltarix/python-wcwidth/archive/0.2.8-custom-r1.tar.gz"
-  version "0.2.8"
-  sha256 "59ac8d05e43a2721511f64aa92d3c9cb7b92109740191f5e6712314ca877cd95"
+  desc "Python library that measures the width of unicode strings rendered to a terminal"
+  homepage "https://github.com/jquast/wcwidth"
+  url "https://github.com/waltarix/python-wcwidth/archive/0.2.13-custom.tar.gz"
+  version "0.2.13"
+  sha256 "62c676a02b9da3afc704b5409f5d5006f8fa9d7ea12dc9ac6308cac67fb3337c"
   license "MIT"
-  revision 1
 
-  depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
 
   def pythons
     deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .sort_by(&:version)
+        .select { |f| f.name.start_with?("python@") }
         .map { |f| f.opt_libexec/"bin/python" }
   end
 
   def install
     pythons.each do |python|
-      system python, "-m", "pip", "install", "--prefix=#{prefix}", "--no-deps", "."
+      system python, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
     end
   end
 
