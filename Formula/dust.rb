562af581d7778bfd37c7353415b72678166577cf
waltari <gitting.crew@gmail.com>
2024-07-14 22:36:01 +0900

    dust 1.0.0

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -1,17 +1,17 @@
 class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
-  "0.8.6".tap do |v|
+  "1.0.0".tap do |v|
     if OS.linux?
       url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "62f1a5f4910c675db963e79bb06a9811197a4e46e92d35d7cc3707c755b8fbac"
+      sha256 "c5c94ed2747f334c9047a2aca73904e9343c2cb8da45ac0f0d16b979980c62ce"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "024e80dc204701366a8e856b343c9886e81ea78aac80467d23f95e8db1bb8a83"
+        sha256 "adcfcb24fe5ba2f9d8732684ef50f6fdab7858e9a58f1b78b7723fb7d01dcd26"
       else
         url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "8c8735c7b27f20600d34948ff83bc22e5c3f21218ec2e1010748846363e10660"
+        sha256 "75af806a6a2d11ee84c6485c47c3c493dce1d514696678ed8a26973a2a7a4267"
       end
     end
   end
@@ -25,7 +25,7 @@ class Dust < Formula
   def install
     bin.install "dust"
     man1.install "manual/dust.1"
-    bash_completion.install "etc/completions/dust.bash" => "dust"
+    bash_completion.install "etc/completions/dust.bash"
     fish_completion.install "etc/completions/dust.fish"
     zsh_completion.install "etc/completions/_dust"
   end
