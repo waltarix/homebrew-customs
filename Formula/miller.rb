4c7006a0398167a3f04fb0163d671ad3247e2307
waltari <gitting.crew@gmail.com>
2024-07-23 10:06:17 +0900

    miller 6.12.0

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -1,17 +1,22 @@
 class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
-  "6.10.0".tap do |v|
+  "6.12.0".tap do |v|
     if OS.linux?
-      url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-linux_amd64.tar.xz"
-      sha256 "4926ba5b4492909b9a81ca6275f641b4b88611241715b7122782a769e679d562"
+      if Hardware::CPU.flags.grep(/\Aavx512/).size.zero?
+        url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-linux_amd64_v2.tar.xz"
+        sha256 "71bd79593031aeff2faacb85e8551faae9235be43db4c85797743c86fb208e31"
+      else
+        url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-linux_amd64_v4.tar.xz"
+        sha256 "296cec7fc0c45efe5d24c973b8dbdf4dc6152f152e3c1d63880b7e496b676c6d"
+      end
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-darwin_arm64.tar.xz"
-        sha256 "598ac0101e1c3b04e88af0f0c1a8205b0c4a8c999b2bcd230269bbf6746af239"
+        sha256 "56f355c60278aa02236f3c0d5e46ca69870d8c5b320b962f72e0fb8f9a413c9c"
       else
-        url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-darwin_amd64.tar.xz"
-        sha256 "e54d13a5e2b4d85ffd462078d615669c6262ae432a2b8e67368cabe961547089"
+        url "https://github.com/waltarix/miller/releases/download/v#{v}-custom/mlr-#{v}-darwin_amd64_v3.tar.xz"
+        sha256 "b12eb8d02a278123d1e54fd2b776ee124e1833a3a184a8124ba00d621947f0fc"
       end
     end
     version v
