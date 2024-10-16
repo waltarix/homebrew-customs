984529b5d49965f33cca4e5a139f2817120f77c3
waltari <gitting.crew@gmail.com>
2024-10-16 10:20:50 +0900

    python-tabulate: migrate to `python@3.13`

--- Formula/python-tabulate.rb
+++ Formula/python-tabulate.rb
@@ -1,35 +1,21 @@
 class PythonTabulate < Formula
+  include Language::Python::Virtualenv
+
   desc "Pretty-print tabular data in Python"
   homepage "https://github.com/astanin/python-tabulate"
   url "https://github.com/waltarix/python-tabulate/releases/download/v0.9.0-custom-r2/tabulate-0.9.0.tar.gz"
   sha256 "f5c8bc48780a17933e60b042b0919f2b0c90c187413b1299cdabdebf4326b3fe"
   license "MIT"
-  revision 1
+  revision 2
 
-  depends_on "python-setuptools" => :build
-  depends_on "python@3.11" => [:build, :test]
-  depends_on "python@3.12" => [:build, :test] # FIXME: should be runtime dependency
+  depends_on "python@3.13"
   depends_on "waltarix/customs/python-wcwidth"
 
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
-        .sort_by(&:version)
-  end
-
   def install
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, "-m", "pip", "install", *std_pip_args, "."
-    end
+    virtualenv_install_with_resources
   end
 
   test do
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, "-c", "from tabulate import tabulate"
-    end
-
     (testpath/"in.txt").write <<~EOS
       one,two,three
       …,……,………
