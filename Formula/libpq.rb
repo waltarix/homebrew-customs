504838d150205c5b5796065a413bc8d6b05f3361
waltari <gitting.crew@gmail.com>
2025-02-22 18:35:49 +0900

    libpq 17.4

--- Formula/libpq.rb
+++ Formula/libpq.rb
@@ -1,8 +1,8 @@
 class Libpq < Formula
   desc "Postgres C API library"
   homepage "https://www.postgresql.org/docs/current/libpq.html"
-  url "https://ftp.postgresql.org/pub/source/v16.4/postgresql-16.4.tar.bz2"
-  sha256 "971766d645aa73e93b9ef4e3be44201b4f45b5477095b049125403f9f3386d6f"
+  url "https://ftp.postgresql.org/pub/source/v17.4/postgresql-17.4.tar.bz2"
+  sha256 "c4605b73fea11963406699f949b966e5d173a7ee0ccaef8938dec0ca8a995fe7"
   license "PostgreSQL"
 
   livecheck do
@@ -12,7 +12,9 @@ class Libpq < Formula
 
   keg_only "conflicts with postgres formula"
 
-  depends_on "pkg-config" => :build
+  depends_on "bison" => :build
+  depends_on "flex" => :build
+  depends_on "pkgconf" => :build
   depends_on "icu4c"
   # GSSAPI provided by Kerberos.framework crashes when forked.
   # See https://github.com/Homebrew/homebrew-core/issues/47494.
@@ -22,14 +24,9 @@ class Libpq < Formula
   depends_on "zlib"
 
   resource "wcwidth9.h" do
-    url "https://github.com/waltarix/localedata/releases/download/15.1.0-r1/wcwidth9.h"
-    sha256 "5afe09e6986233b517c05e4c82dbb228bb6ed64ba4be6fd7bf3185b7d3e72eb0"
+    url "https://github.com/waltarix/localedata/releases/download/16.0.0/wcwidth9.h"
+    sha256 "a9ddb9059f0a17dc0efee89e7ed73c9b0412b10111987090068f49ba708bfa70"
   end
-
-  # Fix compatibility with OpenSSL 3.2
-  # Remove once merged
-  # Ref https://www.postgresql.org/message-id/CX9SU44GH3P4.17X6ZZUJ5D40N%40neon.tech
-  patch :DATA
 
   def install
     resource("wcwidth9.h").stage(buildpath/"src/common")
@@ -42,6 +39,7 @@ class Libpq < Formula
                           "--prefix=#{prefix}",
                           "--with-gssapi",
                           "--with-openssl",
+                          "--without-xml",
                           "--libdir=#{opt_lib}",
                           "--includedir=#{opt_include}"
     dirs = %W[
@@ -57,11 +55,10 @@ class Libpq < Formula
     system "make", "-C", "src/interfaces", "install", *dirs
     system "make", "-C", "src/common", "install", *dirs
     system "make", "-C", "src/port", "install", *dirs
-    system "make", "-C", "doc", "install", *dirs
   end
 
   test do
-    (testpath/"libpq.c").write <<~EOS
+    (testpath/"libpq.c").write <<~C
       #include <stdlib.h>
       #include <stdio.h>
       #include <libpq-fe.h>
@@ -84,7 +81,7 @@ class Libpq < Formula
 
           return 0;
         }
-    EOS
+    C
     system ENV.cc, "libpq.c", "-L#{lib}", "-I#{include}", "-lpq", "-o", "libpqtest"
     assert_equal "Connection to database attempted and failed", shell_output("./libpqtest")
   end
