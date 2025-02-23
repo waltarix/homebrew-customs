0d8cb4d673f7cb74434b1e385a64d0116245f377
waltari <gitting.crew@gmail.com>
2025-02-23 11:19:11 +0900

    fzf 0.60.1

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -1,27 +1,27 @@
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/refs/tags/v0.55.0.tar.gz"
-  sha256 "805383f71bca7f8fb271ecd716852aea88fd898d5027d58add9e43df6ea766da"
+  url "https://github.com/junegunn/fzf/archive/refs/tags/v0.60.1.tar.gz"
+  sha256 "9252219096cf9a9dbf41fc177b9007ecafcfab4ff61ece8d78fee99cb9997bcc"
   license "MIT"
 
   resource "binary" do
-    "0.55.0".tap do |v|
+    "0.60.1".tap do |v|
       if OS.linux?
         if Hardware::CPU.flags.grep(/\Aavx512/).size.zero?
           url "https://github.com/waltarix/fzf/releases/download/v#{v}-custom/fzf-#{v}-linux_amd64_v2.tar.xz"
-          sha256 "d1d2df1469335374ff7297a5654ccde3c82fd3ba0ce5d2cc7e0d2ecc64991d96"
+          sha256 "b57b323d960c010af0c75a0ea2959df9cd8b93c3e97e086576b6e1de00776c3b"
         else
           url "https://github.com/waltarix/fzf/releases/download/v#{v}-custom/fzf-#{v}-linux_amd64_v4.tar.xz"
-          sha256 "8e6494caaa1784a2ac21b88f21b530f11972dc6eaf1423107effae00b39b602c"
+          sha256 "909665d4c282ca3a6b19e3fc64bb0e530112bf2f2f8bb6e5d50553392c52668b"
         end
       else
         if Hardware::CPU.arm?
           url "https://github.com/waltarix/fzf/releases/download/v#{v}-custom/fzf-#{v}-darwin_arm64.tar.xz"
-          sha256 "1d120caa7d2f7183781ff555967e5c62f60f415f3e25ab8cb8dbafd8a97f4bba"
+          sha256 "da8ac2a2903557aea99fae2ecff7cb47f0a3cf2726ffed2711ca6c94f3910789"
         else
           url "https://github.com/waltarix/fzf/releases/download/v#{v}-custom/fzf-#{v}-darwin_amd64_v3.tar.xz"
-          sha256 "0d358c48323e4b031409a7a7aefca54f2ab0661ebeee73fe380427d0d58cf6ca"
+          sha256 "8c953c1661abe69d6595494d12fa55a9912084f4c55ad73638a15b1f48c91c5f"
         end
       end
     end
@@ -41,20 +41,13 @@ class Fzf < Formula
     (prefix/"shell").install %w[bash zsh fish].map { |s| "shell/key-bindings.#{s}" }
     (prefix/"shell").install %w[bash zsh].map { |s| "shell/completion.#{s}" }
     man1.install "man/man1/fzf.1", "man/man1/fzf-tmux.1"
+    bin.install "bin/fzf-preview.sh"
   end
 
   def caveats
     <<~EOS
-      To set up shell integration, add this to your shell configuration file:
-        # bash
-        eval "$(fzf --bash)"
-
-        # zsh
-        source <(fzf --zsh)
-
-        # fish
-        fzf --fish | source
-
+      To set up shell integration, see:
+        https://github.com/junegunn/fzf#setting-up-shell-integration
       To use fzf in Vim, add the following line to your .vimrc:
         set rtp+=#{opt_prefix}
     EOS
