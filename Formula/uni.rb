c7b06e71e3764295627a6707cb53cb27606381ea
waltari <gitting.crew@gmail.com>
2025-02-24 17:45:44 +0900

    uni 2.8.0

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -1,23 +1,23 @@
 class Uni < Formula
   desc "Unicode database query tool for the command-line"
   homepage "https://github.com/arp242/uni"
-  ["2.7.0", nil].tap do |(v, r)|
+  ["2.8.0", nil].tap do |(v, r)|
     rev = r ? "-r#{r}" : ""
     if OS.linux?
       if Hardware::CPU.flags.grep(/\Aavx512/).size.zero?
         url "https://github.com/waltarix/uni/releases/download/v#{v}-custom#{rev}/uni-#{v}-linux_amd64_v2.tar.xz"
-        sha256 "213bd4005129fb2bd28d52bb11972bbd75888a0415d6879b3e42fcd83fe15a8b"
+        sha256 "77f0664f2ef4ecb6313a30842e4873f44a1b7728bd3ee8a136cef1b9d1990fa9"
       else
         url "https://github.com/waltarix/uni/releases/download/v#{v}-custom#{rev}/uni-#{v}-linux_amd64_v4.tar.xz"
-        sha256 "394b43e30310d387ecfac9673fc6c7de8b869154003d62a43011a9467e133c01"
+        sha256 "3543b14eb246ebb932ff31079d88aab3d47ce6263f18c0918888722e6830c311"
       end
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/uni/releases/download/v#{v}-custom#{rev}/uni-#{v}-darwin_arm64.tar.xz"
-        sha256 "f4efc1e8a5e5beb31a2edfd62968d59158ba97dafb9004b66fc71eb2e55e0375"
+        sha256 "9adb14e7fc21d7853581ed42634ee7d81b770d609f1908b532e4c6e06b13d5d3"
       else
         url "https://github.com/waltarix/uni/releases/download/v#{v}-custom#{rev}/uni-#{v}-darwin_amd64_v3.tar.xz"
-        sha256 "610f0450950ba474aad2208d920bf7760d0b236a7c6b6b68185c82ca5edc2136"
+        sha256 "b1b472cbe59317d1309add17c3c81bb714034ebd9e9de27a27b4f19536f8c10d"
       end
     end
     version v
