7f13824c23de6e16145f0da4f12f3cf9dd110030
waltari <gitting.crew@gmail.com>
2025-05-17 16:40:34 +0900

    dust 1.2.0

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -1,17 +1,17 @@
 class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
-  "1.1.2".tap do |v|
+  "1.2.0".tap do |v|
     if OS.linux?
       url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "42fdceb87d5dde0ce7481d313308f91b2c203b6bda48912de4d56b5ddd7aff70"
+      sha256 "3590df4e31416da7d46123cfd366d8f943ff0f8b6cebc71e42346e28a38a47ff"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "5f022ad7be81c90b0906b63680515b55a0efecf87218d24528fe1b9b33fb5365"
+        sha256 "8ee8c344c271704765bbdad57eff87fd0aa345d1ea2140dd46b96d8cafc9b8fb"
       else
         url "https://github.com/waltarix/dust/releases/download/v#{v}-custom/dust-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "e045b377a098742ef8cc67be8239cf0c7dbb9c78e6e06ae8ad76e0384c569ae5"
+        sha256 "5df4da11e5bdbb54cc80a139278e165fe41fbc0f1bbdddaf0b3dfd439b3f56f3"
       end
     end
   end
