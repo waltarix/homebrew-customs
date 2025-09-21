08ad76888be46169503e0996f0e39bf90330946c
waltari <gitting.crew@gmail.com>
2025-09-21 17:46:27 +0900

    xh 0.25.0

--- Formula/xh.rb
+++ Formula/xh.rb
@@ -1,18 +1,18 @@
 class Xh < Formula
   desc "Friendly and fast tool for sending HTTP requests"
   homepage "https://github.com/ducaale/xh"
-  ["0.24.1", nil].tap do |(v, r)|
+  ["0.25.0", nil].tap do |(v, r)|
     rev = r ? "-r#{r}" : ""
     if OS.linux?
       url "https://github.com/waltarix/xh/releases/download/v#{v}-custom#{rev}/xh-#{v}-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "4b770cd7998d41f6c076f0fe457fee830e227a75f320d2187107feefb12f2ee0"
+      sha256 "0e1764d1dcedc6053c5568a1821a60471b283d60243b64f44265cc1de475635a"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/xh/releases/download/v#{v}-custom#{rev}/xh-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "471ba23028eb01915279c010bb6ea08063670667949171450c9a7cd732dc203a"
+        sha256 "01005eb59f0a2e23ae243d6a9ff4caf13080550672c1518d4e00f269e58c2f68"
       else
         url "https://github.com/waltarix/xh/releases/download/v#{v}-custom#{rev}/xh-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "64d2de393b6ea95f8b38a6d2a0a5cbf3f0c6ae3b498de6cc4b04e2b2b1eb18d0"
+        sha256 "3897ea2813234cfc3b79f4e1ffdba6344647d7a3ad2b594acf34cd44db9b18c2"
       end
     end
     revision r if r
