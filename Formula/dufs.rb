94f5843998759bbc79b5e60e84561f5ca88546ea
waltari <gitting.crew@gmail.com>
2025-11-08 18:43:40 +0900

    dufs 0.45.0

--- Formula/dufs.rb
+++ Formula/dufs.rb
@@ -1,18 +1,18 @@
 class Dufs < Formula
   desc "Static file server"
   homepage "https://github.com/sigoden/dufs"
-  ["0.44.0", nil].tap do |(v, r)|
+  ["0.45.0", nil].tap do |(v, r)|
     rev = r ? "-r#{r}" : ""
     if OS.linux?
       url "https://github.com/waltarix/dufs/releases/download/v#{v}-custom#{rev}/dufs-#{v}-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "2ff6fb37620ff104e10117db8118fd853a9bb479c462ad739969f03c31d7b285"
+      sha256 "32fdb9797fb663db98727060a1e50771e975b696ff677e0b839ae7abf64e1f5e"
     else
       if Hardware::CPU.arm?
         url "https://github.com/waltarix/dufs/releases/download/v#{v}-custom#{rev}/dufs-#{v}-aarch64-apple-darwin.tar.xz"
-        sha256 "d0058e64d3c4310e33e80a296e4bce2497a4461140d1d60c6a58f09e0a7f1c34"
+        sha256 "1ddd639242aa77d155ae5d97a51397c202d2df1920e31b2363952f76be8b5cc9"
       else
         url "https://github.com/waltarix/dufs/releases/download/v#{v}-custom#{rev}/dufs-#{v}-x86_64-apple-darwin.tar.xz"
-        sha256 "b1ef2008c67a8e351f35e1527ca9c7b6f00ff87f25343572f8255571ed0bfcbd"
+        sha256 "99c0ec33104f1a2602fe1584065d71f70d87a48f8d0d8adad9be0a254c0c9aed"
       end
     end
     revision r if r
