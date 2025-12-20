1ff93c0e6bd7e8eebaa7a8b340dfc71fe2264323
waltari <gitting.crew@gmail.com>
2025-12-20 17:32:23 +0900

    pspg 5.8.14

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -1,8 +1,8 @@
 class Pspg < Formula
   desc "Unix pager optimized for psql"
   homepage "https://github.com/okbob/pspg"
-  url "https://github.com/okbob/pspg/archive/refs/tags/5.8.13.tar.gz"
-  sha256 "b6f198a98c0e8ec0f1ea0893deb23f20be7196a4f075c879722f91bef65a12f8"
+  url "https://github.com/okbob/pspg/archive/refs/tags/5.8.14.tar.gz"
+  sha256 "9ff44945fdf08b99468808ff67c903f62205583743b6b45921dc6b366aa5e243"
   license "BSD-2-Clause"
   head "https://github.com/okbob/pspg.git", branch: "master"
 
